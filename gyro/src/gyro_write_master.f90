3a17ff4386467852ecdaed3f63fec16669a63d13
Jeff Candy <jcandy@san.rr.com>
2010-12-20 18:17:00 -0800

    Moved I/O helper subroutines specific to gyro_write_master inside gyro_write_master.

--- gyro/src/gyro_write_master.f90
+++ gyro/src/gyro_write_master.f90
@@ -3,7 +3,12 @@
 !
 ! PURPOSE:
 !  This is the master file controlling output of
-!  data on a per-timestep basis.
+!  data on a per-timestep basis.  This file also 
+!  contains the MPI IO routines 
+!
+!  - write_distributed_real
+!  - write_distributed_complex
+!  - write_local_real
 !-----------------------------------------------------
 
 subroutine gyro_write_master(action)
@@ -521,3 +526,345 @@ subroutine gyro_write_master(action)
 10 format(t2,a,t24,es9.3)
 
 end subroutine gyro_write_master
+
+!===========================================================================
+
+!------------------------------------------------------
+! write_distributed_real.f90
+!
+! PURPOSE:
+!  Control merged output of distributed real array.
+!------------------------------------------------------
+
+subroutine write_distributed_real(datafile,io,action,n_fn,fn)
+
+  use gyro_globals, only : &
+       n_n,&
+       n_n_1,&
+       n_proc_1,&
+       recv_status,&
+       data_step,&
+       GYRO_COMM_WORLD,&
+       i_proc,&
+       i_err
+
+  !------------------------------------------------------
+  implicit none
+  !
+  character (len=*), intent(in) :: datafile
+  integer, intent(in) :: io
+  integer, intent(in) :: action
+  integer, intent(in) :: n_fn
+  real, intent(in) :: fn(n_fn)
+  !
+  integer :: data_loop
+  integer :: i_group_send
+  integer :: i_send
+  integer :: in
+  !
+  real :: fn_recv(n_fn)
+  !------------------------------------------------------
+
+  include 'mpif.h'
+
+  !-----------------------------
+  ! action = 1 -> open file
+  !        = 2 -> write to file
+  !        = 3 -> reposition
+  !-----------------------------
+
+  select case (action)
+
+  case(1)
+ 
+     ! Initial open
+
+     if (i_proc == 0) then
+        open(unit=io,file=datafile,status='replace')
+        close(io)
+     endif
+
+  case(2,4)
+
+     ! Output
+
+     if (i_proc == 0) &
+          open(unit=io,file=datafile,status='old',position='append')
+
+     do in=1,n_n
+
+        !-----------------------------------------
+        ! Subgroup collector:
+        !
+        i_group_send = (in-1)/n_n_1
+
+        if (i_group_send /= 0) then
+
+           i_send = i_group_send*n_proc_1
+
+           if (i_proc == 0) then
+
+              call MPI_RECV(fn_recv,&
+                   n_fn,&
+                   MPI_DOUBLE_PRECISION,&
+                   i_send,&
+                   in,&
+                   GYRO_COMM_WORLD,&
+                   recv_status,&
+                   i_err)
+
+           else if (i_proc == i_send) then
+
+              call MPI_SEND(fn,&
+                   n_fn,&
+                   MPI_DOUBLE_PRECISION,&
+                   0,&
+                   in,&
+                   GYRO_COMM_WORLD,&
+                   i_err)
+
+           endif
+
+        else
+
+           fn_recv(:) = fn(:)
+
+        endif
+        !
+        !-----------------------------------------
+
+        if (i_proc == 0) then
+
+           write(io,10) fn_recv(:)
+
+        endif
+
+     enddo ! in
+
+     if (i_proc == 0) close(io)
+
+  case(3)
+
+     ! Reposition after restart
+
+     if (i_proc == 0) then
+
+        open(unit=io,file=datafile,status='old')
+        do data_loop=0,data_step
+
+           do in=1,n_n
+              read(io,10) fn_recv(:)
+           enddo
+
+        enddo ! data_loop
+
+        endfile(io)
+        close(io)
+
+     endif
+
+  end select
+
+10 format(es11.4,1x)
+
+end subroutine write_distributed_real
+
+!===========================================================================
+
+!------------------------------------------------------
+! write_distributed_complex.f90
+!
+! PURPOSE:
+!  Control merged output of complex distributed array.
+!------------------------------------------------------
+
+subroutine write_distributed_complex(datafile,io,action,n_fn,fn)
+
+  use gyro_globals, only : &
+       n_n,&
+       n_n_1,&
+       n_proc_1,&
+       recv_status,&
+       data_step,&
+       GYRO_COMM_WORLD,&
+       i_proc,&
+       i_err
+
+  !------------------------------------------------------
+  implicit none
+  !
+  character (len=*), intent(in) :: datafile
+  integer, intent(in) :: io
+  integer, intent(in) :: action
+  integer, intent(in) :: n_fn
+  complex, intent(in) :: fn(n_fn)
+  !
+  integer :: data_loop
+  integer :: i_group_send
+  integer :: i_send
+  integer :: in
+  !
+  complex :: fn_recv(n_fn)
+  !------------------------------------------------------
+
+  include 'mpif.h'
+
+  !-----------------------------
+  ! action = 1 -> open file
+  !        = 2 -> write to file
+  !        = 3 -> reposition
+  !-----------------------------
+
+  select case (action)
+
+  case(1)
+
+     ! Initial open
+
+     if (i_proc == 0) then
+        open(unit=io,file=datafile,status='replace')
+        close(io)
+     endif
+
+  case(2,4)
+
+     ! Output
+
+     if (i_proc == 0) &
+          open(unit=io,file=datafile,status='old',position='append')
+
+     do in=1,n_n
+
+        !-----------------------------------------
+        ! Subgroup collector:
+        !
+        i_group_send = (in-1)/n_n_1
+
+        if (i_group_send /= 0) then
+
+           i_send = i_group_send*n_proc_1
+
+           if (i_proc == 0) then
+
+              call MPI_RECV(fn_recv,&
+                   n_fn,&
+                   MPI_DOUBLE_COMPLEX,&
+                   i_send,&
+                   in,&
+                   GYRO_COMM_WORLD,&
+                   recv_status,&
+                   i_err)
+
+           else if (i_proc == i_send) then
+
+              call MPI_SEND(fn,&
+                   n_fn,&
+                   MPI_DOUBLE_COMPLEX,&
+                   0,&
+                   in,&
+                   GYRO_COMM_WORLD,&
+                   i_err)
+
+           endif
+
+        else
+
+           fn_recv(:) = fn(:)
+
+        endif
+        !
+        !-----------------------------------------
+
+        if (i_proc == 0) then
+
+           write(io,10) fn_recv(:)
+
+        endif
+
+     enddo ! in
+
+     if (i_proc == 0) close(io)
+
+  case(3)
+
+     ! Reposition after restart
+
+     if (i_proc == 0) then
+
+        open(unit=io,file=datafile,status='old')
+        do data_loop=0,data_step
+
+           do in=1,n_n
+              read(io,10) fn_recv(:)
+           enddo
+
+        enddo ! data_loop
+
+        endfile(io)
+        close(io)
+
+     endif
+
+  end select
+
+10 format(2(es11.4,1x))
+
+end subroutine write_distributed_complex
+
+!===========================================================================
+
+!------------------------------------------------------
+! write_local_real.f90
+!
+! PURPOSE:
+!  This routine write a vector of nondistributed reals.
+!------------------------------------------------------
+
+subroutine write_local_real(datafile,io,action,n_fn,fn)
+
+  use gyro_globals, only : &
+       data_step
+
+  !---------------------------------------------------
+  implicit none
+  !
+  character (len=*), intent(in) :: datafile
+  integer, intent(in) :: io
+  integer, intent(in) :: action
+  integer, intent(in) :: n_fn
+  real, intent(in) :: fn(n_fn)
+  !
+  integer :: data_loop
+  real :: dummy(n_fn)
+  !---------------------------------------------------
+
+  select case (action)
+
+  case(1)
+
+     open(unit=io,file=datafile,status='replace')
+     close(io)
+
+  case(2,4)
+
+     open(unit=io,file=datafile,status='old',position='append')
+     write(io,'(20(1pe15.8,1x))')  fn(:)
+     close(io)
+
+  case(3)
+
+     ! Reposition after restart
+
+     open(unit=io,file=datafile,status='old')
+
+     do data_loop=0,data_step
+        read(io,*) dummy(:)
+     enddo
+
+     endfile(io)
+     close(io)
+
+  end select
+
+end subroutine write_local_real
+
