d47992f04304ad04e3b9e8e5f702b2b0229f5727
Jeff Candy <jcandy@san.rr.com>
2011-01-19 01:08:49 -0800

    Cleaned up and added some additional documentation.

--- gyro/src/gyro_bessel_operator.f90
+++ gyro/src/gyro_bessel_operator.f90
@@ -1,14 +1,35 @@
-!---------------------------------------------------------
+!--------------------------------------------------------------
 ! gyro_bessel_operator.f90
 !
-! itype=1:   J_0 
-!       2:   J_0^2
-!       3:   -(i/2)*k_x*rho*[ J_0(z)+J_2(z) ]
-!       4:   G_perp = (1/2)*[ J_0(z)+J_2(z) ] 
-!       5:   G_perp^2 
-!       6:   G_perp*J_0
-!       7:   I_0
-!---------------------------------------------------------
+! PURPOSE:
+!  Compute all the Bessel-function kernel (averaging) stencils
+!  to the required bandwidth (m_gyro).  In the limit of maximal
+!  m_gyro (n_x/2), these are the pseudospectral operators.
+!
+! NOTES:
+!
+! itype=1:  J_0(x) 
+!       2:  J_0^2(x)
+!       3:  -(i/2)*k_x*rho*[ J_0(x)+J_2(x) ]
+!       4:  G_perp = (1/2)*[ J_0(x)+J_2(x) ] 
+!       5:  G_perp^2(x) 
+!       6:  G_perp(x)*J_0(x)
+!       7:  I_0(x^2)
+!
+! x = rho*sqrt((2*pi*p*a+u)^2+v^2)
+!
+! kx -> pi_2*p*a + u
+! ky -> v
+! 
+! INPUT:
+!
+!  a = |grad(r)|/L 
+!  u = k_theta*Gq*Theta
+!  v = k_theta*Gq 
+!
+!  rho = v_perp/Omega_c               (itype=1,..,6)
+!  rho = rhos_unit*sqrt(T)/(mu*z*b0)  (itype=7)
+!---------------------------------------------------------------
 
 subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
 
@@ -23,13 +44,12 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
   real, intent(in) :: v
   real, intent(in) :: rho
   integer, intent(in) :: itype
+  complex, intent(inout) :: g(-m_gyro:m_gyro-i_gyro)
   !
   integer :: p
   integer :: p0
   integer :: m
   integer :: ierr
-  !
-  complex, intent(inout) :: g(-m_gyro:m_gyro-i_gyro)
   !
   real :: x
   real :: bessel(0:2)
@@ -42,6 +62,7 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
 
   p0 = n_x/2
 
+
   select case (itype)
 
   case (1)
@@ -49,12 +70,8 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! J_0
 
      do p=-p0,p0-1
-
-        ! kx = pi_2*p*a + u
-        ! ky = v
-
-        func(p)=BESJ0(rho*sqrt((pi_2*p*a+u)**2+v**2))/n_x
-
+        x = rho*sqrt((pi_2*p*a+u)**2+v**2)
+        func(p)=BESJ0(x)/n_x
      enddo
 
   case (2)
@@ -62,27 +79,23 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! J_0^2
 
      do p=-p0,p0-1
-
-        ! kx = pi_2*p*a + u
-        ! ky = v
-
-        func(p)=BESJ0(rho*sqrt((pi_2*p*a+u)**2+v**2))**2/n_x
-
+        x = rho*sqrt((pi_2*p*a+u)**2+v**2)
+        func(p)=BESJ0(x)**2/n_x
      enddo
 
   case (3)
+
+     ! POSSIBLE ISSUE: the accuracy of this operator when 
+     ! banded has not been studied.
 
      ! -(i/2)*k_x*rho*[ J0(z)+J2(z) ]
 
      ! The factor -(i/2) will be applied outside this loop
 
      do p=-p0,p0-1
-
         x = rho*sqrt((pi_2*p*a+u)**2+v**2)
         call RJBESL(x,0.0,3,bessel,ierr)
-
         func(p) = (pi_2*p*a+u)*rho*(bessel(0)+bessel(2))/n_x
-
      enddo
 
   case (4)
@@ -90,12 +103,9 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! G = (1/2)*[ J_0(z)+J_2(z) ] 
 
      do p=-p0,p0-1
-
         x = rho*sqrt((pi_2*p*a+u)**2+v**2)
         call RJBESL(x,0.0,3,bessel,ierr)
-
         func(p) = 0.5*(bessel(0)+bessel(2))/n_x
-
      enddo
 
   case (5)
@@ -103,12 +113,9 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! G^2 
 
      do p=-p0,p0-1
-
         x = rho*sqrt((pi_2*p*a+u)**2+v**2)
         call RJBESL(x,0.0,3,bessel,ierr)
-
         func(p) = (0.5*(bessel(0)+bessel(2)))**2/n_x
-
      enddo
 
   case (6)
@@ -116,12 +123,9 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! G * J_0
 
      do p=-p0,p0-1
-
         x = rho*sqrt((pi_2*p*a+u)**2+v**2)
         call RJBESL(x,0.0,3,bessel,ierr)
-
         func(p) = 0.5*(bessel(0)+bessel(2))*bessel(0)/n_x
-
      enddo
 
   case (7)
@@ -129,7 +133,8 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      ! I_0
 
      do p=-p0,p0-1
-        func(p) = BESEI0(rho**2*((pi_2*p*a+u)**2+v**2))/n_x
+        x = rho*sqrt((pi_2*p*a+u)**2+v**2)
+        func(p) = BESEI0(x*x)/n_x
      enddo
 
   end select
@@ -143,7 +148,11 @@ subroutine gyro_bessel_operator(rho,a,u,v,g,itype)
      g(m) = g0
   enddo
 
-  ! Add final factor if i for case 3
+  ! Add final factor of i for case 3
   if (itype == 3) g = -(i_c/2.0)*g
+
+  if (debug_flag == 1 .and. i_proc == 0) then
+     print *,'[gyro_bessel_operator done]'
+  endif
 
 end subroutine gyro_bessel_operator
