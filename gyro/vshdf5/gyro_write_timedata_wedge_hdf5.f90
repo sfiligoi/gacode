6fa7ee27bbf70e04e1170fd7e579e11f5b03a6be
Srinath Vadlamani <srinath@txcorp.com>
2011-06-07 17:07:23 -0600

    Calculate RZ->rho,theta jacobian.
    In order to interpolate to the rho,theta grid for synthetic diagnostics, we must calculate the jacobian.
    We now dump this on the rho,theta grid to the wege hdf5 file.

--- gyro/vshdf5/gyro_write_timedata_wedge_hdf5.f90
+++ gyro/vshdf5/gyro_write_timedata_wedge_hdf5.f90
@@ -49,6 +49,11 @@ subroutine gyro_write_timedata_wedge_hdf5
   !
   n_wedge = n_theta_plot*n_theta_mult
 
+  if (i_proc == 0) then
+    if (n_wedge <= 1) write(*,*) "Wedge caluculations need  n_theta_plot*n_theta_mult > 1."
+  endif
+
+
   !---------------------------------------------------
   ! SEK: Should I do this every time?
   !---------------------------------------------------
@@ -155,13 +160,25 @@ contains
     !------------------------------------------
     real, dimension(:,:), allocatable :: Rc,Zc,Rf,Zf
     real, dimension(:,:,:), allocatable :: bufferwedgeMesh
-    real :: theta, rmajc, zmagc, kappac, deltac, zetac, r_c, dr,xdc
+    real :: rmajc, zmagc, kappac, deltac, zetac, dr,xdc,rhoc
+    real :: dRdr,dZdr,dkappadr,ddeltadr,dzetadr 
+    real, dimension (:), allocatable :: theta,r_c
+    real, dimension (:,:), allocatable :: dRdrho,DRdtheta
+    real, dimension (:,:), allocatable :: dZdrho,DZdtheta
+    real, dimension (:,:), allocatable :: rtJacobian
     real :: zeta_wedge
     integer :: iphi, ix, iy, j, ncoarse, nwedge
 
     ncoarse = n_theta_plot
     nwedge = n_theta_plot*n_theta_mult
     allocate(Rf(1:nwedge,n_x), Zf(1:nwedge,n_x))
+    allocate(theta(1:nwedge), r_c(1:n_x))
+
+    allocate(dRdrho(1:nwedge,1:n_x))
+    allocate(dZdrho(1:nwedge,1:n_x))
+    allocate(dRdtheta(1:nwedge,1:n_x))
+    allocate(dZdtheta(1:nwedge,1:n_x))
+    allocate(rtJacobian(1:nwedge,1:n_x))
 
     !----------------------------------------
     ! Calculate the R,Z coordinates.  See write_geometry_arrays.f90
@@ -169,28 +186,59 @@ contains
 
     do ix=1,n_x
        if (flat_profile_flag == 0) then
-          r_c=r_s(ix)
+          r_c(ix)=r_s(ix)
        else
-          r_c=r(ix)
+          r_c(ix)=r(ix)
        endif
-       rmajc = rmaj_s(ix)
-       zmagc = zmag_s(ix)
-       kappac = kappa_s(ix)
-       deltac = delta_s(ix)
-       xdc    = asin(deltac)
-       zetac  = zeta_s(ix)
-       do j=1,nwedge
+    enddo 
+    do j=1,nwedge
           ! This needs to match up with what's in gyro_set_blend_arrays.f90
-          theta=theta_wedge_offset+real(j-1)*theta_wedge_angle/       &
+          theta(j)=theta_wedge_offset+real(j-1)*theta_wedge_angle/       &
                real(n_theta_plot*n_theta_mult-1)
           !theta = -pi+REAL(j)*pi*2./REAL(nwedge)
+    enddo
+
+    do ix=1,n_x
+       rhoc     = r_c(ix)
+       rmajc  = rmaj_s(ix)
+       dRdr   = drmaj_s(ix)
+       zmagc  = zmag_s(ix)
+       dZdr   = dzmag_s(ix)
+       kappac = kappa_s(ix)
+       dkappadr = (kappac/rhoc)*s_kappa_s(ix)
+       deltac   = delta_s(ix)
+       ddeltadr = (1./rhoc)*s_delta_s(ix)
+       xdc    = asin(deltac)
+       zetac  = zeta_s(ix)
+       dzetadr = (1./rhoc)*s_zeta_s(ix)
+
+       do j=1,nwedge
           if(radial_profile_method==1) then
-             Rf(j,ix)=rmajc+r_c*cos(theta)
-             Zf(j,ix)=zmagc+r_c*sin(theta)
+            Rf(j,ix)=rmajc+rhoc*cos(theta(j))
+            Zf(j,ix)=zmagc+rhoc*sin(theta(j))
           else
-             Rf(j,ix)=rmajc+r_c*cos(theta+xdc*sin(theta))
-             Zf(j,ix)=zmagc+kappac*r_c*sin(theta+zetac*sin(2.*theta))
+            Rf(j,ix)=rmajc+rhoc*cos(theta(j)+xdc*sin(theta(j)))
+            Zf(j,ix)=zmagc+kappac*rhoc*sin(theta(j)+zetac*sin(2.*theta(j)))
           endif
+!pieces of jacobian 
+          dRdrho(j,ix) = COS(theta(j)+xdc*SIN(theta(j))) + &
+            dRdr - (1./SQRT(1-deltac**2))* &
+            rhoc*SIN(theta(j))*SIN(theta(j)+xdc*SIN(theta(j)))*ddeltadr
+
+          dZdrho(j,ix) = SIN(theta(j)+SIN(2.*theta(j))*zetac*kappac) + &
+            dZdr + rhoc*COS(theta(j)+SIN(2.*theta(j))*zetac)*SIN(2.*theta(j))* &
+            kappac*dzetadr + &
+            rhoc*SIN(theta(j)+SIN(2.*theta(j))*zetac)*dkappadr
+ 
+          dRdtheta(j,ix) = -1.*rhoc*(1.+xdc*COS(theta(j)))* &
+            SIN(theta(j)+xdc)*SIN(theta(j))
+
+          dZdtheta(j,ix)= rhoc*COS(theta(j)+SIN(2.*theta(j))*zetac) * &
+            (1.+2.*COS(2*theta(j))*zetac*kappac)
+
+          rtJacobian(j,ix) = dRdrho(j,ix)*dZdtheta(j,ix) - &
+            dZdrho(j,ix)*dRdtheta(j,ix)
+
        enddo
     enddo
 
@@ -220,6 +268,10 @@ contains
     h5in%units="m"
     call dump_h5(gidwedge,'R',Rf*a_meters,h5in,h5err)
     call dump_h5(gidwedge,'Z',Zf*a_meters,h5in,h5err)
+    call dump_h5(gidwedge,'rho',r_c*a_meters,h5in,h5err)
+    call dump_h5(gidwedge,'rtJacobian',rtJacobian*a_meters,h5in,h5err)
+    h5in%units="radians"
+    call dump_h5(gidwedge,'theta',theta,h5in,h5err)
 
     ! For ease of use, have a single data set that has R,Z. 
     allocate(bufferwedgeMesh(nwedge,n_x,2))
@@ -235,6 +287,9 @@ contains
     ! 
     !---------------------------------------- 
     deallocate(Rf, Zf)
+
+    deallocate(theta, dRdrho,dZdrho,dRdtheta,dZdtheta)
+
     return
   end subroutine hdf5_write_wedge_coords
 
