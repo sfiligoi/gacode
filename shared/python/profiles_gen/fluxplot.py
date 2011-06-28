2b7b01868373f02cbca4003098b4ac9e2efbf9b9
buuck <buuck@stolaf.edu>
2011-06-28 12:48:18 -0700

    Still figuring out how this works.  Here are some things I forgot to push earlier.

---
+++ shared/python/profiles_gen/fluxplot.py
@@ -0,0 +1,91 @@
+"""fluxplot.py contains plotting routines for data parsed with
+profiles_genData.  As the name suggests, it plots flux surfaces.
+
+This module mostly just checks for errors in the user input and then passes
+control over to profiles_genData.
+
+The module requires at least 4 command line arguments to run: the module
+itself, a directory location, a plot type, and a radius to plot.  Optionally,
+the user can specify another radius, and a number of radii to plot between
+the two given radii.  See ~/gacode/shared/bin/profiles_gen for more
+information on the command line interface."""
+
+import sys
+import matplotlib.pyplot as plt
+from profiles_genData import profiles_genData
+from math import *
+
+#Error catching
+if len(sys.argv) < 4:
+    print "ERROR: Too few arguments.  Type profiles_gen for help."
+    sys.exit()
+try:
+    prof1 = profiles_genData(sys.argv[1])
+except IOError:
+    print "ERROR: " + sys.argv[1]
+    print "does not contain file input.profiles and/or file input.profiles.geo.  Type profiles_gen for help."
+    sys.exit()
+try:
+    m1 = float(sys.argv[3])
+except ValueError:
+    print "ERROR: " + sys.argv[3] + " is not a valid number."
+    sys.exit()
+if m1 > 1:
+    print "ERROR: Rho1 cannot be more than 1."
+    sys.exit()
+if m1 < 0:
+    print "ERROR: Rho1 cannot be less than 0."
+    sys.exit()
+
+#If there are 6 arguments, then the user is looking for a range of radii to be
+#plotted.
+if len(sys.argv) == 6:
+    #More error catching
+    try:
+        m2 = float(sys.argv[4])
+    except ValueError:
+        print "ERROR: " + m2 + " is not a vaild number."
+        sys.exit()
+    if m2 > 1:
+        print "ERROR: Rho2 cannot be more than 1."
+        sys.exit()
+    if m2 < 0:
+        print "ERROR: Rho2 cannot be less than 0."
+        sys.exit()
+    try:
+        n = float(sys.argv[5])
+    except ValueError:
+        print "ERROR: " + n + " is not a vaild number."
+        sys.exit()
+    if n != floor(n):
+        print "ERROR: Number of flux surfaces must be an integer."
+        sys.exit()
+    if n < 2:
+        print "ERROR: Number of flux surfaces must at least 2."
+        sys.exit()
+    min1 = min(m1, m2)
+    max1 = max(m1, m2)
+    if min1 == max1:
+        print "ERROR: Radius interval must be greater than zero."
+        sys.exit()
+    #Checks to see if requested number of surfaces is greater than total
+    #number available.
+    maxn = prof1.match(max1, prof1.get('rho (-)')) - prof1.match(min1, prof1.get('rho (-)'))
+    if maxn == 0:
+        print "ERROR: No flux surfaces in given interval.  Please specify a larger interval."
+        sys.exit()
+    if maxn < n:
+        print "Warning: Maximum number of flux surfaces for this interval"
+        print "is " + str(maxn) + ".  Changing n to " + str(maxn) + "."
+        n = maxn
+    prof1.fplot(sys.argv[2], len(sys.argv), min1, max1, n)
+
+#If there are only 4 arguments, then the user is only looking for one radius.
+elif len(sys.argv) == 4:
+    prof1.fplot(sys.argv[2], len(sys.argv), m1)
+
+#Any other number of arguments is an error.
+else:
+    print "ERROR: Wrong number of arguments.  Type profiles_gen for help."
+    sys.exit()
+plt.show()
