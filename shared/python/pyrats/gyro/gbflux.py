f988781277d21201236475cdcddb41927b50b349
buuck <buuck@stolaf.edu>
2011-07-25 09:43:01 -0700

    Added y-labels to gbflux plot.

--- shared/python/pyrats/gyro/gbflux.py
+++ shared/python/pyrats/gyro/gbflux.py
@@ -6,8 +6,8 @@ from pyrats.data import GYROData
 directory=sys.argv[1]
 n_field=sys.argv[3]
 try:
-    n_kinetic=int(sys.argv[2])-1
-    n_moment=int(sys.argv[4])-1
+    n_kinetic=int(sys.argv[2])
+    n_moment=int(sys.argv[4])
 except ValueError:
     print "ERROR: Invalid parameter.  Try again."
     sys.exit()
@@ -17,7 +17,7 @@ try:
 except IOError:
     print "ERROR: Invalid directory location.  Try again."
     sys.exit()
-if (n_kinetic > sim1.profile['n_kinetic']) or (n_moment > 3):
+if (n_kinetic > sim1.profile['n_kinetic']) or (n_moment > 2):
     print "ERROR: Parameter out of range.  Try again."
     sys.exit()
 sim1.make_gbflux()
@@ -25,18 +25,25 @@ fig = plt.figure(1)
 ax = fig.add_subplot(111)
 if n_field == 's':
     ax.plot(np.sum(sim1.gbflux, axis=1)[n_kinetic, n_moment, :])
-    ax.set_title("Species " + str(n_kinetic + 1) + ", field " + str(n_field) + ", moment " + str(n_moment + 1))
-    ax.set_xlabel("Time")
+    ax.set_title("Species " + str(n_kinetic) + ", field " + str(n_field) + ", moment " + str(n_moment))
 else:
-    try:
-        n_field = int(n_field)-1
+     try:
+        n_field = int(n_field)
         if (n_field > sim1.profile['n_field']):
             print "ERROR: Parameter out of range.  Try again."
             sys.exit()
-    except ValueError:
+     except ValueError:
         print "ERROR: Invalid parameter.  Try again."
         sys.exit()
-    ax.plot(sim1.gbflux[n_kinetic, n_field, n_moment, :])
-    ax.set_title("Species " + str(n_kinetic + 1) + ", field " + str(n_field + 1) + ", moment " + str(n_moment + 1))
-    ax.set_xlabel("Time")
+     ax.plot(sim1.gbflux[n_kinetic, n_field, n_moment, :])
+     ax.set_title("Species " + str(n_kinetic) + ", field " + str(n_field) + ", moment " + str(n_moment))
+ax.set_xlabel("Time")
+if n_moment == 0:
+    ax.set_ylabel("GAMMA_" + str(n_kinetic) + "/GAMMA_{GB}")
+elif n_moment == 1:
+    ax.set_ylabel("Q_{0," + str(n_kinetic) + "}/Q_{GB}")
+elif n_moment == 2:
+    ax.set_ylabel("PI_" + str(n_kinetic) + "/PI_{GB}")
+elif n_moment == 3:
+    ax.set_ylabel("S^tur_{W," + str(n_kinetic) + "}/S_{GB}")
 plt.show()
