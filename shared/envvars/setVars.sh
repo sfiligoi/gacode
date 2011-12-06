57a25bb039e96bc0bde5e5ddb8e8e5c5b82e8f63
Srinath Vadlamani <vadlaman@jaguarpf-login6.ccs.ornl.gov>
2011-12-06 15:10:56 -0500

    Better for local repo.

--- shared/envvars/setVars.sh
+++ shared/envvars/setVars.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
-<<<<<<< HEAD
 export GACODE_ROOT=$HOME/gacode
 export GACODE_PLATFORM=XT5
 #export OPT=debug
 source $GACODE_ROOT/shared/bin/gacode_setup
-=======
-export GACODE_ROOT=${HOME}/work/gacode
-export GACODE_PLATFORM=GFORTRAN_OSX_MACPORTS
-export OPT=debug
-source ${GACODE_ROOT}/shared/bin/gacode_setup
-export OMP=true
->>>>>>> 6518498fae12b0e58b802b694e25074f1a75c0ac
