0175a639cbe2add14da7df42309d2a9b769b45b0
Srinath Vadlamani <srinath@txcorp.com>
2012-03-23 12:36:25 -0600

    Fixed restart logic for hdf5 files.
    This now follows the same logic for restart_method/io_control as for the
    ascii output.

--- gyro/src/gyro_do.f90
+++ gyro/src/gyro_do.f90
@@ -326,6 +326,10 @@ subroutine gyro_do
   endif
   if (gkeigen_j_set == 0) then
      if (io_method < 3) call gyro_write_timedata
+     if (io_method > 1) then
+         call gyro_write_timedata_hdf5
+         if (time_skip_wedge > 0) call gyro_write_timedata_wedge_hdf5
+     endif
   endif
 
   !-------------------------------------------------
