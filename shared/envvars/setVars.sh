23235d1aa8c8a6a9707467e40c1163f8c2952077
Srinath Vadlamani <srinath@txcorp.com>
2012-05-17 11:51:34 -0600

    An attempt at using pgi accelerator.

--- shared/envvars/setVars.sh
+++ shared/envvars/setVars.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 export GACODE_ROOT=$HOME/work/gacode
-export GACODE_PLATFORM=GFORTRAN_OSX_TECHX
-export OPT=debug
+export GACODE_PLATFORM=PGFORTRAN_OSX
+export OPT=OPT
 source $GACODE_ROOT/shared/bin/gacode_setup
