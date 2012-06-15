46d69cd0014f2d4d1721d0901f358d74bd066e37
Srinath Vadlamani <srinath@txcorp.com>
2012-06-15 15:41:33 -0600

    Getting CAPs directive processor working

--- shared/envvars/setVars.sh
+++ shared/envvars/setVars.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-export GACODE_ROOT=/ccs/home/vadlaman/gacode-acc
-export GACODE_PLATFORM=JAGUAR_CRAY
-export OPT=OPT
+export GACODE_ROOT=/Users/srinath/work/gacode
+export GACODE_PLATFORM=GFORTRAN_OSX_MACPORTS
+export OPT=debug
 source $GACODE_ROOT/shared/bin/gacode_setup
