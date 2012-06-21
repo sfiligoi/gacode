574425531d36fca8add8bf73b5f338b87e23b774
Jeff Candy <jcandy@san.rr.com>
2012-06-21 09:28:58 -0700

    Change plot dimensions.

--- shared/python/pyrats/tgyro/oneplot.py
+++ shared/python/pyrats/tgyro/oneplot.py
@@ -9,10 +9,8 @@ ftype = sys.argv[3]
 
 GFONTSIZE=18
 #======================================
-fig = plt.figure(figsize=(18,6))
+fig = plt.figure(figsize=(12,10))
 fig.suptitle(sys.argv[1])
-fig.subplots_adjust(left=0.05,right=0.95)
-fig.subplots_adjust(wspace=0.3)
 #=====================================
 
 n = sim.n_iterations
@@ -20,6 +18,7 @@ n = sim.n_iterations
 ax = fig.add_subplot(111)
 ax.set_ylabel(f,color='k',fontsize=GFONTSIZE)
 ax.set_xlabel('$r/a$',fontsize=GFONTSIZE)
+
 for i in range(n+1):
     ax.plot(sim.data['r/a'][i],sim.data[f][i],label=str(i))
 
