e1b37c748df52664bb231238dc0083c1e12a763a
Srinath Vadlamani <srinath@txcorp.com>
2012-07-02 16:30:10 -0600

    compiles, but who knows
    I have not ran this yet because need to init correctly.

--- gyro/src/gyro_write_h5mesh.f90
+++ gyro/src/gyro_write_h5mesh.f90
@@ -13,12 +13,13 @@
     real, dimension(:,:,:,:), allocatable :: buffer
     real, dimension(:,:,:), allocatable :: bufferMesh
     real :: theta,rmajc,zmagc,kappac,deltac,zetac,r_c,xdc
-    integer :: iphi,ix,j,ncoarse,nphi
+    integer :: iphi,ncoarse,nphi,nwedge
 
     character(60) :: description
     character(128) :: dumpfile
-`
+
     integer(HID_T) :: gridFileID,gridGroupID
+    integer(HID_T) :: poloidalGridID, threeDGridID, wedgeGridID 
     type(hdf5InOpts) :: h5in
     type(hdf5ErrorType) :: h5err
     integer :: number_label
@@ -28,14 +29,13 @@
   !wedge w/ duplicates **CLEAN UP**
     real, dimension(:,:), allocatable :: Rf,Zf
     real, dimension(:,:,:), allocatable :: bufferwedgeMesh
-    real :: rmajc,zmagc,kappac,deltac,zetac,xdc,rhoc
+    real :: rhoc
     real :: dRdr,dZdr,dkappadr,ddeltadr,dzetadr 
-    real, dimension (:), allocatable :: theta,r_c
+    real, dimension (:), allocatable :: w_theta,w_r_c,zeta_phi
     real, dimension (:,:), allocatable :: dRdrho,DRdtheta
     real, dimension (:,:), allocatable :: dZdrho,DZdtheta
     real, dimension (:,:), allocatable :: rtJacobian
     real :: zeta_wedge
-    integer :: iphi,ix,j,ncoarse,nwedge
 
 
     !---------------------------------------
@@ -46,12 +46,20 @@
     ! 1) open grid file 
          dumpfile=TRIM(path)//"gyroMesh.h5" 
          description=""
-         call open_h5file(trim(openmethod),dumpfile,gridFileID,description, &
+         call open_h5file("append",dumpfile,gridFileID,description, &
             gridGroupID,h5in,h5err)
          if (h5err%errBool) call catch_error(h5err%errorMsg)
 
 
-    ! 2) add poloidal group
+      !1.1) make groups
+         call make_mesh_group(gridGroupID,poloidalGridID,h5in,"poloidalMesh",&
+              h5err)
+         call make_mesh_group(gridGroupID,threeDGridID,h5in,"threeDMesh",&
+              h5err)
+         call make_mesh_group(gridGroupID,wedgeGridID,h5in,"wedgeMesh",&
+              h5err)
+ 
+
     ! 3) calculate poloidal coordinates
     ! 4) write poloidal coordinates
     ! 2) add 3d group
@@ -102,11 +110,11 @@
     !---------------------------------------- 
 
     h5in%units=""
-    call dump_h5(dumpGid,'Rgyro',Rc,h5in,h5err)
-    call dump_h5(dumpGid,'Zgyro',Zc,h5in,h5err)
+    call dump_h5(poloidalGridID,'Rgyro',Rc,h5in,h5err)
+    call dump_h5(poloidalGridID,'Zgyro',Zc,h5in,h5err)
     h5in%units="m"
-    call dump_h5(dumpGid,'R',Rc*a_meters,h5in,h5err)
-    call dump_h5(dumpGid,'Z',Zc*a_meters,h5in,h5err)
+    call dump_h5(poloidalGridID,'R',Rc*a_meters,h5in,h5err)
+    call dump_h5(poloidalGridID,'Z',Zc*a_meters,h5in,h5err)
 
     ! Here we do not repeat the points since this is the grid
     ! that will be used for the mode plots on thete E [0,2 pi)
@@ -115,7 +123,7 @@
     bufferMesh(:,:,2)= Zc*a_meters
     h5in%units="m"
     h5in%mesh="mesh-structured"
-    call dump_h5(dumpGid,'cartMesh',bufferMesh(:,:,:),h5in,h5err)
+    call dump_h5(poloidalGridID,'cartMesh',bufferMesh(:,:,:),h5in,h5err)
     h5in%mesh=" "
     deallocate(bufferMesh)
 
@@ -145,12 +153,12 @@
      endif
 
      h5in%units="m"
-     call dump_h5(gid3d,'R',Rc*a_meters,h5in,h5err)
-     call dump_h5(gid3d,'Z',Zc*a_meters,h5in,h5err)
+     call dump_h5(threeDGridID,'R',Rc*a_meters,h5in,h5err)
+     call dump_h5(threeDGridID,'Z',Zc*a_meters,h5in,h5err)
      h5in%units="radians"
-     call dump_h5(gid3d,'torAngle',zeta_phi,h5in,h5err)
-     call dump_h5(gid3d,'torangle_offset',torangle_offset,h5in,h5err)
-     call dump_h5(gid3d,'alpha',alpha_phi,h5in,h5err)
+     call dump_h5(threeDGridID,'torAngle',zeta_phi,h5in,h5err)
+     call dump_h5(threeDGridID,'torangle_offset',torangle_offset,h5in,h5err)
+     call dump_h5(threeDGridID,'alpha',alpha_phi,h5in,h5err)
 
      allocate(buffer(0:ncoarse,n_x,n_torangle_3d,3))
       buffer = -9999.999
@@ -174,149 +182,143 @@
 
 
      h5in%units="m"; h5in%mesh="mesh-structured"
- !    call dump_h5_4d(gid3d,'cartMesh',buffer*a_meters,h5in,h5err)
+ !    call dump_h5_4d(threeDGridID,'cartMesh',buffer*a_meters,h5in,h5err)
 ! This is what I want for the external linking.
-!     call dump_h5(gid3d,'cartMesh',figGrid,gidGrid,'threeDMesh',buffer*a_meters,h5in,h5err)
-     call dump_h5(gid3d,'cartMesh',buffer*a_meters,h5in,h5err)
+!     call dump_h5(threeDGridID,'cartMesh',figGrid,gidGrid,'threeDMesh',buffer*a_meters,h5in,h5err)
+     call dump_h5(threeDGridID,'cartMesh',buffer*a_meters,h5in,h5err)
      deallocate(buffer)
     endif
 
-    !----------------------------------------
-    ! Dump the wedge mesh(es)
-    !---------------------------------------- 
     if (allocated(Rc)) deallocate(Rc)
     if (allocated(Zc)) deallocate(Zc)
     if (allocated(zeta_phi)) deallocate(zeta_phi)
 
 !----------------------dump wedge grid ------------------
 
-   wedge: if(time_skip_wedge > 0) then:
+  wedge: if (time_skip_wedge > 0) then
     
-    ncoarse = n_theta_plot
-    nwedge = n_theta_plot*n_theta_mult
-    allocate(Rf(1:nwedge,n_x), Zf(1:nwedge,n_x))
-    allocate(theta(1:nwedge), r_c(1:n_x))
+      ncoarse = n_theta_plot
+      nwedge = n_theta_plot*n_theta_mult
+      allocate(Rf(1:nwedge,n_x), Zf(1:nwedge,n_x))
+      allocate(w_theta(1:nwedge), w_r_c(1:n_x))
 
-    allocate(dRdrho(1:nwedge,1:n_x))
-    allocate(dZdrho(1:nwedge,1:n_x))
-    allocate(dRdtheta(1:nwedge,1:n_x))
-    allocate(dZdtheta(1:nwedge,1:n_x))
-    allocate(rtJacobian(1:nwedge,1:n_x))
-
-
-
-        !----------------------------------------
-    ! Calculate the R,Z coordinates.  See write_geometry_arrays.f90
-    !---------------------------------------- 
-
-    do ix=1,n_x
-       if (flat_profile_flag == 0) then
-          r_c(ix)=r_s(ix)
-       else
-          r_c(ix)=r(ix)
-       endif
-    enddo
-    do j=1,nwedge
-       ! This needs to match up with what's in gyro_set_blend_arrays.f90
-       theta(j)=theta_wedge_offset+real(j-1)*theta_wedge_angle/       &
-            real(n_theta_plot*n_theta_mult-1)
-       !theta = -pi+REAL(j)*pi*2./REAL(nwedge)
-    enddo
-
-    do ix=1,n_x
-       rhoc     = r_c(ix)
-       rmajc  = rmaj_s(ix)
-       dRdr   = drmaj_s(ix)
-       zmagc  = zmag_s(ix)
-       dZdr   = dzmag_s(ix)
-       kappac = kappa_s(ix)
-       dkappadr = (kappac/rhoc)*s_kappa_s(ix)
-       deltac   = delta_s(ix)
-       ddeltadr = (1./rhoc)*s_delta_s(ix)
-       xdc    = asin(deltac)
-       zetac  = zeta_s(ix)
-       dzetadr = (1./rhoc)*s_zeta_s(ix)
-
-       do j=1,nwedge
-          if(radial_profile_method==1) then
-             Rf(j,ix)=rmajc+rhoc*cos(theta(j))
-             Zf(j,ix)=zmagc+rhoc*sin(theta(j))
-          else
-             Rf(j,ix)=rmajc+rhoc*cos(theta(j)+xdc*sin(theta(j)))
-             Zf(j,ix)=zmagc+kappac*rhoc*sin(theta(j)+zetac*sin(2.*theta(j)))
-          endif
-          !pieces of jacobian 
-          dRdrho(j,ix) = COS(theta(j)+xdc*SIN(theta(j))) + &
-               dRdr - (1./SQRT(1-deltac**2))* &
-               rhoc*SIN(theta(j))*SIN(theta(j)+xdc*SIN(theta(j)))*ddeltadr
-
-          dZdrho(j,ix) = SIN(theta(j)+SIN(2.*theta(j))*zetac*kappac) + &
-               dZdr + rhoc*COS(theta(j)+SIN(2.*theta(j))*zetac)*SIN(2.*theta(j))* &
-               kappac*dzetadr + &
-               rhoc*SIN(theta(j)+SIN(2.*theta(j))*zetac)*dkappadr
-
-          dRdtheta(j,ix) = -1.*rhoc*(1.+xdc*COS(theta(j)))* &
-               SIN(theta(j)+xdc)*SIN(theta(j))
-
-          dZdtheta(j,ix)= rhoc*COS(theta(j)+SIN(2.*theta(j))*zetac) * &
-               (1.+2.*COS(2*theta(j))*zetac*kappac)
-
-          rtJacobian(j,ix) = dRdrho(j,ix)*dZdtheta(j,ix) - &
-               dZdrho(j,ix)*dRdtheta(j,ix)
-
-       enddo
-    enddo
-
-    !-------------------------------------------------------
-    ! Set up the alpha grid
-    ! These are saved in a module so no need to recalculate
-    !-------------------------------------------------------
-
-    if (.not. allocated(alpha_phi_wedge) ) then
-       allocate(alpha_phi_wedge(nwedge,n_x,n_torangle_wedge))
-       do iphi=1,n_torangle_wedge
-          !Don't store zeta_wedge b/c analysis is on a plane by plane basis
-          zeta_wedge=REAL(iphi-1)/REAL(n_torangle_wedge)*2.*pi
-          alpha_phi_wedge(:,:,iphi)=torangle_offset+zeta_wedge+nu_wedge(:,:)
-       end do
-    endif
+      allocate(dRdrho(1:nwedge,1:n_x))
+      allocate(dZdrho(1:nwedge,1:n_x))
+      allocate(dRdtheta(1:nwedge,1:n_x))
+      allocate(dZdtheta(1:nwedge,1:n_x))
+      allocate(rtJacobian(1:nwedge,1:n_x))
 
 
-    !----------------------------------------
-    ! Dump the wedge meshes
-    !---------------------------------------- 
 
-    h5in%units=""
-    call dump_h5(gidwedge,'Rgyro',Rf,h5in,h5err)
-    call dump_h5(gidwedge,'Zgyro',Zf,h5in,h5err)
-    call dump_h5(gidwedge,'torangle_offset',torangle_offset,h5in,h5err)
-    call dump_h5(gidwedge,'alpha',alpha_phi_wedge,h5in,h5err)
-    h5in%units="m"
-    call dump_h5(gidwedge,'R',Rf*a_meters,h5in,h5err)
-    call dump_h5(gidwedge,'Z',Zf*a_meters,h5in,h5err)
-    call dump_h5(gidwedge,'r_min',r_c*a_meters,h5in,h5err)
-    call dump_h5(gidwedge,'rtJacobian',rtJacobian*a_meters,h5in,h5err)
-    h5in%units="radians"
-    call dump_h5(gidwedge,'theta',theta,h5in,h5err)
+          !----------------------------------------
+      ! Calculate the R,Z coordinates.  
+      !---------------------------------------- 
 
-    ! For ease of use, have a single data set that has R,Z. 
-    allocate(bufferwedgeMesh(nwedge,n_x,2))
-    bufferwedgeMesh(:,:,1) = Rf(:,:)*a_meters
-    bufferwedgeMesh(:,:,2) = Zf(:,:)*a_meters
-    h5in%units="m"
-    h5in%mesh="mesh-structured"
-    call dump_h5(gidwedge,'cartMesh',bufferwedgeMesh(:,:,:),h5in,h5err)
-    h5in%mesh=""
-    deallocate(bufferwedgeMesh)
+      do ix=1,n_x
+         if (flat_profile_flag == 0) then
+            w_r_c(ix)=r_s(ix)
+         else
+            w_r_c(ix)=r(ix)
+         endif
+      enddo
+      do j=1,nwedge
+         ! This needs to match up with what's in gyro_set_blend_arrays.f90
+         w_theta(j)=theta_wedge_offset+real(j-1)*theta_wedge_angle/       &
+              real(n_theta_plot*n_theta_mult-1)
+         !theta = -pi+REAL(j)*pi*2./REAL(nwedge)
+      enddo
 
-    !----------------------------------------
-    ! 
-    !---------------------------------------- 
-    deallocate(Rf, Zf)
+      do ix=1,n_x
+         rhoc     = w_r_c(ix)
+         rmajc  = rmaj_s(ix)
+         dRdr   = drmaj_s(ix)
+         zmagc  = zmag_s(ix)
+         dZdr   = dzmag_s(ix)
+         kappac = kappa_s(ix)
+         dkappadr = (kappac/rhoc)*s_kappa_s(ix)
+         deltac   = delta_s(ix)
+         ddeltadr = (1./rhoc)*s_delta_s(ix)
+         xdc    = asin(deltac)
+         zetac  = zeta_s(ix)
+         dzetadr = (1./rhoc)*s_zeta_s(ix)
+
+         do j=1,nwedge
+            if(radial_profile_method==1) then
+               Rf(j,ix)=rmajc+rhoc*cos(w_theta(j))
+               Zf(j,ix)=zmagc+rhoc*sin(w_theta(j))
+            else
+               Rf(j,ix)=rmajc+rhoc*cos(w_theta(j)+xdc*sin(w_theta(j)))
+               Zf(j,ix)=zmagc+kappac*rhoc*sin(w_theta(j)+zetac*sin(2.*w_theta(j)))
+            endif
+            !pieces of jacobian 
+            dRdrho(j,ix) = COS(w_theta(j)+xdc*SIN(w_theta(j))) + &
+                 dRdr - (1./SQRT(1-deltac**2))* &
+                 rhoc*SIN(w_theta(j))*SIN(w_theta(j)+xdc*SIN(w_theta(j)))*ddeltadr
+
+            dZdrho(j,ix) = SIN(w_theta(j)+SIN(2.*w_theta(j))*zetac*kappac) + &
+                 dZdr + rhoc*COS(w_theta(j)+SIN(2.*w_theta(j))*zetac)*SIN(2.*w_theta(j))* &
+                 kappac*dzetadr + &
+                 rhoc*SIN(w_theta(j)+SIN(2.*w_theta(j))*zetac)*dkappadr
+
+            dRdtheta(j,ix) = -1.*rhoc*(1.+xdc*COS(w_theta(j)))* &
+                 SIN(w_theta(j)+xdc)*SIN(w_theta(j))
+
+            dZdtheta(j,ix)= rhoc*COS(w_theta(j)+SIN(2.*w_theta(j))*zetac) * &
+                 (1.+2.*COS(2*w_theta(j))*zetac*kappac)
+
+            rtJacobian(j,ix) = dRdrho(j,ix)*dZdtheta(j,ix) - &
+                 dZdrho(j,ix)*dRdtheta(j,ix)
+
+         enddo
+      enddo
+
+      !-------------------------------------------------------
+      ! Set up the alpha grid
+      ! These are saved in a module so no need to recalculate
+      !-------------------------------------------------------
+
+      if (.not. allocated(alpha_phi_wedge) ) then
+         allocate(alpha_phi_wedge(nwedge,n_x,n_torangle_wedge))
+         do iphi=1,n_torangle_wedge
+            !Don't store zeta_wedge b/c analysis is on a plane by plane basis
+            zeta_wedge=REAL(iphi-1)/REAL(n_torangle_wedge)*2.*pi
+            alpha_phi_wedge(:,:,iphi)=torangle_offset+zeta_wedge+nu_wedge(:,:)
+         end do
+      endif
 
 
-    deallocate(theta, dRdrho,dZdrho,dRdtheta,dZdtheta)
+      !----------------------------------------
+      ! Dump the wedge meshes
+      !---------------------------------------- 
 
+      h5in%units=""
+      call dump_h5(wedgeGridID,'Rgyro',Rf,h5in,h5err)
+      call dump_h5(wedgeGridID,'Zgyro',Zf,h5in,h5err)
+      call dump_h5(wedgeGridID,'torangle_offset',torangle_offset,h5in,h5err)
+      call dump_h5(wedgeGridID,'alpha',alpha_phi_wedge,h5in,h5err)
+      h5in%units="m"
+      call dump_h5(wedgeGridID,'R',Rf*a_meters,h5in,h5err)
+      call dump_h5(wedgeGridID,'Z',Zf*a_meters,h5in,h5err)
+      call dump_h5(wedgeGridID,'r_min',w_r_c*a_meters,h5in,h5err)
+      call dump_h5(wedgeGridID,'rtJacobian',rtJacobian*a_meters,h5in,h5err)
+      h5in%units="radians"
+      call dump_h5(wedgeGridID,'w_theta',w_theta,h5in,h5err)
+
+      ! For ease of use, have a single data set that has R,Z. 
+      allocate(bufferwedgeMesh(nwedge,n_x,2))
+      bufferwedgeMesh(:,:,1) = Rf(:,:)*a_meters
+      bufferwedgeMesh(:,:,2) = Zf(:,:)*a_meters
+      h5in%units="m"
+      h5in%mesh="mesh-structured"
+      call dump_h5(wedgeGridID,'cartMesh',bufferwedgeMesh(:,:,:),h5in,h5err)
+      h5in%mesh=""
+      deallocate(bufferwedgeMesh)
+
+      !----------------------------------------
+      ! 
+      !---------------------------------------- 
+      deallocate(Rf, Zf)
+      deallocate(w_theta, dRdrho,dZdrho,dRdtheta,dZdtheta)
 
     endif wedge
 
