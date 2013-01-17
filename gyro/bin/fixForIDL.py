92a45dba89968c679f5551b56d61f110b49d29d8
Jeff Candy <jcandy@san.rr.com>
2013-01-16 23:50:05 -0800

    Regression reset.  Also removed some kind of IDL HDF5 file.

--- gyro/bin/fixForIDL.py
+++
@@ -1,7 +0,0 @@
-import tables
-a=tables.openFile("gyro05800.h5","a")
-b=tables.openFile("gyroMesh.h5","r")
-a.removeNode(a.root.coarseMesh)
-a.copyNode(b.root.coarseMesh,a.root,recursive=True)
-a.close()
-b.close()
