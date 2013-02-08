b0f582c0f19bd667b883e4014c4879b330587524
Jeff Candy <jcandy@san.rr.com>
2013-02-07 23:45:57 -0800

    Plotting-related tweaks.  Synchronizing files.

--- shared/EXPRO/EXPRO_write_derived.f90
+++ shared/EXPRO/EXPRO_write_derived.f90
@@ -27,22 +27,20 @@ subroutine EXPRO_write_derived
   write(io,10) transpose(EXPRO_dlntidr(:,:)) ! 15-19
   write(io,10) EXPRO_dlnptotdr(:)! 20 
   write(io,10) EXPRO_drdrho(:)   ! 21
-  write(io,10) EXPRO_w0(:)       ! 22
-  write(io,10) EXPRO_w0p(:)      ! 23
-  write(io,10) EXPRO_vol(:)      ! 24
-  write(io,10) EXPRO_volp(:)     ! 25
-  write(io,10) EXPRO_cs(:)       ! 26
-  write(io,10) EXPRO_rhos(:)     ! 27
-  write(io,10) EXPRO_ni_new(:)   ! 28
-  write(io,10) EXPRO_dlnnidr_new(:) ! 29 
-  write(io,10) EXPRO_grad_r0(:)     ! 30
-  write(io,10) EXPRO_ave_grad_r(:)  ! 31 
-  write(io,10) EXPRO_bp0(:)      ! 32
-  write(io,10) EXPRO_bt0(:)      ! 33
-  write(io,10) EXPRO_poloidalfluxover2pi(:) ! 34
-  write(io,10) EXPRO_gamma_e(:)  ! 35
-  write(io,10) EXPRO_gamma_p(:)  ! 36
-  write(io,10) EXPRO_mach(:)     ! 37
+  write(io,10) EXPRO_w0p(:)      ! 22
+  write(io,10) EXPRO_vol(:)      ! 23
+  write(io,10) EXPRO_volp(:)     ! 24
+  write(io,10) EXPRO_cs(:)       ! 25
+  write(io,10) EXPRO_rhos(:)     ! 26
+  write(io,10) EXPRO_ni_new(:)   ! 27
+  write(io,10) EXPRO_dlnnidr_new(:) ! 28 
+  write(io,10) EXPRO_grad_r0(:)     ! 29
+  write(io,10) EXPRO_ave_grad_r(:)  ! 30 
+  write(io,10) EXPRO_bp0(:)      ! 31
+  write(io,10) EXPRO_bt0(:)      ! 32
+  write(io,10) EXPRO_gamma_e(:)  ! 33
+  write(io,10) EXPRO_gamma_p(:)  ! 34
+  write(io,10) EXPRO_mach(:)     ! 35
 
   close(io)
 
