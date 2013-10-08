8d73759c779e4525ed6be4f4b66c4c5e545a9e36
amollen <mollen@nephy.chalmers.se>
2013-10-08 22:24:29 +0200

    Changes to gyro_linmap tool

--- gyro/bin/gyro_linmap_parse.py
+++ gyro/bin/gyro_linmap_parse.py
@@ -12,6 +12,17 @@ x.add('DET_TOLERANCE','1e-03')
 x.add('ERROR_TOLERANCE','1e-03')
 x.add('WI_STABLE_LIMIT','1e-02')
 
+x.add('startWR','-2.0E-01')
+x.add('startWI','2.0E-01')
+x.add('RADIUSstart','0.5')
+x.add('RADIUSmin','0.4')
+x.add('RADIUSmax','0.6')
+x.add('RADIUSstep','0.05')
+x.add('L_Ystart','0.5')
+x.add('L_Ymin','0.4')
+x.add('L_Ymax','0.6')
+x.add('L_Ystep','0.05')
+
 # Deprecated parameters
 
 x.dep('MYPARAM','Deprecated.')
