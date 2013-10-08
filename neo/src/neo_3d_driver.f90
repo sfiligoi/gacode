3f26427bda72afc5070521d1d9d5213d428e6125
bellie <bellie@fusion.gat.com>
2013-10-08 12:24:11 -0700

    first version of neo 3d

--- neo/src/neo_3d_driver.f90
+++ neo/src/neo_3d_driver.f90
@@ -3,15 +3,15 @@ module neo_3d_driver
 
   public :: ThreeD_do
 
-  integer, parameter,private :: io_neo=10, io_f=11
   character(len=80),private  :: runfile_f = 'out.neo.f_3d'
-  integer, dimension(:,:,:,:,:), allocatable, private :: mindx 
+  character(len=80),private  :: runfile_transp = 'out.neo.transport_3d'
+  integer, dimension(:,:,:,:), allocatable, private :: mindx 
   integer, private :: stat
   integer, private :: tpmatsize
   integer, private :: n_tptheta, n_tpvarphi, indx_c00
   real, dimension(:,:), allocatable, private :: tpmat_trap, tpmat_stream_dt, &
        tpmat_stream_dp
-  real, dimension(:,:), allocatable, private :: tpvec_vdriftx, tpvec_flux, &
+  real, dimension(:), allocatable, private :: tpvec_vdriftx, tpvec_flux, &
        tpvec_uparB, tpvec_upar, tpvec_fsa
   ! transport coefficients
   real, dimension(:), allocatable, private :: pflux, eflux, uparB
@@ -52,8 +52,8 @@ contains
        call neo_error('ERROR: (NEO) le3 files not available')
        goto 200
     endif
-    do i=1,matsize
-       do j=1, matsize
+    do i=1,tpmatsize
+       do j=1, tpmatsize
           read(1,*) tpmat_trap(i,j), tpmat_stream_dt(i,j), tpmat_stream_dp(i,j)
        enddo
     enddo
@@ -63,33 +63,31 @@ contains
     allocate(tpvec_uparB(tpmatsize))
     allocate(tpvec_upar(tpmatsize))
     allocate(tpvec_fsa(tpmatsize))
-    open(unit=1,file='out.le3.geomatrix',status='old',iostat=stat)
+    open(unit=1,file='out.le3.geovector',status='old',iostat=stat)
     if(stat .ne. 0) then
        call neo_error('ERROR: (NEO) le3 files not available')
        goto 200
     endif
-    do i=1,matsize
-       do j=1, matsize
-          read(1,*) tpmat_trap(i,j), tpmat_stream_dt(i,j), &
-               tpmat_stream_dp(i,j), tpvec_fsa(i,j)
-       enddo
+    do i=1,tpmatsize
+       read(1,*) tpvec_vdriftx(i), tpvec_flux(i), tpvec_uparB(i), &
+            tpvec_upar(i), tpvec_fsa(i)
     enddo
 
     if (silent_flag == 0 .and. i_proc == 0) then
-       open(unit=io_neo,file=trim(path)//'out.neo.grid_3d',status='replace')
-       write(io_neo,*) n_species
-       write(io_neo,*) n_energy
-       write(io_neo,*) n_xi
-       write(io_neo,*) n_tptheta
-       write(io_neo,*) n_tpvarphi
-       write(io_neo,*) n_radial
+       open(unit=1,file=trim(path)//'out.neo.grid_3d',status='replace')
+       write(1,*) n_species
+       write(1,*) n_energy
+       write(1,*) n_xi
+       write(1,*) n_tptheta
+       write(1,*) n_tpvarphi
+       write(1,*) n_radial
        do ir=1, n_radial
-          write(io_neo,*) r(ir)
+          write(1,*) r(ir)
        enddo
-       close(io_neo)
+       close(1)
        
-       open(unit=io_f,file=trim(path)//runfile_f,status='replace')
-       close(io_f)
+       open(unit=1,file=trim(path)//runfile_f,status='replace')
+       close(1)
     end if
 
     do ir=1, n_radial
@@ -101,14 +99,14 @@ contains
     call ENERGY_coll_ints_alloc(0)
     call PROFILE_SIM_alloc(0)
 
-    if allocated(tpmat_trap)           deallocate(tpmat_trap)
-    if allocated(tpmat_stream_dt)      deallocate(tpmat_stream_dt)
-    if allocated(tpmat_stream_dp)      deallocate(tpmat_stream_dp)
-    if allocated(tpvec_vdriftx)        deallocate(tpvec_vdriftx) 
-    if allocated(tpvec_flux)           deallocate(tpvec_flux)
-    if allocated(tpvec_uparB)          deallocate(tpvec_uparB) 
-    if allocated(tpvec_upar)           deallocate(tpvec_upar)
-    if allocated(tpvec_fsa)            deallocate(tpvec_fsa)
+    if (allocated(tpmat_trap))           deallocate(tpmat_trap)
+    if (allocated(tpmat_stream_dt))      deallocate(tpmat_stream_dt)
+    if (allocated(tpmat_stream_dp))      deallocate(tpmat_stream_dp)
+    if (allocated(tpvec_vdriftx))        deallocate(tpvec_vdriftx) 
+    if (allocated(tpvec_flux))           deallocate(tpvec_flux)
+    if (allocated(tpvec_uparB))          deallocate(tpvec_uparB) 
+    if (allocated(tpvec_upar))           deallocate(tpvec_upar)
+    if (allocated(tpvec_fsa))            deallocate(tpvec_fsa)
 
   end subroutine ThreeD_do
 
@@ -120,7 +118,7 @@ contains
     integer, intent(in)  :: ir
     integer, intent(out) :: status
     integer :: is, ie, ix, it, js, je, jx, jt, ks
-    integer :: i, j, k, id
+    integer :: i, j, k
     integer :: ierr
     integer :: n_elem
     real, dimension(:), allocatable :: a
@@ -132,22 +130,22 @@ contains
     status = 0
 
     ! Matrix solve allocations
-    n_row = n_species*(n_energy+1)*(n_xi+1)*tp_matsize
+    n_row = n_species*(n_energy+1)*(n_xi+1)*tpmatsize
     ! Estimate number of elements
     i = 0
     ! constraint
     if(collision_model == 1 .or. collision_model == 2) then
-       i = i + n_species*tp_matsize*(n_energy+1)
+       i = i + n_species*(n_energy+1)
     else
-       i = i + n_species*tp_matsize*2
+       i = i + n_species*2
     endif
     ! collisions
-    i = i + n_species*(n_xi+1)*tp_matsize*(n_energy+1)**2 &
+    i = i + n_species*(n_xi+1)*tpmatsize*(n_energy+1)**2 &
          * (1 + n_species)
     ! streaming (theta and varphi derivs)
-    i = i + 2*n_species*(tp_matsize**2)*(n_energy+1)**2*((n_xi-1)*4*2 + 2*4)
+    i = i + 2*n_species*(tpmatsize**2)*(n_energy+1)**2*((n_xi-1)*4*2 + 2*4)
     ! trapping
-    i = i + n_species*(tp_matsize**2)*(n_energy+1)**2*((n_xi-1)*2 + 2)
+    i = i + n_species*(tpmatsize**2)*(n_energy+1)**2*((n_xi-1)*2 + 2)
     n_max = i
     allocate(a(n_max),stat=ierr)
     if(ierr /= 0) then
@@ -168,7 +166,7 @@ contains
        goto 100
     end if
     allocate(g(n_row))
-    allocate(mindx(n_species,0:n_energy,0:n_xi,tp_matsize))
+    allocate(mindx(n_species,0:n_energy,0:n_xi,tpmatsize))
     allocate(is_indx(n_row))
     allocate(ie_indx(n_row))
     allocate(ix_indx(n_row))
@@ -179,7 +177,7 @@ contains
     do is=1,n_species
        do ie=0,n_energy
           do ix=0,n_xi
-             do it=1,n_tp_matsize
+             do it=1,tpmatsize
                 i = i+1
                 mindx(is,ie,ix,it) = i
                 is_indx(i) = is
@@ -220,11 +218,11 @@ contains
     a_jindx(:) = 0
     a(:) = 0.0
     k = 0
-    
+
     do is=1,n_species
        do ie=0,n_energy
           do ix=0,n_xi
-             do it=1,n_tpmatsize
+             do it=1,tpmatsize
                 
                 i = mindx(is,ie,ix,it)
                 
@@ -236,10 +234,10 @@ contains
                      then
                    ! <f_ie> = 0
                    js = is; je = ie; jx=0
-                   do jt=1, n_tpmatsiz
+                   do jt=1, tpmatsize
                       j = mindx(js,je,jx,jt)
                       k = k+1
-                      a(k) = vec_fsa(jt)
+                      a(k) = tpvec_fsa(jt)
                       a_iindx(k) = i
                       a_jindx(k) = j
                    enddo
@@ -250,10 +248,10 @@ contains
                      collision_model==4 .or. collision_model==5)) then
                    ! <f_ie> = 0
                    js = is; je = ie; jx=0
-                   do jt=1, n_tpmatsize
+                   do jt=1, tpmatsize
                       j = mindx(js,je,jx,jt)
                       k = k+1
-                      a(k) = vec_fsa(jt)
+                      a(k) = tpvec_fsa(jt)
                       a_iindx(k) = i
                       a_jindx(k) = j
                    enddo
@@ -293,7 +291,7 @@ contains
                    fac = sqrt(2.0) * vth(is,ir)
                    js = is
                    do je=0, n_energy
-                      do jt=1,n_tpmatsize
+                      do jt=1,tpmatsize
                          jx = ix-1
                          if (jx >= 0) then
                             j = mindx(js,je,jx,jt)
@@ -322,7 +320,7 @@ contains
                    ! Streaming -- d/dvarphi
                    js = is
                    do je=0, n_energy
-                      do jt=1,n_tpmatsize
+                      do jt=1,tpmatsize
                          jx = ix-1
                          if (jx >= 0) then
                             j = mindx(js,je,jx,jt)
@@ -352,7 +350,7 @@ contains
                    fac  = sqrt(0.5) * vth(is,ir)
                    js = is
                    do je=0, n_energy
-                      do jt=1,n_tpmatsize
+                      do jt=1,tpmatsize
                          jx = ix-1
                          if (jx >= 0) then
                             j = mindx(js,je,jx,jt)
@@ -385,7 +383,7 @@ contains
     enddo ! is
     
     ! Factor the Matrix -- uses a(:) and a_indx(:)
-    
+
     n_elem = k
     n_max = n_elem*matsz_scalefac
     matfac_err = 0
@@ -473,9 +471,9 @@ contains
     endif
     
     if(silent_flag == 0 .and. i_proc == 0) then
-       open(io_f,file=trim(path)//runfile_f,status='old',position='append')
-       write(io_f,*) g(:)
-       close(io_f)
+       open(1,file=trim(path)//runfile_f,status='old',position='append')
+       write(1,*) g(:)
+       close(1)
     endif
     
     
@@ -518,7 +516,7 @@ contains
        
        do ie=0,n_energy
           do ix=0,n_xi
-             do it=1,n_tpmatsize
+             do it=1,tpmatsize
                    
                 i = mindx(is,ie,ix,it)
                 
@@ -567,12 +565,13 @@ contains
     use neo_energy_grid
     implicit none
     integer, intent(in) :: ir
-    integer :: is
+    integer :: is, ie, ix, it, i
+    real    :: fac_drift, fac_upar
 
     allocate(pflux(n_species))
     allocate(eflux(n_species))
     allocate(uparB(n_species))
-    allocate(upar(n_species,n_tpmatsize))
+    allocate(upar(n_species,tpmatsize))
 
     pflux(:)  = 0.0
     eflux(:)  = 0.0
@@ -580,7 +579,7 @@ contains
     upar(:,:) = 0.0
 
     ! Compute the transport coefficients here
-    !!!!!!!!!!!!!!! EAB : not complete
+
     do i=1,n_row
 
        is = is_indx(i)
@@ -588,20 +587,76 @@ contains
        ix = ix_indx(i)
        it = it_indx(i)
 
+       fac_drift = rho(ir)*mass(is)/(1.0*Z(is)) * (vth(is,ir))**2
+
+       fac_upar  = sqrt(2.0) * vth(is,ir)
+
+       if(ix == 0) then
+
+          pflux(is) = pflux(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (4.0/3.0) * evec_e1(ie,ix) &
+               * fac_drift * tpvec_flux(it)
+
+          eflux(is) = eflux(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (4.0/3.0) * evec_e2(ie,ix) * temp(is,ir) &
+               * fac_drift * tpvec_flux(it)
+
+       else if(ix == 2) then
+          
+          pflux(is) = pflux(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (2.0/15.0) * evec_e1(ie,ix) &
+               * fac_drift * tpvec_flux(it)
+
+          eflux(is) = eflux(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (2.0/15.0) * evec_e2(ie,ix) * temp(is,ir) &
+               * fac_drift * tpvec_flux(it)
+
+       else if(ix == 1) then
+
+          upar(is,it) = upar(is,it) + g(i) &
+               * 4.0/sqrt(pi) * (1.0/3.0) * evec_e05(ie,ix) &
+               * fac_upar * tpvec_upar(it)
+          
+          uparB(is) = uparB(is) + g(i) &
+               * 4.0/sqrt(pi) * (1.0/3.0) * evec_e05(ie,ix) &
+               * fac_upar * tpvec_uparB(it)
+
+       endif
+
     enddo
 
     ! Bootstrap current = sum <Z*n*upar B>
     jpar = 0.0
     do is=1, n_species
-       jpar = jpar + Z(is) * uparB(is) * dens(is)
+       jpar = jpar + Z(is) * uparB(is) * dens(is,ir)
     enddo
 
+    if(silent_flag == 0 .and. i_proc == 0) then
+       if(ir==1) then
+          open(unit=1,file=trim(path)//runfile_transp,status='replace')
+       else
+          open(1,file=trim(path)//runfile_transp,status='old',position='append')
+       endif
+       write (1,'(e16.8)',advance='no') r(ir)
+       write (1,'(e16.8)',advance='no') jpar
+       do is=1, n_species
+          write (1,'(e16.8)',advance='no') pflux(is)
+          write (1,'(e16.8)',advance='no') eflux(is)
+          write (1,'(e16.8)',advance='no') uparB(is)
+       enddo
+       write (1,*)
+       close(1)
+    endif
 
     deallocate(pflux)
     deallocate(eflux)
     deallocate(uparB)
     deallocate(upar)
-
+    
   end subroutine threed_transport
 
 end module neo_3d_driver
