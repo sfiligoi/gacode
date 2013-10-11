bcbffe880d20a80a8c246640a5802cf9edca8e50
bellie <bellie@fusion.gat.com>
2013-10-11 16:38:16 -0700

    ntv added to 3d neo

--- neo/src/neo_3d_driver.f90
+++ neo/src/neo_3d_driver.f90
@@ -12,9 +12,10 @@ module neo_3d_driver
   real, dimension(:,:), allocatable, private :: tpmat_trap, tpmat_stream_dt, &
        tpmat_stream_dp, tpmat_coll
   real, dimension(:), allocatable, private :: tpvec_vdriftx, tpvec_flux, &
-       tpvec_uparB, tpvec_upar, tpvec_fsa, tpvec_thetabar
+       tpvec_uparB, tpvec_upar, tpvec_fsa, tpvec_thetabar, tpvec_bmag, &
+       tpvec_ntv
   ! transport coefficients
-  real, dimension(:), allocatable, private :: pflux, eflux, uparB
+  real, dimension(:), allocatable, private :: pflux, eflux, uparB, ntv
   real, dimension(:,:), allocatable, private :: upar
   real, private :: jpar
 
@@ -66,6 +67,8 @@ contains
     allocate(tpvec_uparB(tpmatsize))
     allocate(tpvec_upar(tpmatsize))
     allocate(tpvec_fsa(tpmatsize))
+    allocate(tpvec_bmag(tpmatsize))
+    allocate(tpvec_ntv(tpmatsize))
     open(unit=1,file='out.le3.geovector',status='old',iostat=stat)
     if(stat .ne. 0) then
        call neo_error('ERROR: (NEO) le3 files not available')
@@ -73,7 +76,8 @@ contains
     endif
     do i=1,tpmatsize
        read(1,*) tpvec_thetabar(i), tpvec_vdriftx(i), tpvec_flux(i), &
-            tpvec_uparB(i), tpvec_upar(i), tpvec_fsa(i)
+            tpvec_uparB(i), tpvec_upar(i), tpvec_fsa(i), tpvec_bmag(i), &
+            tpvec_ntv(i)
     enddo
 
     if (silent_flag == 0 .and. i_proc == 0) then
@@ -112,6 +116,8 @@ contains
     if (allocated(tpvec_uparB))          deallocate(tpvec_uparB) 
     if (allocated(tpvec_upar))           deallocate(tpvec_upar)
     if (allocated(tpvec_fsa))            deallocate(tpvec_fsa)
+    if (allocated(tpvec_bmag))           deallocate(tpvec_bmag)
+    if (allocated(tpvec_ntv))            deallocate(tpvec_ntv)
 
   end subroutine ThreeD_do
 
@@ -587,11 +593,13 @@ contains
     allocate(eflux(n_species))
     allocate(uparB(n_species))
     allocate(upar(n_species,tpmatsize))
+    allocate(ntv(n_species))
 
     pflux(:)  = 0.0
     eflux(:)  = 0.0
     uparB(:)  = 0.0
     upar(:,:) = 0.0
+    ntv(:)    = 0.0
 
     ! Compute the transport coefficients here
 
@@ -618,6 +626,11 @@ contains
                * (4.0/3.0) * evec_e2(ie,ix) * temp(is,ir) &
                * fac_drift * tpvec_flux(it)
 
+          ntv(is) = ntv(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (4.0/3.0) * evec_e1(ie,ix) * mass(is) &
+               * (-0.5) * tpvec_ntv(it)
+
        else if(ix == 2) then
           
           pflux(is) = pflux(is) + g(i) &
@@ -629,6 +642,11 @@ contains
                * 4.0/sqrt(pi) * dens(is,ir) &
                * (2.0/15.0) * evec_e2(ie,ix) * temp(is,ir) &
                * fac_drift * tpvec_flux(it)
+
+          ntv(is) = ntv(is) + g(i) &
+               * 4.0/sqrt(pi) * dens(is,ir) &
+               * (2.0/15.0) * evec_e1(ie,ix) * mass(is) &
+               * (-0.5) * tpvec_ntv(it)
 
        else if(ix == 1) then
 
@@ -662,6 +680,7 @@ contains
           write (1,'(e16.8)',advance='no') pflux(is)
           write (1,'(e16.8)',advance='no') eflux(is)
           write (1,'(e16.8)',advance='no') uparB(is)
+          write (1,'(e16.8)',advance='no') ntv(is)
        enddo
        write (1,*)
        close(1)
@@ -671,6 +690,7 @@ contains
     deallocate(eflux)
     deallocate(uparB)
     deallocate(upar)
+    deallocate(ntv)
     
   end subroutine threed_transport
 
