c7547a00cb38f7b67477874718ba50b90eb3314c
bellie <bellie@fusion.gat.com>
2014-02-06 12:01:35 -0800

    Changed implemenation of inductive electric field

--- neo/src/neo_do.f90
+++ neo/src/neo_do.f90
@@ -688,7 +688,8 @@ contains
 
                    else if(ix == 1) then
                       g(i) = sqrt(2.0) * vth(is,ir) * evec_e05(ie,ix) &
-                           * (1.0*Z(is))/temp(is,ir) * epar0(ir) / Bmag(it) &
+                           * (1.0*Z(is))/temp(is,ir) * epar0(ir) * Bmag(it) &
+                           / Bmag2_avg &
                            - driftxrot2(is,it) &
                            * ( (src_F0_Ln + src_P0 + src_Rot1) &
                            * evec_e05(ie,ix) &
