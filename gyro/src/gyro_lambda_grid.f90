1b9706f41ac97895afda95aa32e9f937de64bdb8
jcandy <jcandy@san.rr.com>
2014-11-14 15:30:56 -0800

    More startup optimizations.

--- gyro/src/gyro_lambda_grid.f90
+++ gyro/src/gyro_lambda_grid.f90
@@ -25,47 +25,59 @@ subroutine gyro_lambda_grid
      return
   endif
 
+  ! Fast startup option for flat profiles
+
   do i=1,n_x
 
-     call gyro_to_geo(i)
+     if (i == 1 .or. flat_profile_flag == 0) then
 
-     call gyro_banana_init(nint_ORB_s)
+        call gyro_to_geo(i)
 
-     ! Passing
+        call gyro_banana_init(nint_ORB_s)
 
-     call gyro_banana_getlambda(lambda_tp(i),lambda_max(i))
+        ! Passing
 
-     ! two regions
+        call gyro_banana_getlambda(lambda_tp(i),lambda_max(i))
 
-     call gyro_banana_integrate_tau(lambda_tp(i),s_tp)
+        ! two regions
 
-     ! s_tp => s at trapped-passing boundary
+        call gyro_banana_integrate_tau(lambda_tp(i),s_tp)
 
-     allocate(s_temp(n_pass))
-     allocate(w_temp(n_pass))
-     call gauss_legendre(0.0,s_tp,s_temp,w_temp,n_pass)
-     s_lambda(1:n_pass) = s_temp
-     w_lambda(i,1:n_pass) = w_temp
-     deallocate(s_temp)
-     deallocate(w_temp)
+        ! s_tp => s at trapped-passing boundary
 
-     allocate(s_temp(n_trap))
-     allocate(w_temp(n_trap))
-     call gauss_legendre(s_tp,1.0,s_temp,w_temp,n_trap)
-     s_lambda(n_pass+1:n_lambda) = s_temp
-     w_lambda(i,n_pass+1:n_lambda) = w_temp
-     deallocate(s_temp)
-     deallocate(w_temp)
+        allocate(s_temp(n_pass))
+        allocate(w_temp(n_pass))
+        call gauss_legendre(0.0,s_tp,s_temp,w_temp,n_pass)
+        s_lambda(1:n_pass) = s_temp
+        w_lambda(i,1:n_pass) = w_temp
+        deallocate(s_temp)
+        deallocate(w_temp)
 
-     do k=1,n_lambda
-        call gyro_banana_s2lambda(s_lambda(k),lambda(i,k))
-     enddo
+        allocate(s_temp(n_trap))
+        allocate(w_temp(n_trap))
+        call gauss_legendre(s_tp,1.0,s_temp,w_temp,n_trap)
+        s_lambda(n_pass+1:n_lambda) = s_temp
+        w_lambda(i,n_pass+1:n_lambda) = w_temp
+        deallocate(s_temp)
+        deallocate(w_temp)
 
-     ! Two signs of velocity
+        do k=1,n_lambda
+           call gyro_banana_s2lambda(s_lambda(k),lambda(i,k))
+        enddo
 
-     do k=1,n_lambda
-        w_lambda(i,k) = 0.5*w_lambda(i,k)
-     enddo
+        ! Two signs of velocity
+
+        do k=1,n_lambda
+           w_lambda(i,k) = 0.5*w_lambda(i,k)
+        enddo
+
+     else
+
+        w_lambda(i,:) = w_lambda(1,:)
+        lambda(i,:)   = lambda(1,:)
+        lambda_max(i) = lambda_max(1)
+
+     endif
 
   enddo ! i
 
