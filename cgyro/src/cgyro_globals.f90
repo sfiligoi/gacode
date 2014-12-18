c93fc3cd17ff1b5188a020c801f766e0dffaf33d
Jeff Candy <jcandy@san.rr.com>
2014-12-18 11:21:50 -0800

    Merged.

---
+++ cgyro/src/cgyro_globals.f90
@@ -0,0 +1,252 @@
+module cgyro_globals
+
+  !---------------------------------------------------------------
+  ! Input parameters:
+  !
+  integer :: n_energy
+  integer :: n_xi
+  integer :: n_theta
+  integer :: n_radial
+  integer :: n_toroidal
+  integer :: n_field
+  integer :: e_max
+  real    :: delta_t
+  real    :: max_time
+  integer :: print_step
+  real    :: freq_tol
+  integer :: restart_write
+  integer :: restart_mode
+  real    :: up_radial
+  integer :: up_radial_n
+  real    :: up_theta
+  real    :: ky
+  integer :: box_size
+  integer :: silent_flag
+  integer :: equilibrium_model
+  integer :: collision_model
+  integer :: collision_mom_restore
+  integer :: collision_ene_restore
+  integer :: collision_ene_diffusion
+  integer :: collision_kperp
+  integer :: zf_test_flag 
+  integer :: nonlinear_flag 
+  real :: te_ade
+  real :: ne_ade
+  real :: masse_ade
+  real :: lambda_debye
+  !
+  ! Geometry input
+  !
+  real :: rmin
+  real :: rmaj
+  real :: q
+  real :: s
+  real :: shift
+  real :: kappa
+  real :: s_kappa
+  real :: delta
+  real :: s_delta
+  real :: zeta
+  real :: s_zeta
+  real :: zmag
+  real :: s_zmag
+  real :: beta_star
+  real :: betae_unit
+  !
+  ! Species parameters
+  !
+  integer :: n_species
+  real :: nu_ee_in
+  integer, dimension(6) :: z
+  real, dimension(6) :: mass
+  real, dimension(6) :: dens
+  real, dimension(6) :: temp
+  real, dimension(6) :: dlnndr
+  real, dimension(6) :: dlntdr
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! MPI variables and pointers
+  ! 
+  integer :: i_err
+  integer :: i_proc
+  integer :: i_proc_1
+  integer :: i_proc_2
+  integer :: n_proc
+  integer :: n_proc_1
+  integer :: n_proc_2
+  integer :: i_group_1
+  integer :: i_group_2
+  integer :: CGYRO_COMM_WORLD
+  integer :: NEW_COMM_1
+  integer :: NEW_COMM_2
+  integer :: nv1,nv2,nc1,nc2
+  integer :: nsplit
+  integer, dimension(:), allocatable :: recv_status
+  !
+  ! Pointers
+  !
+  integer :: nv,iv
+  integer :: nv_loc,iv_loc
+  integer :: nc,ic
+  integer :: nc_loc,ic_loc
+  integer, dimension(:), allocatable :: ie_v
+  integer, dimension(:), allocatable :: ix_v
+  integer, dimension(:), allocatable :: is_v
+  integer, dimension(:), allocatable :: ir_c
+  integer, dimension(:), allocatable :: it_c
+  integer, dimension(:,:), allocatable :: ic_c
+  integer, dimension(:,:,:), allocatable :: iv_v
+  !
+  integer :: n
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! Constants
+  !
+  real, parameter    :: pi   = 3.1415926535897932
+  complex, parameter :: i_c  = (0.0,1.0)
+  real, parameter    :: num1 = 1.0
+  real, parameter    :: num0 = 0.0
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! I/O and error management variables
+  !
+  character(len=80) :: path
+  character(len=18) :: runfile         = 'out.cgyro.run'
+  character(len=18) :: runfile_restart = 'out.cgyro.restart'
+  character(len=18) :: runfile_hb      = 'out.cgyro.hb'
+  character(len=18) :: runfile_grids   = 'out.cgyro.grids'
+  character(len=18) :: runfile_time    = 'out.cgyro.time'
+  character(len=18) :: runfile_freq    = 'out.cgyro.freq'
+  character(len=14), dimension(3)  :: runfile_field = &
+       (/'out.cgyro.phi ','out.cgyro.apar','out.cgyro.bpar'/)
+  character(len=15), dimension(3)  :: runfile_fieldb = &
+       (/'out.cgyro.phib ','out.cgyro.aparb','out.cgyro.bparb'/)
+  character(len=16), dimension(2)  :: runfile_flux = &
+       (/'out.cgyro.flux_n','out.cgyro.flux_e'/)
+  character(len=18), dimension(3)  :: runfile_power = &
+       (/'out.cgyro.pwr_phi ','out.cgyro.pwr_apar','out.cgyro.pwr_bpar'/)
+  integer, parameter :: io=1
+  !
+  ! error checking
+  integer :: error_status = 0
+  character(len=80) :: error_message
+  !
+  integer :: io_control
+  integer :: signal
+  !
+  ! Standard precision for IO 
+  character(len=8) :: fmtstr='(es11.4)'
+  character(len=14) :: fmtstr2='(2(es11.4,1x))'
+  !----------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! Time stepping
+  integer :: i_time
+  integer :: n_time
+  complex :: freq
+  complex :: freq_err
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! Physics variables
+  !
+  integer :: ae_flag
+  integer :: is_ele
+  real :: dens_ele
+  real :: temp_ele
+  real :: mass_ele
+  !
+  real, dimension(6) :: vth  
+  real, dimension(6) :: nu
+  real :: rho
+  real :: k_theta
+  real :: length
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  ! Numerical/work arrays and dimensions
+  !
+  ! Velocity space
+  ! 
+  integer, dimension(:), allocatable :: indx_xi, px
+  real, dimension(:), allocatable :: energy, w_e
+  real, dimension(:), allocatable :: xi, w_xi
+  real, dimension(:,:), allocatable :: xi_deriv_mat, xi_lor_mat
+  real, dimension(:,:), allocatable :: e_deriv1_mat, e_deriv2_mat
+  !
+  ! Parallel streaming
+  !
+  real, dimension(:), allocatable :: theta
+  real, dimension(-2:2) :: uderiv
+  real, dimension(-2:2) :: cderiv
+  integer, dimension(:), allocatable :: thcyc
+  integer, dimension(:,:,:), allocatable :: rcyc
+  integer, dimension(:), allocatable :: pcyc
+  integer, dimension(:), allocatable :: ncyc
+  complex, dimension(:,:,:), allocatable :: dtheta
+  complex, dimension(:,:,:), allocatable :: dtheta_up
+  !
+  ! Distributions
+  !
+  complex, dimension(:,:,:), allocatable :: rhs
+  complex, dimension(:,:), allocatable :: h_x
+  complex, dimension(:,:), allocatable :: h0_x
+  complex, dimension(:,:), allocatable :: psi
+  complex, dimension(:,:,:), allocatable :: f_nl
+  complex, dimension(:,:,:), allocatable :: g_nl
+  complex, dimension(:,:), allocatable :: omega_cap_h
+  complex, dimension(:,:), allocatable :: omega_h
+  complex, dimension(:,:,:), allocatable :: omega_s
+  complex, dimension(:,:), allocatable :: cap_h_c
+  complex, dimension(:,:), allocatable :: cap_h_ct
+  complex, dimension(:,:), allocatable :: cap_h_v
+  complex, dimension(:,:), allocatable :: cap_h_v_prime
+  real, dimension(:,:), allocatable :: j0_c
+  real, dimension(:,:), allocatable :: j0_v
+  !
+  ! Fields
+  !
+  complex, dimension(:,:,:), allocatable :: field
+  complex, dimension(:,:,:), allocatable :: field_loc
+  complex, dimension(:,:,:), allocatable :: field_old
+  complex, dimension(:,:,:), allocatable :: field_old2
+  complex, dimension(:,:,:), allocatable :: field_old3
+  !
+  ! Nonlinear fluxes
+  real, dimension(:,:), allocatable :: flux_loc
+  real, dimension(:,:), allocatable :: flux
+  real, dimension(:,:), allocatable :: power
+  !  
+  ! Work arrays
+  !
+  complex, dimension(:,:), allocatable :: f_balloon
+  real :: field_error
+  !
+  ! LAPACK work arrays 
+  !
+  real, dimension(:), allocatable :: work  
+  integer, dimension(:), allocatable :: i_piv
+  integer :: info
+  !
+  real :: sum_den_h
+  real, dimension(:,:), allocatable :: sum_den_x, sum_cur_x
+  !
+  ! n=0 test variables
+  !
+  real, dimension(:,:,:), allocatable :: hzf, xzf 
+  real, dimension(:), allocatable :: pvec_in, pvec_outr, pvec_outi
+  !---------------------------------------------------------------
+
+  !---------------------------------------------------------------
+  integer :: geo_ny_in
+  real, dimension(8,0:16) :: geo_yin_in
+  !
+  integer :: geo_numeq_flag
+  integer :: geo_ny
+  real, dimension(:,:), allocatable :: geo_yin
+  !---------------------------------------------------------------
+
+end module cgyro_globals
---
+++ cgyro/src/cgyro_write_initdata.f90
@@ -0,0 +1,136 @@
+!-----------------------------------------------------------------
+! cgyro_write_timedata.f90
+!
+! PURPOSE:
+!  Output of time-independent (initial) data.
+!-----------------------------------------------------------------
+
+subroutine cgyro_write_initdata
+
+  use mpi
+  use cgyro_globals
+  use cgyro_equilibrium
+
+  implicit none
+
+  integer :: in,is
+
+  !----------------------------------------------------------------------------
+  ! Runfile to give complete summary to user
+  ! 
+  ! NOTE: Some of this data is reproduced in other data files.
+  !
+  if (silent_flag == 0 .and. i_proc == 0) then
+
+     open(unit=io,file=trim(path)//runfile,status='old',position='append')
+
+     write(io,*)
+     write(io,'(a)') 'n_radial  n_theta   n_species n_energy  n_xi'
+     write(io,'(t1,5(i4,6x))') n_radial,n_theta,n_species,n_energy,n_xi
+
+     write(io,*) 
+     write(io,'(a,f7.2,a,f7.2)') '(Lx,Ly)/rho',length/rho,',',2*pi/ky
+     write(io,*) 
+
+     if (zf_test_flag == 1) then
+        write(io,20) 'ky*rho',0.0
+     else if (n_toroidal == 1) then
+        write(io,20) 'ky*rho',ky
+     else
+        write(io,20) 'min(ky*rho)',(q/rmin)*rho
+        write(io,20) 'max(ky*rho)',(n_toroidal-1)*(q/rmin)*rho
+     endif
+
+     write(io,*) 
+
+     if (zf_test_flag == 1) then
+        write(io,20) 'kx*rho',2*pi*rho/length
+     else
+        write(io,20) 'min(kx*rho)',2*pi*rho/length
+        write(io,20) 'max(kx*rho)',2*pi*rho*(n_radial/2-1)/length
+     endif
+
+     write(io,*) 
+     write(io,20) 'rho',rho
+     write(io,20) 'r/a',rmin
+     write(io,20) 'R/a',rmaj
+     write(io,20) 'q',q
+     write(io,20) 's',s
+     write(io,20) 'shift',shift
+     write(io,20) 'kappa',kappa
+     write(io,20) 's_kappa',s_kappa
+     write(io,20) 'delta',delta
+     write(io,20) 's_delta',s_delta
+     write(io,20) 'zeta',zeta
+     write(io,20) 's_zeta',s_zeta
+     write(io,20) 'zmag',zmag
+     write(io,20) 's_zmag',s_zmag
+
+     write(io,*)
+     write(io,'(a)') &
+          'indx  z    n/n_norm    T/T_norm    m/m_norm     a/Ln        a/Lt        nu'
+     do is=1,n_species
+        write(io,'(t2,i2,2x,i2,2x,6(1pe10.4,2x))') &
+             is,z(is),dens(is),temp(is),mass(is),dlnndr(is),dlntdr(is),nu(is)
+     enddo
+
+     write(io,*)
+
+     close(io)
+
+  endif
+  !----------------------------------------------------------------------------
+
+  !----------------------------------------------------------------------------
+  ! Write the initial equilibrium data
+  !
+  if (silent_flag == 0 .and. i_proc == 0) then
+
+     open(unit=io,file=trim(path)//'out.cgyro.equil',status='replace')
+     write (io,'(e16.8)',advance='no') rmin
+     write (io,'(e16.8)',advance='no') rmaj
+     write (io,'(e16.8)',advance='no') q
+     write (io,'(e16.8)',advance='no') s
+     write (io,'(e16.8)',advance='no') rho
+     write (io,'(e16.8)',advance='no') ky
+     do is=1,n_species
+        write (io,'(e16.8)',advance='no') dens(is)
+        write (io,'(e16.8)',advance='no') temp(is)
+        write (io,'(e16.8)',advance='no') dlnndr(is)
+        write (io,'(e16.8)',advance='no') dlntdr(is)
+        write (io,'(e16.8)',advance='no') nu(is)
+     enddo
+     write (io,*)
+     close(io)
+
+  endif
+  !----------------------------------------------------------------------------
+
+  !----------------------------------------------------------------------------
+  ! Write the initial grid data 
+  !
+  if (silent_flag == 0 .and. i_proc == 0) then
+
+     open(unit=io,file=trim(path)//runfile_grids,status='replace')
+     write(io,'(i4)') n_toroidal
+     write(io,'(i4)') n_species
+     write(io,'(i4)') n_field
+     write(io,'(i4)') n_radial
+     write(io,'(i4)') n_theta
+     write(io,'(i4)') n_energy
+     write(io,'(i4)') n_xi
+     write(io,'(i4)') box_size
+     write(io,'(i4)') px(:)
+     write(io,'(1pe12.5)') theta(:)
+     write(io,'(1pe12.5)') energy(:)
+     write(io,'(1pe12.5)') xi(:)
+     write(io,'(1pe12.5)') transpose(thetab(:,:))
+     write(io,'(1pe12.5)') (rho*q/rmin*in,in=0,n_toroidal-1)
+     close(io)
+
+  endif
+  !----------------------------------------------------------------------------
+
+20 format(t2,a,':',t13,1pe12.4) 
+
+end subroutine cgyro_write_initdata
