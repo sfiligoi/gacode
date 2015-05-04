ac6f6ce6f540165bee1cd0939e77a3e13a1a84c4
Jeff Candy <jcandy@san.rr.com>
2015-05-04 14:24:39 -0700

    Fixes to EXPRO for TGYRO usage.

--- shared/EXPRO/EXPRO_write_original.f90
+++ shared/EXPRO/EXPRO_write_original.f90
@@ -9,36 +9,37 @@
 !   about sign of q is retained in IPCCW and BTCCW.  
 !--------------------------------------------------------
 
-subroutine EXPRO_write_original(tag)
+subroutine EXPRO_write_original(io1,datafile1,io2,datafile2,tag)
 
   use EXPRO_globals
   use EXPRO_interface
 
   implicit none
 
+  integer, intent(in) :: io1,io2
+  character (len=*), intent(in) :: datafile1,datafile2
+  character (len=*), intent(in) :: tag
+
   integer :: ierr
-
-  character(len=*), intent(in) :: tag
   character (len=80) :: line
-
   
-  open(unit=1,file=trim(path)//'input.profiles',status='old')
-  open(unit=2,file=trim(path)//'input.profiles.new',status='replace')
+  open(unit=io1,file=trim(path)//trim(datafile1),status='old')
+  open(unit=io2,file=trim(path)//trim(datafile2),status='replace')
 
-  write(2,'(a)') '# '//tag
-  write(2,'(a)') '# '
+  write(io2,'(a)') '# '//tag
+  write(io2,'(a)') '# '
   do 
-     read(1,'(a)',iostat=ierr) line
+     read(io1,'(a)',iostat=ierr) line
      if (ierr < 0) exit
      if (line(2:4) /= 'rho') then
-        write(2,'(a)') line
+        write(io2,'(a)') line
      else
         exit
      endif
   enddo
-  call EXPRO_write(2)
+  call EXPRO_write(io2)
   
-  close(1)
-  close(2)
+  close(io1)
+  close(io2)
 
 end subroutine EXPRO_write_original
