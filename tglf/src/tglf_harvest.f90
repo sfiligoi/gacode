0323b47c7b9bfe688f6edd7cd487f9e7dbaca464
Orso Meneghini <meneghini@fusion.gat.com>
2015-06-23 22:04:09 -0700

    Improved list and types of harvested TGLF parameters

--- tglf/src/tglf_harvest.f90
+++ tglf/src/tglf_harvest.f90
@@ -1,13 +1,27 @@
   ! Harvest LOCAL INTERFACE variables
-  subroutine tglf_harvest_local()
+  SUBROUTINE tglf_harvest_local()
 
     USE tglf_interface
 
-    integer :: ierr, i
-    character(LEN=65507) :: harvest_sendline
-    character(LEN=2) :: NUM
-    character NUL
+    INTEGER :: ierr, i
+    CHARACTER(LEN=65507) :: harvest_sendline
+    CHARACTER(LEN=2) :: NUM
+    CHARACTER NUL
     PARAMETER(NUL = CHAR(0))
+
+    IF (.NOT.tglf_use_transport_model_in) THEN
+        RETURN
+    ENDIF
+
+    IF (.NOT.tglf_new_eikonal_in) THEN
+        RETURN
+    ENDIF
+
+    IF (tglf_geometry_flag_in .NE. 1 ) THEN
+       WRITE(1,*) 'HARVEST ONLY SUPPORTS MILLER GEOMETRY'
+       CLOSE(1)
+       RETURN
+    ENDIF
 
     ierr=set_harvest_verbose(1)
     ierr=set_harvest_table('TGLF_harvest?'//NUL)
@@ -15,33 +29,27 @@
     ierr=set_harvest_port(32000)
     ierr=init_harvest(harvest_sendline,LEN(harvest_sendline))
 
-    if (tglf_geometry_flag_in == 2 ) then
-       write(1,*) 'HARVEST NOT SUPPORTED FOR FOURIER GEOMETRY'
-       close(1)
-       return
-    endif
-
 !   '#---------------------------------------------------'
 !   '# Control parameters:'
 !   '#---------------------------------------------------'
-    ierr=set_harvest_payload_int(harvest_sendline,'NS'//NUL,tglf_ns_in)
-    ierr=set_harvest_payload_bol(harvest_sendline,'USE_TRANSPORT_MODEL'//NUL,tglf_use_transport_model_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'GEOMETRY_FLAG'//NUL,tglf_geometry_flag_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'NS'//NUL,tglf_ns_in)
+!    ierr=set_harvest_payload_bol(harvest_sendline,'USE_TRANSPORT_MODEL'//NUL,tglf_use_transport_model_in) ! DISABLED because we only allow runs with flux calculations
+!    ierr=set_harvest_payload_swt(harvest_sendline,'GEOMETRY_FLAG'//NUL,tglf_geometry_flag_in) ! DISABLED because we only allow Miller geometry
 
     ierr=set_harvest_payload_bol(harvest_sendline,'USE_BPER'//NUL,tglf_use_bper_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'USE_BPAR'//NUL,tglf_use_bpar_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'USE_MHD_RULE'//NUL,tglf_use_mhd_rule_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'USE_BISECTION'//NUL,tglf_use_bisection_in)
 
-    ierr=set_harvest_payload_int(harvest_sendline,'SAT_RULE'//NUL,tglf_sat_rule_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'KYGRID_MODEL'//NUL,tglf_kygrid_model_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'XNU_MODEL'//NUL,tglf_xnu_model_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'VPAR_MODEL'//NUL,tglf_vpar_model_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'VPAR_SHEAR_MODEL'//NUL,tglf_vpar_shear_model_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'SIGN_BT'//NUL,INT(tglf_sign_bt_in))
-    ierr=set_harvest_payload_int(harvest_sendline,'SIGN_IT'//NUL,INT(tglf_sign_it_in))
+    ierr=set_harvest_payload_swt(harvest_sendline,'SAT_RULE'//NUL,tglf_sat_rule_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'KYGRID_MODEL'//NUL,tglf_kygrid_model_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'XNU_MODEL'//NUL,tglf_xnu_model_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'VPAR_MODEL'//NUL,tglf_vpar_model_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'VPAR_SHEAR_MODEL'//NUL,tglf_vpar_shear_model_in)
+    ierr=set_harvest_payload_bol(harvest_sendline,'SIGN_BT'//NUL,INT((tglf_sign_bt_in+1)/2.))
+    ierr=set_harvest_payload_bol(harvest_sendline,'SIGN_IT'//NUL,INT((tglf_sign_it_in+1)/2.))
     ierr=set_harvest_payload_dbl(harvest_sendline,'KY'//NUL,tglf_ky_in)
-    ierr=set_harvest_payload_bol(harvest_sendline,'NEW_EIKONAL'//NUL,tglf_new_eikonal_in)
+!    ierr=set_harvest_payload_bol(harvest_sendline,'NEW_EIKONAL'//NUL,tglf_new_eikonal_in) ! DISABLED because we only allow freshly computed eikonal
     ierr=set_harvest_payload_dbl(harvest_sendline,'VEXB'//NUL,tglf_vexb_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'VEXB_SHEAR'//NUL,tglf_vexb_shear_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'BETAE'//NUL,tglf_betae_in)
@@ -49,12 +57,12 @@
     ierr=set_harvest_payload_dbl(harvest_sendline,'ZEFF'//NUL,tglf_zeff_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'DEBYE'//NUL,tglf_debye_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'IFLUX'//NUL,tglf_iflux_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'IBRANCH'//NUL,tglf_ibranch_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NMODES'//NUL,tglf_nmodes_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NBASIS_MAX'//NUL,tglf_nbasis_max_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NBASIS_MIN'//NUL,tglf_nbasis_min_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NXGRID'//NUL,tglf_nxgrid_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NKY'//NUL,tglf_nky_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'IBRANCH'//NUL,tglf_ibranch_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'NMODES'//NUL,tglf_nmodes_in)
+!    ierr=set_harvest_payload_swt(harvest_sendline,'NBASIS_MAX'//NUL,tglf_nbasis_max_in) ! DISABLED because not physics inputs/outputs
+!    ierr=set_harvest_payload_swt(harvest_sendline,'NBASIS_MIN'//NUL,tglf_nbasis_min_in) ! DISABLED because not physics inputs/outputs
+    ierr=set_harvest_payload_swt(harvest_sendline,'NXGRID'//NUL,tglf_nxgrid_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'NKY'//NUL,tglf_nky_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'ADIABATIC_ELEC'//NUL,tglf_adiabatic_elec_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'ALPHA_MACH'//NUL,tglf_alpha_mach_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'ALPHA_E'//NUL,tglf_alpha_e_in)
@@ -63,17 +71,17 @@
     ierr=set_harvest_payload_dbl(harvest_sendline,'XNU_FACTOR'//NUL,tglf_xnu_factor_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'DEBYE_FACTOR'//NUL,tglf_debye_factor_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'ETG_FACTOR'//NUL,tglf_etg_factor_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'WRITE_WAVEFUNCTION_FLAG'//NUL,tglf_write_wavefunction_flag_in)
+!    ierr=set_harvest_payload_swt(harvest_sendline,'WRITE_WAVEFUNCTION_FLAG'//NUL,tglf_write_wavefunction_flag_in)  ! DISABLED because not physics inputs/outputs
 
 !   '#---------------------------------------------------'
 !   '# Species vectors:'
 !   '#---------------------------------------------------'
-    do i = 1,tglf_ns_in
-      if (i < 10) then
-         write (NUM, "(I01)") i
+    DO i = 1,tglf_ns_in
+      IF (i < 10) THEN
+         write (NUM, "(I01,A1)") i,NUL
       else
-         write (NUM, "(I02)") i
-      endif
+         write (NUM, "(I02,A1)") i,NUL
+      ENDIF
       ierr=set_harvest_payload_dbl(harvest_sendline,'ZS_'//NUM,tglf_zs_in(i))
       ierr=set_harvest_payload_dbl(harvest_sendline,'MASS_'//NUM,tglf_mass_in(i))
       ierr=set_harvest_payload_dbl(harvest_sendline,'RLNS_'//NUM,tglf_rlns_in(i))
@@ -84,7 +92,7 @@
       ierr=set_harvest_payload_dbl(harvest_sendline,'VPAR_SHEAR_'//NUM,tglf_vpar_shear_in(i))
       ierr=set_harvest_payload_dbl(harvest_sendline,'VNS_SHEAR_'//NUM,tglf_vns_shear_in(i))
       ierr=set_harvest_payload_dbl(harvest_sendline,'VTS_SHEAR_'//NUM,tglf_vts_shear_in(i))
-    enddo
+    ENDDO
 
 !   '#---------------------------------------------------'
 !   '# Gaussian width parameters:'
@@ -92,7 +100,7 @@
     ierr=set_harvest_payload_dbl(harvest_sendline,'WIDTH'//NUL,tglf_width_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'WIDTH_MIN'//NUL,tglf_width_min_in)
     ierr=set_harvest_payload_bol(harvest_sendline,'FIND_WIDTH'//NUL,tglf_find_width_in)
-    ierr=set_harvest_payload_int(harvest_sendline,'NWIDTH'//NUL,tglf_nwidth_in)
+    ierr=set_harvest_payload_swt(harvest_sendline,'NWIDTH'//NUL,tglf_nwidth_in)
 
 !   '#---------------------------------------------------'
 !   '# Miller geometry parameters:'
@@ -115,19 +123,6 @@
     ierr=set_harvest_payload_dbl(harvest_sendline,'KX0_LOC'//NUL,tglf_kx0_loc_in)
 
 !   '#---------------------------------------------------'
-!   '# s-alpha geometry parameters:' (DISABLED)
-!   '#---------------------------------------------------'
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'RMIN_SA'//NUL,tglf_rmin_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'RMAJ_SA'//NUL,tglf_rmaj_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'Q_SA'//NUL,tglf_q_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'SHAT_SA'//NUL,tglf_shat_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'ALPHA_SA'//NUL,tglf_alpha_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'XWELL_SA'//NUL,tglf_xwell_sa_in)
-!    ierr=set_harvest_payload_dbl(harvest_sendline,'THETA0_SA'//NUL,tglf_theta0_sa_in)
-!    ierr=set_harvest_payload_int(harvest_sendline,'B_MODEL_SA'//NUL,tglf_b_model_sa_in)
-!    ierr=set_harvest_payload_int(harvest_sendline,'FT_MODEL_SA'//NUL,tglf_ft_model_sa_in)
-
-!   '#---------------------------------------------------'
 !   '# Expert parameters:'
 !   '#---------------------------------------------------'
     ierr=set_harvest_payload_dbl(harvest_sendline,'DAMP_PSI'//NUL,tglf_damp_psi_in)
@@ -141,6 +136,24 @@
     ierr=set_harvest_payload_dbl(harvest_sendline,'FILTER'//NUL,tglf_filter_in)
     ierr=set_harvest_payload_dbl(harvest_sendline,'THETA_TRAPPED'//NUL,tglf_theta_trapped_in)
 
-    ierr=harvest_send(harvest_sendline)
+    DO i = 1,tglf_ns_in
+      IF (i < 10) THEN
+         write (NUM, "(I01,A1)") i,NUL
+      else
+         write (NUM, "(I02,A1)") i,NUL
+      ENDIF
+      ierr=set_harvest_payload_dbl(harvest_sendline,'OUT_PARTICLE_FLUX_'//NUM, &
+                                   get_particle_flux(i,1)+get_particle_flux(i,2)+get_particle_flux(i,3))
+      ierr=set_harvest_payload_dbl(harvest_sendline,'OUT_ENERGY_FLUX_'//NUM, &
+                                   get_energy_flux(i,1)+get_energy_flux(i,2)+get_energy_flux(i,3))
+      ierr=set_harvest_payload_dbl(harvest_sendline,'OUT_STRESS_PAR_'//NUM, &
+                                   get_stress_par(i,1)+get_stress_par(i,2)+get_stress_par(i,3))
+      ierr=set_harvest_payload_dbl(harvest_sendline,'OUT_STRESS_TOR_'//NUM, &
+                                   get_stress_tor(i,1)+get_stress_tor(i,2)+get_stress_tor(i,3))
+      ierr=set_harvest_payload_dbl(harvest_sendline,'OUT_STRESS_EXCHANGE_'//NUM, &
+                                   get_exchange(i,1)+get_exchange(i,2)+get_exchange(i,3))
+   ENDDO
 
-  end subroutine tglf_harvest_local
+   ierr=harvest_send(harvest_sendline)
+
+  END SUBROUTINE tglf_harvest_local
