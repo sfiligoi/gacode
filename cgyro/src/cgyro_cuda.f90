32bbf80be440525ceb2fb89624eb1c900059450a
Jeff Candy <jcandy@san.rr.com>
2016-02-02 04:16:09 -0500

    Should now work on desktop/gfortran and titan

--- cgyro/src/cgyro_cuda.f90
+++
@@ -1,3 +0,0 @@
-module precision_m
-  implicit none
-end module precision_m
---
+++ cgyro/src/precision_m.f90
@@ -0,0 +1,13 @@
+module precision_m
+  implicit none
+  integer, parameter :: sp = selected_real_kind(6,30)
+  integer, parameter :: dp = selected_real_kind(14,60)
+  integer, parameter :: wp = dp
+  
+  integer, parameter :: singlePrecision = sp
+  integer, parameter :: doublePrecision = dp
+  integer, parameter :: fp_kind = wp
+  
+  integer, parameter :: i4 = selected_int_kind(9)
+  integer, parameter :: i8 = selected_int_kind(14)
+end module precision_m
