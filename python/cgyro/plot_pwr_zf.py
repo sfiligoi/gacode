e4bb690802ab1b2c25cd8fc91c692543ce5df5a3
Jeff Candy <jcandy@san.rr.com>
2016-02-17 16:45:28 -0800

    Improved spectral plotting

--- python/cgyro/plot_pwr_zf.py
+++ python/cgyro/plot_pwr_zf.py
@@ -4,7 +4,6 @@ from gacodeplotdefs import *
 from cgyro.data import cgyrodata
 
 ftype = sys.argv[1]
-field = sys.argv[2]
 
 sim = cgyrodata('./')
 sim.getbig()
@@ -18,26 +17,29 @@ ax.set_xlabel(r'$(c_s/a)\, t$')
 ax.set_ylabel(r'$\Phi$')
 ax.set_yscale('log')
 
-y = np.sum(sim.phisq,axis=0)
+y0 = sim.phisq[:,0,:]
 
-for n in range(sim.n_n):
-    num = r'$n='+str(n)+'$'
-    if n==0:
-        ax.plot(sim.t,np.sqrt(y[n,:]),linewidth=2,label=num)
-    else:
-        ax.plot(sim.t,np.sqrt(y[n,:]),label=num)
+for i in range(sim.n_radial):
+    num = r'$p='+str(i)+'$'
+    if abs(i-sim.n_radial/2) < 3:
+        ax.plot(sim.t,y0[i,:])
+#    if i == 0:
+#        ax.plot(sim.t,y0[i,:],'r',alpha=0.2)
+#    if i == sim.n_radial/2:
+#        ax.plot(sim.t,y0[i,:],'--k')
 
 ax.set_xlim([0,max(sim.t)])
+#ax.set_ylim([-0.1,0.1])
 #======================================
 
-if sim.n_n > 16:
-    ax.legend(loc=4, ncol=5, prop={'size':12})
-else:
-    ax.legend(loc=4, ncol=6, prop={'size':12})
+#if sim.n_n > 16:
+#    ax.legend(loc=4, ncol=5, prop={'size':12})
+#else:
+#    ax.legend(loc=4, ncol=6, prop={'size':12})
 
 
 if ftype == 'screen':
     plt.show()
 else:
-    outfile = 'pwr.'+ftype
+    outfile = 'pwr_zf.'+ftype
     plt.savefig(outfile)
