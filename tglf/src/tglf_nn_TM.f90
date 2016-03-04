11835414d54d8e8c692a458bb0ca38753a3682a5
Teo Luda <s208973@studenti.polito.it>
2016-03-04 15:30:14 -0800

    Removed reading of input.lim

--- tglf/src/tglf_nn_TM.f90
+++ tglf/src/tglf_nn_TM.f90
@@ -23,11 +23,7 @@
      real :: OUT_PARTICLE_FLUX_3_STD, OUT_STRESS_TOR_1_STD, OUT_STRESS_TOR_3_STD
      real :: OUT_ENERGY_FLUX_1_RNG, OUT_ENERGY_FLUX_3_RNG, OUT_PARTICLE_FLUX_1_RNG
      real :: OUT_PARTICLE_FLUX_3_RNG, OUT_STRESS_TOR_1_RNG, OUT_STRESS_TOR_3_RNG
-     real :: AS_2_LIM, AS_3_LIM, BETAE_LIM, DELTA_LOC_LIM, KAPPA_LOC_LIM, Q_LOC_LIM
-     real :: Q_PRIME_LOC_LIM, RLNS_1_LIM, RLTS_1_LIM, RLTS_2_LIM, RMAJ_LOC_LIM 
-     real :: RMIN_LOC_LIM, S_KAPPA_LOC_LIM, TAUS_2_LIM, XNUE_LIM
      integer :: n
-     real :: nn_in_lim=10000
 !
 ! initialize fluxes
 !
@@ -82,36 +78,17 @@
 ! Read values for checking accuracy of the NN
 !
      open (unit=16, file=TRIM(tglf_path_in)//"output.std", action="read")
-     open (unit=17, file=TRIM(tglf_path_in)//"input.lim", action="read")
      open (unit=20, file=TRIM(tglf_path_in)//"output.rng", action="read")
      read(16,*) n, OUT_ENERGY_FLUX_1_STD, OUT_ENERGY_FLUX_3_STD, OUT_PARTICLE_FLUX_1_STD, &
           OUT_PARTICLE_FLUX_3_STD, OUT_STRESS_TOR_1_STD, OUT_STRESS_TOR_3_STD     
-     read(17,*) n, AS_2_LIM, AS_3_LIM, BETAE_LIM, DELTA_LOC_LIM, KAPPA_LOC_LIM, Q_LOC_LIM, &
-          Q_PRIME_LOC_LIM, RLNS_1_LIM, RLTS_1_LIM, RLTS_2_LIM, RMAJ_LOC_LIM, &
-          RMIN_LOC_LIM, S_KAPPA_LOC_LIM, TAUS_2_LIM, XNUE_LIM
      read(20,*) n, OUT_ENERGY_FLUX_1_RNG, OUT_ENERGY_FLUX_3_RNG, OUT_PARTICLE_FLUX_1_RNG, &
           OUT_PARTICLE_FLUX_3_RNG, OUT_STRESS_TOR_1_RNG, OUT_STRESS_TOR_3_RNG
      close(16)
-     close(17)
      close(20)
 !
-! If only 1 NN: check 'input.lim', else: check JUST 'output.rng'
+! Switch between TGLF and the NN depending on 'nn_thrsh' values
 !
-     if ((OUT_ENERGY_FLUX_1_RNG==0) .and. (OUT_ENERGY_FLUX_3_RNG==0) .and. (OUT_PARTICLE_FLUX_1_RNG==0) .and. &
-          (OUT_PARTICLE_FLUX_3_RNG==0) .and. (OUT_STRESS_TOR_1_RNG==0) .and. (OUT_STRESS_TOR_3_RNG==0)) then
-        nn_in_lim=2
-     else 
-        nn_in_lim=9999 
-     endif
-!
-! Switch between TGLF and the NN depending on 'input.lim' e/o 'nn_thrsh' values
-!
-     if ((abs(AS_2_LIM)<nn_in_lim) .and. (abs(AS_3_LIM)<nn_in_lim) .and. (abs(BETAE_LIM)<nn_in_lim) .and. &
-          (abs(DELTA_LOC_LIM)<nn_in_lim) .and. (abs(KAPPA_LOC_LIM)<nn_in_lim) .and. (abs(Q_LOC_LIM)<nn_in_lim) .and. &
-          (abs(Q_PRIME_LOC_LIM)<nn_in_lim) .and. (abs(RLNS_1_LIM)<nn_in_lim) .and. (abs(RLTS_1_LIM)<nn_in_lim) .and. &
-          (abs(RLTS_2_LIM)<nn_in_lim) .and. (abs(RMAJ_LOC_LIM)<nn_in_lim) .and. (abs(RMIN_LOC_LIM)<nn_in_lim) .and. &
-          (abs(S_KAPPA_LOC_LIM)<nn_in_lim) .and. (abs(TAUS_2_LIM)<nn_in_lim) .and. (abs(XNUE_LIM)<nn_in_lim) .and. &
-          (OUT_ENERGY_FLUX_1_RNG<tglf_nn_thrsh_energy_in) .and. (OUT_ENERGY_FLUX_3_RNG<tglf_nn_thrsh_energy_in) .and. &
+     if ((OUT_ENERGY_FLUX_1_RNG<tglf_nn_thrsh_energy_in) .and. (OUT_ENERGY_FLUX_3_RNG<tglf_nn_thrsh_energy_in) .and. &
           (OUT_PARTICLE_FLUX_1_RNG<tglf_nn_thrsh_energy_in) .and. (OUT_PARTICLE_FLUX_3_RNG<tglf_nn_thrsh_energy_in) .and. &
           (OUT_STRESS_TOR_1_RNG<tglf_nn_thrsh_energy_in) .and. (OUT_STRESS_TOR_3_RNG<tglf_nn_thrsh_energy_in)) then
 !
