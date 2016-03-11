4c94339178177a059e542fda0e7d9d892404a169
Teo Luda <s208973@studenti.polito.it>
2016-03-10 16:46:49 -0800

    Set to zero negative energy flux & Ions sorted as Harvest

--- tglf/src/tglf_nn_TM.f90
+++ tglf/src/tglf_nn_TM.f90
@@ -18,12 +18,14 @@
      REAL :: exch0(nsm,3)
      REAL :: nsum0(nsm),tsum0(nsm)
 !
+     REAL, DIMENSION(10) :: tmp
+     INTEGER, DIMENSION(10) :: ions_order
 !
      real :: OUT_ENERGY_FLUX_1_STD, OUT_ENERGY_FLUX_3_STD, OUT_PARTICLE_FLUX_1_STD
      real :: OUT_PARTICLE_FLUX_3_STD, OUT_STRESS_TOR_1_STD, OUT_STRESS_TOR_3_STD
      real :: OUT_ENERGY_FLUX_1_RNG, OUT_ENERGY_FLUX_3_RNG, OUT_PARTICLE_FLUX_1_RNG
      real :: OUT_PARTICLE_FLUX_3_RNG, OUT_STRESS_TOR_1_RNG, OUT_STRESS_TOR_3_RNG
-     integer :: n
+     integer :: n, j, i
 !
 ! initialize fluxes
 !
@@ -43,11 +45,25 @@
        q_low_out(is) = 0.0
      enddo
 !
+! '#---------------------------------------------------'
+! '# Sort ions by A, Z, Te/Ti'
+! '#---------------------------------------------------'
+     tmp=0.0
+     tmp(1)=1E10
+     DO i = 2,tglf_ns_in
+         tmp(i)=tglf_mass_in(i)*100000+tglf_zs_in(i)*1000+1./(1+tglf_rlts_in(i))
+     ENDDO
+     DO i = 1,tglf_ns_in
+         j=MAXLOC(tmp, DIM=1)
+         ions_order(i)=j
+         tmp(j)=0
+     ENDDO
+!
 ! Write input file for the NN
 !
      open (unit=14, file=TRIM(tglf_path_in)//"input.dat", action="write")
      write (14,*) '1' 
-     write (14,"(15(f6.3,x))") tglf_as_in(3), tglf_as_in(2), tglf_betae_in, &
+     write (14,"(15(f6.3,x))") tglf_as_in(ions_order(2)), tglf_as_in(ions_order(3)), tglf_betae_in, &
           tglf_delta_loc_in, tglf_kappa_loc_in, tglf_q_loc_in, &
           tglf_q_prime_loc_in, tglf_rlns_in(1), tglf_rlts_in(1), &
           tglf_rlts_in(3), tglf_rmaj_loc_in, tglf_rmin_loc_in, &
@@ -71,6 +87,18 @@
           particle_flux_out(1,1), particle_flux_out(3,1), &
           stress_tor_out(1,1), stress_tor_out(3,1)
      close(15)
+!
+! Set negative energy fluxes to zero
+!    
+     if (energy_flux_out(1,1)<0) then
+        energy_flux_out(1,1)=0
+     endif
+     if (energy_flux_out(3,1)<0) then
+        energy_flux_out(3,1)=0
+     endif
+!
+! Set Carbon fluxes equal to Deuterium fluxes
+!
      energy_flux_out(2,1)=energy_flux_out(3,1)
      particle_flux_out(2,1)=particle_flux_out(3,1)
      stress_tor_out(2,1)=stress_tor_out(3,1)
