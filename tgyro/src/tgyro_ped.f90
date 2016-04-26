703d48a6918820f9d98eec3ff40bb60173de94af
Jeff Candy <jcandy@san.rr.com>
2016-04-26 11:43:05 -0700

    Commented out code in tgyro_ped to allow compilation

--- tgyro/src/tgyro_ped.f90
+++ tgyro/src/tgyro_ped.f90
@@ -18,7 +18,7 @@ module tgyro_ped
   real :: neped_in
   real :: r_in
   real :: zeffped_in
-
+ 
   ! Typical values
   !
   !       a = 0.55
@@ -48,16 +48,21 @@ contains
     real :: t_ped,n_ped
     real :: zt_ped
     real :: beta_ped_NN
+    real :: n_ped_NN,n_edge_NN
+    real :: t_ped_NN,t_edge_NN 
     real :: p_ped_NN,w_ped_NN
     real :: p_top_NN,w_top_NN
-    real :: t_ped_NN,n_ped_NN
-    real :: n_edge_NN,t_edge_NN
+    real :: n_top_NN
     real :: psi_ped(1),r_ped(1),psip_ped(1)
     character(len=1000) :: nn_executable
     character(len=1000) :: nn_files
-    integer dummy
+    character(len=1) :: dummy
+    integer :: nx_exp
+    integer, parameter :: nx_nn=1001
+    real :: nn_vec(nx_nn,3) 
 
     integer, parameter :: print_flag=1
+    integer, parameter :: test_flag=1
 
     if (tgyro_ped_model == 1) return
 
@@ -67,79 +72,70 @@ contains
     ! ** temporary **
     neped_in   = 3.62
     zeffped_in = 2.07
-    betan_in   = 1.28
-
-    ! All *_in variables set in tgyro_init_profiles
-
-    if (print_flag == 1 .and. i_proc_global == 0) then
-       print 10,'     a_in [m]',a_in
-       print 10,' betan_in [-]',betan_in
-       print 10,'    bt_in [T]',bt_in
-       print 10,' delta_in [-]',delta_in
-       print 10,'   ip_in [MA]',ip_in
-       print 10,' kappa_in [-]',kappa_in
-       print 10,'     m_in [-]',m_in
-       print 10,' neped_in [-]',neped_in
-       print 10,'     r_in [m]',r_in
-       print 10,'  zeff_in [-]',zeffped_in
-    endif
     !-------------------------------------------------------------------------
 
     !-------------------------------------------------------------------------
     ! 2. Call EPED1NN to get p_ped_NN, w_ped_NN
     !
-    n_ped_NN  = neped_in
-    n_edge_NN = neped_in*0.25
-    t_edge_NN  = 80
+    if (i_proc_global == 0) then
+       !
+       ! Write input file for the NN
+       open(unit=1,file='input.dat',status='replace')
+       write(1,'(a)') '1'
+       write(1,'(10(f6.3,1x))') &
+            a_in       ,&
+            betan_in   ,&
+            bt_in      ,&
+            delta_in   ,&
+            ip_in      ,&
+            kappa_in   ,&
+            m_in       ,&
+            neped_in   ,&
+            r_in       ,&
+            zeffped_in
+       close(1)
+       !
+       ! Execute the NN
+       if (test_flag == 0) then
+          !call get_environment_variable('BRAINFUSE_RUN',nn_executable)
+          !call get_environment_variable('EPED1NN',nn_files)
+          !call execute_command_line(trim(nn_executable)//' '//trim(nn_files)//' input.dat')
+       endif
 
-    if (print_flag == 1 .and. i_proc_global == 0) then
-        !
-        ! Write input file for the NN
-        !
-        open (unit=14, file="input.dat", action="write")
-        write (14,*) '1'
-        write (14,"(10(f6.3,1x))") a_in       ,&
-                                  betan_in   ,&
-                                  bt_in      ,&
-                                  delta_in   ,&
-                                  ip_in      ,&
-                                  kappa_in   ,&
-                                  m_in       ,&
-                                  neped_in   ,&
-                                  r_in       ,&
-                                  zeffped_in
-         close(14)
+       ! Read neuped_vec=[psi_norm,ne,ptot]
+       open(unit=1,file='neuped.profiles',status='old')
+       read(1,*) dummy
+       read(1,*) nn_vec
+       close(1)
 
-        !
-        ! Execute the NN
-        !
-        call get_environment_variable('BRAINFUSE_RUN',nn_executable)
-        call get_environment_variable('EPED1NN',nn_files)
-        call execute_command_line(trim(nn_executable)//' '//trim(nn_files)//' input.dat')
+       open (unit=1, file="output.avg", action="read")
+       read(1,*) dummy
+       read(1,*) beta_ped_NN,p_ped_NN,p_top_NN,w_ped_NN,w_top_NN
+       close(1)
+
     endif
 
-    ! All processes wait for pedestal calculation to finish and read results
-    call mpi_barrier(MPI_COMM_WORLD,ierr)
+    ! Data from output.avg
+    call MPI_BCAST(nn_vec,size(nn_vec),MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-    !
-    ! Read outputs
-    !
-    open (unit=14, file="output.avg", action="read")
-    read(14,*) dummy
-    read(14,*) beta_ped_NN,p_ped_NN,p_top_NN,w_ped_NN,w_top_NN
-    close(14)
-
-    t_ped_NN=p_ped_NN/n_ped_NN/1.6021766208/2.*1E6 ! From pressure in MPa to temperature in eV
+    !call cub_spline(nn_vec(:,1),nn_vec(:,2),nx_nn,psi_top,ne_top,1)
+    !call cub_spline(nn_vec(:,1),nn_vec(:,3),nx_nn,psi_top,p_top,1)
+ 
+    ! Convert p [MPa] and n [10^13/cm^3] to T [eV]
+    t_ped_NN  = 1e-6*p_top_NN/(2*n_top_NN*k) 
+    ! EPED assumptions: 
+    n_edge_NN = neped_in*0.25
+    t_edge_NN = 80.0
 
     !-------------------------------------------------------------------------
 
     !-------------------------------------------------------------------------
     ! 3. Map EPED1NN outputs to TGYRO pedestal parameters
     !
-    nr = size(rmin_eped)
-    psi_ped(1) = 1-2.0*w_ped_NN
-    call cub_spline(polflux_eped,rmin_eped,nr,psi_ped,r_ped,1)
-    call cub_spline(polflux_eped,polfluxp_eped,nr,psi_ped,psip_ped,1)
+    nx_exp = size(rmin_eped)
+    psi_ped(1) = 1-3.0*w_ped_NN
+    call cub_spline(polflux_eped,rmin_eped,nx_exp,psi_ped,r_ped,1)
+    call cub_spline(polflux_eped,polfluxp_eped,nx_exp,psi_ped,psip_ped,1)
 
     t_ped  = t_ped_NN*1e-3 ! Convert from eV to keV
     n_ped  = n_ped_NN
