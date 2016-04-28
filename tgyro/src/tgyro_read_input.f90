ac6060b735ef1f422abcc19157938ecbc7802d3b
jcandy <jcandy@san.rr.com>
2016-04-27 21:25:43 -0700

    Fixed input issue

--- tgyro/src/tgyro_read_input.f90
+++ tgyro/src/tgyro_read_input.f90
@@ -126,7 +126,7 @@ subroutine tgyro_read_input
   call tgyro_readbc_int(tgyro_neo_n_theta)
   call tgyro_readbc_int(tgyro_ptot_flag)
   call tgyro_readbc_int(tgyro_ped_model)
-  call tgyro_readbc_int(tgyro_neped)
+  call tgyro_readbc_real(tgyro_neped)
   ! ** END input read; ADD NEW PARAMETERS ABOVE HERE!!
   call tgyro_readbc_int(n_inst)
   !-------------------------------------------------------
