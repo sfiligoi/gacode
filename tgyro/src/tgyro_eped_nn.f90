dd01e95e7b5674009723f0b2390545113e452070
Jeff Candy <jcandy@san.rr.com>
2016-05-03 13:51:51 -0700

    Introduced system functions

--- tgyro/src/tgyro_eped_nn.f90
+++ tgyro/src/tgyro_eped_nn.f90
@@ -29,7 +29,7 @@ subroutine tgyro_eped_nn
   call get_environment_variable('EPEDNN_MODEL',nn_files)
   !write(*,*)'EPEDNN executable: ',trim(nn_executable)
   !write(*,*)'EPEDNN model: ',trim(nn_files)
-  call execute_command_line(trim(nn_executable)//' '//trim(nn_files)//' input.dat')
+  call tgyro_system(trim(nn_executable)//' '//trim(nn_files)//' input.dat')
 
   ! Read epednn_vec=[psi_norm,ne,ptot=2 ne T]
   open(unit=1,file='epednn.profiles',status='old')
---
+++ tgyro/src/tgyro_system.legacy.f90
@@ -0,0 +1,8 @@
+subroutine tgyro_system(arg)
+
+  implicit none
+  character(len=*), intent(in) :: arg
+
+  call system(arg)
+
+end subroutine tgyro_system
