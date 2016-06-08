1811bdb56233f26730fe6ef54232bcd3ef6d2824
Jeff Candy <jcandy@san.rr.com>
2016-06-08 13:55:26 -0700

    Improved balloon plotting

--- python/cgyro/plot_ball.py
+++ python/cgyro/plot_ball.py
@@ -16,10 +16,13 @@ if itime > sim.n_time-1:
 # Construct complex eigenfunction at selected time
 if ifield == 0:
     f = sim.phib[0,:,itime]+1j*sim.phib[1,:,itime]
+    ytag = r'$\delta\phi$'
 elif ifield == 1:
     f = sim.aparb[0,:,itime]+1j*sim.aparb[1,:,itime]
+    ytag = r'$\delta A_\parallel$'
 elif ifield == 2:
     f = sim.bparb[0,:,itime]+1j*sim.bparb[1,:,itime]
+    ytag = r'$\delta B_\parallel$'
 
 fig = plt.figure(figsize=(10,6))
 
@@ -28,6 +31,7 @@ ax = fig.add_subplot(111)
 ax.grid(which="majorminor",ls=":")
 ax.grid(which="major",ls=":")
 ax.set_xlabel(r'$\theta_*/\pi$')
+ax.set_ylabel(ytag)
 
 if sim.n_radial == 1:
     x = sim.theta/np.pi
@@ -42,11 +46,8 @@ else:
 y1 = np.real(f)
 y2 = np.imag(f)
 
-ax.plot(x,y1,'-o',color='black',markersize=2,label='Re')
-ax.plot(x,y2,'-o',color='blue',markersize=2,label='Im')
-
-
-
+ax.plot(x,y1,'-o',color='black',markersize=2,label=r'$\mathrm{Re}$')
+ax.plot(x,y2,'-o',color='red',markersize=2,label=r'$\mathrm{Im}$')
 
 ax.legend()
 #======================================
