127c7f35f475df69aa023e32c050ca6d1f022848
Orso Meneghini <meneghini@fusion.gat.com>
2016-07-26 16:51:11 -0700

    half way fortran embedding of EPEDNN

--- tgyro/src/tgyro_eped_nn.f90
+++ tgyro/src/tgyro_eped_nn.f90
@@ -8,6 +8,37 @@ subroutine tgyro_eped_nn
   character(len=1000) :: nn_files
   character(len=1) :: dummy
 
+  character(len=1000) :: epednn_model
+  real*4 :: INPUT_PARAMETERS(10)
+  real*4 :: OUTPUT_PARAMETERS(5)
+  integer :: ierr
+  CHARACTER NUL
+  PARAMETER(NUL = CHAR(0))
+
+  include 'brainfuse_lib.inc'
+
+  INPUT_PARAMETERS( 1)= a_in
+  INPUT_PARAMETERS( 2)= betan_in
+  INPUT_PARAMETERS( 3)= bt_in
+  INPUT_PARAMETERS( 4)= delta_in
+  INPUT_PARAMETERS( 5)= ip_in
+  INPUT_PARAMETERS( 6)= kappa_in
+  INPUT_PARAMETERS( 7)= m_in
+  INPUT_PARAMETERS( 8)= neped_in
+  INPUT_PARAMETERS( 9)= r_in
+  INPUT_PARAMETERS(10)= zeffped_in
+  
+  WRITE(*,*)INPUT_PARAMETERS
+  
+  call get_environment_variable('TGLFNN_MODEL_DIR',epednn_model)
+  write(*,*)TRIM(epednn_model)
+  ierr=load_anns(1, TRIM(epednn_model)//NUL,'brainfuse'//NUL)
+  ! ierr=load_anns_inputs(INPUT_PARAMETERS)
+  ! ierr=run_anns()
+  ! ierr=get_anns_avg_array(OUTPUT_PARAMETERS)
+  
+  ! WRITE(*,*)OUTPUT_PARAMETERS
+  
   ! Write input file for the NN
   open(unit=1,file='input.dat',status='replace')
   write(1,'(a)') '1'
