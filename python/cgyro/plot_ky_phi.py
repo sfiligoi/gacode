491275ef67be1f43a80f302ed22cf2ef3b5f0061
Jeff Candy <jcandy@san.rr.com>
2016-09-30 15:32:38 -0700

    Add n-selection logic to cgyro plotting

--- python/cgyro/plot_ky_phi.py
+++ python/cgyro/plot_ky_phi.py
@@ -5,6 +5,19 @@ from cgyro.data import cgyrodata
 
 ftype = sys.argv[1]
 field = sys.argv[2]
+nstr  = sys.argv[3]
+
+#------------------------------------------------------
+# Construct an explicit integer list based on string
+nvec = []
+for i in nstr.split(','):
+    if '-' in i:
+        v = i.split('-')
+        for j in range(int(v[0]),int(v[1])+1):
+            nvec.append(j)
+    else:
+        nvec.append(int(i))
+#------------------------------------------------------
 
 sim = cgyrodata('./')
 sim.getbigfield()
@@ -20,7 +33,10 @@ ax.set_yscale('log')
 
 y = np.sum(sim.phisq,axis=0)
 
-for n in range(sim.n_n):
+if nstr == 'null':
+    nvec = range(sim.n_n)
+
+for n in nvec:
     num = r'$n='+str(n)+'$'
     if n==0:
         ax.plot(sim.t,np.sqrt(y[n,:]),linewidth=2,label=num)
@@ -28,7 +44,8 @@ for n in range(sim.n_n):
         ax.plot(sim.t,np.sqrt(y[n,:]),label=num)
 
 ax.set_xlim([0,max(sim.t)])
-#======================================
+
+
 
 if sim.n_n > 16:
     ax.legend(loc=4, ncol=5, prop={'size':12})
