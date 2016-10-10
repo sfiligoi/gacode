57743fa94dd8647dc797d44908e68e3915d345ed
Jeff Candy <jcandy@san.rr.com>
2016-10-10 01:24:32 -0400

    Cleaned up GPU FFT code, and simplified makefile

--- cgyro/src/cgyro_nl_fftw.gpu.f90
+++ cgyro/src/cgyro_nl_fftw.gpu.f90
@@ -12,11 +12,8 @@
 
 subroutine cgyro_nl_fftw(ij)
 
-#ifdef _OPENACC
   use precision_m
   use cufft_m
-#endif
-
   use timer_lib
   use parallel_lib
 
@@ -30,34 +27,22 @@ subroutine cgyro_nl_fftw(ij)
   integer :: ierr
 
   complex :: f0,g0
-#ifdef USE_HEAP
   complex, dimension(:,:), allocatable :: fpack
   complex, dimension(:,:), allocatable :: gpack
-#else
-  complex :: fpack(n_radial,nv_loc*n_theta)
-  complex :: gpack(n_radial,nv_loc*n_theta)
-#endif
+! complex :: fpack(n_radial,nv_loc*n_theta)
+! complex :: gpack(n_radial,nv_loc*n_theta)
 
   real :: inv_nxny
-
-  logical, parameter :: use_cufft = .true.
-  logical, parameter :: use_acc = .true.
-  integer, parameter :: default_size = 1024 * 1024 * 32
-  logical :: use_openmp 
-
 
   include 'fftw3.f03'
 
   call timer_lib_in('nl_comm')
 
-#ifdef USE_HEAP
   allocate(fpack(n_radial,nv_loc*n_theta))
   allocate(gpack(n_radial,nv_loc*n_theta))
-#endif
-  use_openmp = (size(fpack) >= default_size)
+
   iexch = 0
-!$omp  parallel do  if (use_openmp) &
-!$omp& private(iv_loc,it,ir,iexch,ic_loc)
+!$omp parallel do private(it,ir,iexch,ic_loc)
   do iv_loc=1,nv_loc
      do it=1,n_theta
         iexch = it + (iv_loc-1)*n_theta
@@ -68,18 +53,17 @@ subroutine cgyro_nl_fftw(ij)
         enddo
      enddo
   enddo
+
   call parallel_slib_f(fpack,f_nl)
   call parallel_slib_f(gpack,g_nl)
   call timer_lib_out('nl_comm')
 
   call timer_lib_in('nl')
 
-#ifdef _OPENACC
 !$acc  data pcopyin(f_nl) pcopy(g_nl)   &
 !$acc& pcreate(fxmany,fymany,gxmany,gymany) &
 !$acc& pcreate(uxmany,uymany,vxmany,vymany) &
 !$acc& pcreate(uvmany)
-#endif
 
 !$acc parallel
 !$acc loop gang
@@ -97,14 +81,13 @@ subroutine cgyro_nl_fftw(ij)
   enddo
 !$acc end parallel
 
-  if (use_acc) then
 !$acc parallel 
 !$acc loop gang
-     do j=1,nsplit
+  do j=1,nsplit
 
-        ! Array mapping
+   ! Array mapping
 !$acc loop worker private(p,ix)
-        do ir=1,n_radial
+    do ir=1,n_radial
 
            p  = ir-1-nx0/2
            ix = p
@@ -122,32 +105,6 @@ subroutine cgyro_nl_fftw(ij)
         enddo
      enddo
 !$acc end parallel
-  else
-!$acc update host(f_nl,g_nl) &
-!$acc& host(fxmany,gxmany,fymany,gymany)
-!$acc wait
-     do j=1,nsplit
-
-        ! Array mapping
-        do ir=1,n_radial
-
-           p  = ir-1-nx0/2
-           ix = p
-           if (ix < 0) ix = ix+nx  
-           do in=1,n_toroidal
-              iy = in-1
-              f0 = i_c*f_nl(ir,j,in)
-              g0 = i_c*g_nl(ir,j,in)
-              fxmany(iy,ix,j) = p*f0
-              gxmany(iy,ix,j) = p*g0
-              fymany(iy,ix,j) = iy*f0
-              gymany(iy,ix,j) = iy*g0
-           enddo
-        enddo
-     enddo
-!$acc update device(fxmany,gxmany,fymany,gymany)
-!$acc wait
-  endif
 
   if (kxfilter_flag == 1) then
 !$acc  parallel
@@ -164,8 +121,6 @@ subroutine cgyro_nl_fftw(ij)
 !$acc  end parallel
   endif
 
-  if (use_cufft) then
-
      ! --------------------------------------
      ! perform many Fourier Transforms at once
      ! --------------------------------------
@@ -174,53 +129,19 @@ subroutine cgyro_nl_fftw(ij)
 !$acc& use_device(fxmany,fymany,gxmany,gymany) &
 !$acc& use_device(uxmany,uymany,vxmany,vymany)
 
-     if (kind(uxmany).eq.singlePrecision) then
-        call cufftExecC2R(cu_plan_c2r_many,fxmany,uxmany)
-        call cufftExecC2R(cu_plan_c2r_many,fymany,uymany)
-        call cufftExecC2R(cu_plan_c2r_many,gxmany,vxmany)
-        call cufftExecC2R(cu_plan_c2r_many,gymany,vymany)
-     else
         call cufftExecZ2D(cu_plan_c2r_many,fxmany,uxmany)
         call cufftExecZ2D(cu_plan_c2r_many,fymany,uymany)
         call cufftExecZ2D(cu_plan_c2r_many,gxmany,vxmany)
         call cufftExecZ2D(cu_plan_c2r_many,gymany,vymany)
-     endif
 !$acc wait
 !$acc end host_data
 !$acc wait
-  else
-!$acc wait
-!$acc update host(fxmany,fymany,gxmany,gymany)
-!$acc wait
-
-!$omp  parallel do &
-!$omp& private(fx,fy,gx,gy,ux,uy,vx,vy)
-     do j=1,nsplit
-        fx(:,:) = fxmany(:,:,j)
-        fy(:,:) = fymany(:,:,j)
-        gx(:,:) = gxmany(:,:,j)
-        gy(:,:) = gymany(:,:,j)
-
-        call fftw_execute_dft_c2r(plan_c2r, fx,ux)
-        call fftw_execute_dft_c2r(plan_c2r, fy,uy)
-        call fftw_execute_dft_c2r(plan_c2r, gx,vx)
-        call fftw_execute_dft_c2r(plan_c2r, gy,vy)
-
-        uxmany(:,:,j) = ux(:,:)
-        uymany(:,:,j) = uy(:,:)
-        vxmany(:,:,j) = vx(:,:)
-        vymany(:,:,j) = vy(:,:)
-     enddo
-!$acc update device(uxmany,uymany,vxmany,vymany)
-!$acc wait
-  endif
 
   ! Poisson bracket in real space
   ! uv = (ux*vy-uy*vx)/(nx*ny)
 
   inv_nxny = dble(1)/dble(nx*ny)
 
-  if (use_acc) then
 !$acc  parallel 
 !$acc loop gang
      do j=1,nsplit
@@ -234,51 +155,21 @@ subroutine cgyro_nl_fftw(ij)
         enddo
      enddo
 !$acc  end parallel
-  else
-!$acc update host(uxmany,uymany,vxmany,vymany)
-!$acc wait
-
-!$omp workshare 
-     uvmany = (uxmany*vymany-uymany*vxmany)/(nx*ny)
-!$omp end workshare
-
-!$acc update device(uvmany)
-!$acc wait
-  endif
 
   ! ------------------
   ! Transform uv to fx
   ! ------------------
 
-  if (use_cufft) then
 !$acc wait
 !$acc host_data use_device(uvmany,fxmany)
-     if (kind(uvmany).eq.singlePrecision) then
-        call cufftExecR2C(cu_plan_r2c_many,uvmany,fxmany)
-     else
         call cufftExecD2Z(cu_plan_r2c_many,uvmany,fxmany)
-     endif
 !$acc wait
 !$acc end host_data
 !$acc wait
-  else
-!$acc wait
-!$acc update host(uvmany)
-!$acc wait
-!$omp parallel do private(j,uv,fx)
-     do j=1,nsplit
-        uv(:,:) = uvmany(:,:,j)
-        call fftw_execute_dft_r2c(plan_r2c,uv,fx)
-        fxmany(:,:,j) = fx(:,:)
-     enddo
-!$acc update device(fxmany)
-!$acc wait
-  endif
 
   ! NOTE: The FFT will generate an unwanted n=0,p=-nr/2 component
   ! that will be filtered in the main time-stepping loop
 
-  if (use_acc) then
 !$acc parallel  
 !$acc loop gang
      do j=1,nsplit
@@ -296,37 +187,16 @@ subroutine cgyro_nl_fftw(ij)
      enddo
 !$acc end parallel
 !$acc wait
-  else
-!$acc update host(fxmany,g_nl)
-!$acc wait
-     do j=1,nsplit
-        do ir=1,n_radial 
-           ix = ir-1-nx0/2
 
-           if (ix < 0) ix = ix+nx
-
-           do in=1,n_toroidal
-              iy = in-1
-              g_nl(ir,j,in) = fxmany(iy,ix,j)
-           enddo
-        enddo
-     enddo
-!$acc update device(g_nl)
-!$acc wait
-  endif
-
-#ifdef _OPENACC
-!$acc wait
 !$acc end data
 !$acc wait
-#endif
   call timer_lib_out('nl')
 
   call timer_lib_in('nl_comm')
   call parallel_slib_r(g_nl,gpack)
+
   iexch = 0
-!$omp  parallel do if (use_openmp) &
-!$omp& private(iv_loc,it,ir,iexch,ic_loc)
+!$omp parallel do private(it,ir,iexch,ic_loc)
   do iv_loc=1,nv_loc
      do it=1,n_theta
         iexch = it + (iv_loc-1)*n_theta
@@ -336,10 +206,9 @@ subroutine cgyro_nl_fftw(ij)
         enddo
      enddo
   enddo
-#ifdef USE_HEAP
+
   deallocate(fpack)
   deallocate(gpack)
-#endif
   call timer_lib_out('nl_comm')
 
   ! RHS -> -[f,g] = [f,g]_{r,-alpha}
