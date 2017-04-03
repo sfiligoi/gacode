fab8039e6ebc64c28aa1eb6fefd8dc67edd843be
Jeff Candy <jcandy@san.rr.com>
2017-04-03 12:23:52 -0700

    Converted te_toq to mean thermal temperature

--- tgyro/src/tgyro_pressure.f90
+++ tgyro/src/tgyro_pressure.f90
@@ -1,8 +1,19 @@
+!-------------------------------------------------------------
+! tgyro_pressure.f90
+!
+! PURPOSE:
+!  Localize the calculation of total pressure, as well as 
+!  effective temperature needed by TOQ/EPED.
+!--------------------------------------------------------------
+
 subroutine tgyro_pressure
 
   use tgyro_globals
   use tgyro_ped
   
+  implicit none
+  integer :: i_ion
+ 
   ! First compute thermal pressure
   ptot_exp = exp_ne*exp_te
   do i_ion=1,loc_n_ion
@@ -11,8 +22,14 @@ subroutine tgyro_pressure
      endif
   enddo
 
-  ! Need this for input in tgyro_eped_nn
-  te_toq = ptot_exp(1)/(2*exp_ne(1))
+  ! Mean thermal temperature on axis.  Need this for input 
+  ! in tgyro_eped_nn
+  !
+  !            p_thermal
+  ! Te_TOQ = ------------
+  !            n_thermal
+
+  te_toq = ptot_exp(1)/(exp_ne(1)+sum(exp_ni(therm_vec,1)))
 
   ! Now add fast ions
   do i_ion=1,loc_n_ion
