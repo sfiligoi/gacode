df136b83cbc08dafe12eab9c2ed3cb385cb0ae50
jcandy <jcandy@san.rr.com>
2017-05-08 19:23:33 -0700

    Optimizing loops

--- cgyro/src/cgyro_advect_wavenumber.f90
+++ cgyro/src/cgyro_advect_wavenumber.f90
@@ -14,29 +14,29 @@ subroutine cgyro_advect_wavenumber(ij)
 
   integer, intent(in) :: ij
   integer :: ir,l,ll,j
-  complex, dimension(:),allocatable :: h0
-  complex :: dh
-  
+  complex, dimension(:,:),allocatable :: h0
+  complex, dimension(nv_loc) :: dh
+
   call timer_lib_in('shear')
 
   ! Wavenumber advection ExB shear
   if (shear_method == 2) then
-     allocate(h0(1-2*n_wave:n_radial+2*n_wave))
+     allocate(h0(nv_loc,1-2*n_wave:n_radial+2*n_wave))
      ! Zero wavenumber (probably causes boundary reflection)
-     h0 = 0.0
-!$omp parallel do private(j,h0,ir,dh,l,ll,ic,c_wave)
-     do iv_loc=1,nv_loc
-        do j=1,n_theta
-           h0(1:n_radial) = h_x(ic_c(:,j),iv_loc)
-           do ir=1,n_radial
-              dh = 0.0
-              do l=1,n_wave
-                 ll = 2*l-1
-                 dh = dh+c_wave(l)*(h0(ir+ll)-h0(ir-ll))
-              enddo
-              ic = ic_c(ir,j)
-              rhs(ic,iv_loc,ij) = rhs(ic,iv_loc,ij)+omega_eb*dh
+!$omp parallel do private(j,ir,h0,dh,l,ll,ic)
+     do j=1,n_theta
+        h0 = 0.0
+        do ir=1,n_radial
+           h0(:,ir) = h_x(ic_c(ir,j),:)
+        enddo
+        do ir=1,n_radial
+           dh(:) = 0.0
+           do l=1,n_wave
+              ll = 2*l-1
+              dh(:) = dh(:)+c_wave(l)*(h0(:,ir+ll)-h0(:,ir-ll))
            enddo
+           ic = ic_c(ir,j)
+           rhs(ic,:,ij) = rhs(ic,:,ij)+omega_eb*dh(:)
         enddo
      enddo
      deallocate(h0)
@@ -46,24 +46,24 @@ subroutine cgyro_advect_wavenumber(ij)
 
   ! Wavenumber advection profile shear
   if (profile_shear_flag == 1) then
-     allocate(h0(1-2*n_wave:n_radial+2*n_wave))
+     allocate(h0(nv_loc,1-2*n_wave:n_radial+2*n_wave))
      ! Zero wavenumber (probably causes boundary reflection)
-     h0 = 0.0
-!$omp parallel do private(j,h0,ir,dh,l,ll,ic,c_wave)
-     do iv_loc=1,nv_loc
-        do j=1,n_theta
-           do ir=1,n_radial
-              h0(ir) = sum(omega_ss(:,ic_c(ir,j),iv_loc)*field(:,ic_c(ir,j)))
+!$omp parallel do private(j,ir,iv_loc,h0,dh,l,ll,ic)
+     do j=1,n_theta
+        h0 = 0.0
+        do ir=1,n_radial
+           do iv_loc=1,nv_loc
+              h0(iv_loc,ir) = sum(omega_ss(:,ic_c(ir,j),iv_loc)*field(:,ic_c(ir,j)))
            enddo
-           do ir=1,n_radial
-              dh = 0.0
-              do l=1,n_wave
-                 ll = 2*l-1
-                 dh = dh+c_wave(l)*(h0(ir+ll)-h0(ir-ll))
-              enddo
-              ic = ic_c(ir,j)
-              rhs(ic,iv_loc,ij) = rhs(ic,iv_loc,ij)+dh
+        enddo
+        do ir=1,n_radial
+           dh(:) = 0.0
+           do l=1,n_wave
+              ll = 2*l-1
+              dh(:) = dh(:)+c_wave(l)*(h0(:,ir+ll)-h0(:,ir-ll))
            enddo
+           ic = ic_c(ir,j)
+           rhs(ic,:,ij) = rhs(ic,:,ij)+dh(:)
         enddo
      enddo
      deallocate(h0)
