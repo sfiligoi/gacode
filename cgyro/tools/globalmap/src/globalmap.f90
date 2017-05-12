386e6a06d03d60f00bb2192af80eeb668a2768e7
jcandy <jcandy@san.rr.com>
2017-05-11 18:16:34 -0700

    Preliminary globalmap code working

--- cgyro/tools/globalmap/src/globalmap.f90
+++ cgyro/tools/globalmap/src/globalmap.f90
@@ -41,57 +41,45 @@ program globalmap
   open(unit=1,file='input.globalmap',status='old')
   read(1,*) rho_star
   close(1)
-  
+
   test_flag   = 1
   silent_flag = 1
   call cgyro_kernel
 
   lr = rho_star/rho*length
-  print *,'Lr',lr
-
+ 
   d_theta = (2*pi/n_theta)
   do it=1,n_theta
      theta(it) = -pi+(it-1)*d_theta
   enddo
 
+  print '(a,1pe12.5)','INFO: (globalmap) L_x   = ',lr
+  print '(a,1pe12.5)','INFO: (globalmap) rho_* = ',rho_star
+  print *
+  print '(a)',' r/a          q            Rmaj'
+
+  open(unit=1,file='out.globalmap',status='replace')
   do ir=1,n_radial
 
-       ri = rmin + (ir-(n_radial+1.0)/2)*lr/(n_radial-1)
-       dr = ri-rmin
-       
-       print *,ri,dr
+     ri = rmin + (ir-(n_radial+1.0)/2)*lr/(n_radial-1)
+     dr = ri-rmin
 
-      GEO_rmin_in  = ri
-      GEO_rmaj_in  = rmaj+shift*dr
-      GEO_drmaj_in = shift
-      GEO_zmag_in  = zmag+dzmag*dr
-      GEO_dzmag_in = dzmag
-      GEO_q_in     = q*(1+s*dr/rmin)
-      GEO_s_in     = s
-      GEO_kappa_in = kappa*(1+s_kappa*dr/rmin)
-      GEO_s_kappa_in = s_kappa
-      !  GEO_delta_in   = delta_s(ir_norm)+s_delta_s(ir_norm)/r(ir_norm)*dr
-      !  GEO_s_delta_in = s_delta_s(ir_norm)
-      !  GEO_zeta_in    = zeta_s(ir_norm)+s_zeta_s(ir_norm)/r(ir_norm)*dr
-      !  GEO_s_zeta_in  = s_zeta_s(ir_norm)
-      !  GEO_beta_star_in = beta_star_s(ir_norm)
-
-     !rmin = ra + (ir-1)*(rb-ra)/(n_radial
-
-     GEO_rmin_in      = rmin
-     GEO_rmaj_in      = rmaj
-     GEO_drmaj_in     = shift
-     GEO_zmag_in      = zmag
-     GEO_dzmag_in     = dzmag
-     GEO_q_in         = q
-     GEO_s_in         = s
-     GEO_kappa_in     = kappa
-     GEO_s_kappa_in   = s_kappa
-     GEO_delta_in     = delta
-     GEO_s_delta_in   = s_delta
-     GEO_zeta_in      = zeta
-     GEO_s_zeta_in    = s_zeta
+     GEO_rmin_in  = ri
+     GEO_rmaj_in  = rmaj+shift*dr
+     GEO_drmaj_in = shift
+     GEO_zmag_in  = zmag+dzmag*dr
+     GEO_dzmag_in = dzmag
+     GEO_q_in     = q*(1+s*dr/rmin)
+     GEO_s_in     = s
+     GEO_kappa_in = kappa*(1+s_kappa*dr/rmin)
+     GEO_s_kappa_in = s_kappa
+     GEO_delta_in   = delta*(1+s_delta*dr/rmin)
+     GEO_s_delta_in = s_delta
+     GEO_zeta_in    = zeta*(1+s_zeta*dr/rmin)
+     GEO_s_zeta_in  = s_zeta
      GEO_beta_star_in = beta_star(0)
+
+     print '(3(1pe12.5,1x))',ri,GEO_q_in,GEO_rmaj_in
 
      call GEO_do()  
 
@@ -99,11 +87,16 @@ program globalmap
 
         call GEO_interp(theta(it))     
 
-        !print *,GEO_bigr
+        write(1,10) GEO_bigr
+        write(1,10) GEO_nu
 
      enddo
   enddo
-
+  close(1)
+  print '(a,1pe12.5)','INFO: (globalmap) Wrote out.globalmap'
+  
   call MPI_FINALIZE(i_err)
 
+10 format(1pe12.5)
+  
 end program globalmap
