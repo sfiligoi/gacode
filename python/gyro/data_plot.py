feea981ce2b445efdc0ec6872f018930c711408d
Jeff Candy <jcandy@san.rr.com>
2017-08-23 14:18:40 -0700

    More work on OMFIT/GACODE plot sharing.  New data_plot_single python control for command-line plots.

--- python/gyro/data_plot.py
+++ python/gyro/data_plot.py
@@ -2,26 +2,30 @@ import data
 import sys
 import numpy as np
 from gacodeplotdefs import *
+from gacodefuncs import *
 from gyro.data import GYROData
 
-#---------------------------------------------------------------------------#
 class gyrodata_plot(data.GYROData):
-    def plot_freq(self, window=0.5, fig=None):
-        """Plot frequency vs time"""
-        """window = avg. window fraction"""
 
-        GFONTSIZE=16
-        if (fig is None):
-            fig = plt.figure()
+    def plot_freq(self,w=0.5,fig=None):
+        '''
+        Plot gamma and omega vs time
 
-        t    = self.t['(c_s/a)t']
+        ARGUMENTS:
+         w: fractional width of time window
+        '''
+
+        if fig is None:
+            fig = plt.figure(figsize=(12,6))
+
+        t = self.t['(c_s/a)t']
 
         # Read freq data
         self.read_freq()
 
         # Determine tmin
         for i in range(len(t)):
-            if t[i] < (1.0-window)*t[len(t)-1]:
+            if t[i] < (1.0-w)*t[len(t)-1]:
                 imin = i
 
         color = ['k','m','b','c']
@@ -31,11 +35,11 @@ class gyrodata_plot(data.GYROData):
         ax = fig.add_subplot(121)
         ax.grid(which="majorminor",ls=":")
         ax.grid(which="major",ls=":")
-        ax.set_xlabel(r'$(c_s/a) t$',fontsize=GFONTSIZE)
-        ax.set_ylabel(r'$(a/c_s)\gamma$',color='k',fontsize=GFONTSIZE)
+        ax.set_xlabel(r'$(c_s/a) t$')
+        ax.set_ylabel(r'$(a/c_s)\gamma$',color='k')
         #=====================================
 
-        #Gamma
+        # Gamma
         for i in range(self.profile['n_n']):
             ax.plot(t[imin:],self.freq['(a/c_s)gamma'][i,imin:],color=color[i],
                     label='gamma_n%d'%tor_n[i])
@@ -44,21 +48,27 @@ class gyrodata_plot(data.GYROData):
         ax = fig.add_subplot(122)
         ax.grid(which="majorminor",ls=":")
         ax.grid(which="major",ls=":")
-        ax.set_xlabel(r'$(c_s/a) t$',fontsize=GFONTSIZE)
-        ax.set_ylabel(r'$(a/c_s)\omega$',color='k',fontsize=GFONTSIZE)
+        ax.set_xlabel(r'$(c_s/a) t$')
+        ax.set_ylabel(r'$(a/c_s)\omega$',color='k')
         #=====================================
 
-        #Omega
+        # Omega
         for i in range(self.profile['n_n']):
             ax.plot(t[imin:],self.freq['(a/c_s)w'][i,imin:],color=color[i],
                     label='omega_n%d'%tor_n[i])
 
-    def plot_balloon(self, index='phi',tmax=-1, fig=None):
+    def plot_balloon(self,index='phi',tmax=-1,fig=None):
         '''
-        Plot the ballooning mode structure
+        Plot the ballooning eigenmode structure
+       
+        ARGUMENTS:
+         index: one of 'phi','a','aperp' or an integer
+         tmax : max value of (extended angle)/pi
+        '''
 
-        index - One of 'phi','a','aperp' or an integer
-        '''
+        if fig is None:
+            fig = plt.figure(figsize=(10,6))
+
         self.read_balloon()
 
         if index == 'phi':
@@ -87,8 +97,6 @@ class gyrodata_plot(data.GYROData):
             ytitle = r'$\delta E_\parallel$'
 
         #======================================
-        if fig is None:
-            fig = plt.figure(figsize=(10,6))
         ax = fig.add_subplot(111)
         ax.grid(which="majorminor",ls=":")
         ax.grid(which="major",ls=":")
@@ -105,19 +113,24 @@ class gyrodata_plot(data.GYROData):
         ax.plot(x,np.imag(self.balloon[key][:,0,-1]),color='m',label='Im')
 
         if tmax < 0.0:
-            ax.set_xlim([1-n_p,-1+n_p])
+            ax.set_xlim([-(n_p-1),n_p-1])
         else:
             ax.set_xlim([-tmax,tmax])
 
         ax.legend()
         return key
 
-    def plot_zf(self,w=.5,fig=None):
+    def plot_zf(self,w=0.5,fig=None):
         '''
-        plot the zonal flow?
+        Plot the zonal (n=0) potential versus time.
 
-        w - how much of normalized time to use
+        ARGUMENTS:
+         w: fractional time-average window
         '''
+
+        if fig is None:
+            fig = plt.figure(figsize=(10,6))
+        fig.subplots_adjust(left=0.1,right=0.95,top=0.95,bottom=0.12)
 
         # Read freq data
         self.read_moment_u()
@@ -125,7 +138,6 @@ class gyrodata_plot(data.GYROData):
         ntheta = self.profile['n_theta_plot']
         nx = self.profile['n_x']
 
-        print nx,ntheta
         y = np.real(self.moment_u[ntheta/3,nx/3,0,0,:])
 
         y = y[:]/y[0]
@@ -141,14 +153,10 @@ class gyrodata_plot(data.GYROData):
 
         ave = average(y[:],t,w)
         print 'INFO: (plot_zf) Integral time-average = %.6f' % ave
+        print 'INFO: (plot_zf) (nx,ntheta)=',nx,ntheta
 
         ave_vec = ave*np.ones(len(t))
         #----------------------------------------------------
-
-        #======================================
-        if fig is None:
-            fig = plt.figure(figsize=(10,6))
-        fig.subplots_adjust(left=0.1,right=0.95,top=0.95,bottom=0.12)
 
         ax = fig.add_subplot(111)
         ax.grid(which="majorminor",ls=":")
@@ -157,9 +165,55 @@ class gyrodata_plot(data.GYROData):
         ax.set_ylabel(r'$\Phi/\Phi_0$',fontsize=GFONTSIZE)
 
         ax.plot(t,y,color='k')
-        ax.plot(t[imin:],ave_vec[imin:],color='b',label=r'$\mathrm{Average}$',linewidth=1)
+        ax.plot(t[imin:],ave_vec[imin:],color='b',
+                label=r'$\mathrm{Average}$',linewidth=1)
 
         theory = 1.0/(1.0+1.6*2.0**2/np.sqrt(0.5/3.0))
-        ax.plot([0,max(t)],[theory,theory],color='grey',label=r'$\mathrm{RH \; theory}$',alpha=0.3,linewidth=4)
+        ax.plot([0,max(t)],[theory,theory],color='grey',
+                label=r'$\mathrm{RH \; theory}$',alpha=0.3,linewidth=4)
+
+        ax.legend()
+
+    def plot_phi_n0(self,lx=10,ly=6,ymax='auto',span1=-1.0,span2=-1.0,fig=None):
+        '''
+        Plot the n=0 AND n>0 potentials versus time.
+
+        ARGUMENTS:        
+         lx   : width of figure 
+         ly   : height of figure 
+         ymax : max vertical plot range
+         span1: left end of axvspan
+         span2: right end of avxspan
+        '''
+
+        fig = plt.figure(figsize=(lx,ly))
+        fig.subplots_adjust(left=0.1,right=0.96,top=0.93,bottom=0.13)
+
+        t = self.t['(c_s/a)t']
+
+        # Read data in gbflux_i and make gbflux
+        self.read_field_rms()
+
+        #======================================
+        ax = fig.add_subplot(111)
+        ax.grid(which="majorminor",ls=":")
+        ax.grid(which="major",ls=":")
+        ax.set_xlabel(r'$(c_s/a) t$',fontsize=GFONTSIZE)
+        ax.set_ylabel(r'$\langle e \phi/T_e \rangle/\rho_\star $',color='k',fontsize=GFONTSIZE)
+        #=====================================
+
+        # rho_* for normalization
+        x=np.average(self.profile['rho_s'])
+
+        ax.plot(t,self.field_rms[0,:]/x,color='k',label=r'$n=0$')
+        ax.plot(t,self.field_rms[1,:]/x,color='purple',label=r'$n>0$')
+
+        if span1 > 0.0:
+           ax.axvspan(span1,span2,facecolor='g',alpha=0.1)
+    
+        ax.set_xlim([0,t[-1]])
+ 
+        if ymax != 'auto':
+           ax.set_ylim([0,float(ymax)])
 
         ax.legend()
---
+++ python/gyro/data_plot_single.py
@@ -0,0 +1,60 @@
+import sys
+import numpy as np
+from gacodeplotdefs import *
+from gyro.data_plot import gyrodata_plot
+
+plot_type = sys.argv[1]
+
+if plot_type == 'freq':
+
+   simdir = sys.argv[2]
+   w      = float(sys.argv[3])
+   ftype  = sys.argv[4]
+
+   gyrodata_plot(simdir).plot_freq(w=w)
+
+   outfile = 'freq.'+ftype
+
+elif plot_type == 'balloon':
+
+   simdir = sys.argv[2]
+   index  = sys.argv[3]
+   ftype  = sys.argv[4]
+   tmax   = float(sys.argv[5])
+
+   key = gyrodata_plot(simdir).plot_balloon(index=index,tmax=tmax)
+
+   outfile = key+'.'+ftype
+
+elif plot_type == 'zf':
+
+   simdir = sys.argv[2]
+   w      = float(sys.argv[3])
+   ftype  = sys.argv[4]
+   
+   gyrodata_plot(simdir).plot_zf(w=w)
+
+   outfile = 'zf.'+ftype
+
+elif plot_type == 'phi_n0':
+
+   simdir = sys.argv[2]
+   lx     = float(sys.argv[3])
+   ly     = float(sys.argv[4])
+   ymax   = sys.argv[5]
+   span1  = float(sys.argv[6])
+   span2  = float(sys.argv[7])
+   ftype  = sys.argv[8]
+
+   gyrodata_plot(simdir).plot_phi_n0(lx=lx,ly=ly,ymax=ymax,span1=span1,span2=span2)
+
+   outfile = 'phi_n0.'+ftype
+  
+
+#---------------------------------------------------------------
+# Plot to screen or to image file
+if ftype == 'screen':
+    plt.show()
+else:
+    plt.savefig(outfile)
+#---------------------------------------------------------------
