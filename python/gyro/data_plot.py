a552f44dbda49dd15322e976715bdcec7d494b8e
Sterling Smith <smithsp@fusion.gat.com>
2017-08-22 23:41:35 -0700

    Converted GYRO zf plotting to class method

--- python/gyro/data_plot.py
+++ python/gyro/data_plot.py
@@ -111,3 +111,55 @@ class gyrodata_plot(data.GYROData):
 
         ax.legend()
         return key
+
+    def plot_zf(self,w=.5,fig=None):
+        '''
+        plot the zonal flow?
+
+        w - how much of normalized time to use
+        '''
+
+        # Read freq data
+        self.read_moment_u()
+
+        ntheta = self.profile['n_theta_plot']
+        nx = self.profile['n_x']
+
+        print nx,ntheta
+        y = np.real(self.moment_u[ntheta/3,nx/3,0,0,:])
+
+        y = y[:]/y[0]
+        t = self.t['(c_s/a)t']
+
+        #----------------------------------------------------
+        # Average calculations
+
+        # Determine tmin
+        for i in range(len(t)):
+            if t[i] < (1.0-w)*t[len(t)-1]:
+                imin = i
+
+        ave = average(y[:],t,w)
+        print 'INFO: (plot_zf) Integral time-average = %.6f' % ave
+
+        ave_vec = ave*np.ones(len(t))
+        #----------------------------------------------------
+
+        #======================================
+        if fig is None:
+            fig = plt.figure(figsize=(10,6))
+        fig.subplots_adjust(left=0.1,right=0.95,top=0.95,bottom=0.12)
+
+        ax = fig.add_subplot(111)
+        ax.grid(which="majorminor",ls=":")
+        ax.grid(which="major",ls=":")
+        ax.set_xlabel(r'$(c_s/a)\, t$',fontsize=GFONTSIZE)
+        ax.set_ylabel(r'$\Phi/\Phi_0$',fontsize=GFONTSIZE)
+
+        ax.plot(t,y,color='k')
+        ax.plot(t[imin:],ave_vec[imin:],color='b',label=r'$\mathrm{Average}$',linewidth=1)
+
+        theory = 1.0/(1.0+1.6*2.0**2/np.sqrt(0.5/3.0))
+        ax.plot([0,max(t)],[theory,theory],color='grey',label=r'$\mathrm{RH \; theory}$',alpha=0.3,linewidth=4)
+
+        ax.legend()
