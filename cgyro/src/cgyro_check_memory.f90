b8353da8ffbc1b9fb1f80c2ffbb1bdce422b7ce8
Igor Sfiligoi <igor.sfiligoi@ga.com>
2017-08-25 11:17:21 -0700

    Add fpack and gpack to cgyro_check_memory. This memory use was hidden before

--- cgyro/src/cgyro_check_memory.f90
+++ cgyro/src/cgyro_check_memory.f90
@@ -106,9 +106,13 @@ subroutine cgyro_check_memory(datafile)
         if (nonlinear_method == 1) then
            call cgyro_alloc_add(io,nc*nsplit*n_toroidal*16.0,'f_nl')
            call cgyro_alloc_add(io,nc*nsplit*n_toroidal*16.0,'g_nl')
+           call cgyro_alloc_add(io,nc*nsplit*n_toroidal*16.0,'fpack')
+           call cgyro_alloc_add(io,nc*nsplit*n_toroidal*16.0,'gpack')
         else
            call cgyro_alloc_add(io,n_radial*nsplit*n_toroidal*16.0,'f_nl')
            call cgyro_alloc_add(io,n_radial*nsplit*n_toroidal*16.0,'g_nl')
+           call cgyro_alloc_add(io,n_radial*nsplit*n_toroidal*16.0,'fpack')
+           call cgyro_alloc_add(io,n_radial*nsplit*n_toroidal*16.0,'gpack')
         endif
      endif
 
