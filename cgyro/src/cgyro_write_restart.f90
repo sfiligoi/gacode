ac66d78e429ca35d69971e70fcf9f171240f27b1
Igor Sfiligoi <Igor.Sfiligoi@ga.com>
2017-09-13 09:59:40 -0700

    Remove support for old-style restart files

--- cgyro/src/cgyro_write_restart.f90
+++ cgyro/src/cgyro_write_restart.f90
@@ -21,12 +21,6 @@ subroutine cgyro_write_restart
     else
        call cgyro_write_restart_v2
     endif
-  elseif (restart_format==1) then
-    if (n_chunk==1) then
-       call cgyro_write_restart_one
-    else
-       call cgyro_write_restart_v1
-    endif
   else
     call cgyro_error('ERROR: (CGYRO) Invalid restart_format found.')
   endif
@@ -43,11 +37,9 @@ subroutine cgyro_write_restart_tag
   write(io,fmtstr) t_current
   close(io)
 
-  if (restart_format/=1) then
-     open(unit=io,file=trim(path)//runfile_restart_tag_version,status='replace')
-     write(io,*) restart_format
-     close(io)
-  endif
+  open(unit=io,file=trim(path)//runfile_restart_tag_version,status='replace')
+  write(io,*) restart_format
+  close(io)
 
 end subroutine cgyro_write_restart_tag
 
@@ -206,12 +198,6 @@ subroutine cgyro_write_restart_v2
   !
   if (i_proc == 0) then
      call cgyro_write_restart_tag
-
-     ! remove ambiguity
-     if (input_restart_format==1) then
-        call cgyro_del_restart_v1
-        input_restart_format=0 ! avoid doing it in the next iteration
-     endif
   endif
   !---------------------------------------------------------
 
@@ -231,129 +217,4 @@ subroutine cgyro_del_restart_v2
   enddo
 end subroutine cgyro_del_restart_v2
 
-! --------------------------------------------------------------
-! cgyro_write_restart_v1 is deprecated (As of Aug 2017)
-! Leaving in the code for now for testing/debugging reasons
-! Should be removed in the near future
-
-subroutine cgyro_write_restart_v1
-
-  use mpi
-  use cgyro_globals
-  use cgyro_io
-
-  !----------------------------------------------
-  implicit none
-  !
-  ! Required for MPI-IO:
-  !
-  integer :: filemode
-  integer :: finfo
-  integer :: fhv
-  integer :: fstatus(MPI_STATUS_SIZE)
-  integer(kind=MPI_OFFSET_KIND) :: disp
-  integer(kind=MPI_OFFSET_KIND) :: offset1
-  !
-  ! File chunking variables
-  !
-  integer :: i1,i2,j,n_loc,n_remain
-  complex, dimension(:,:), allocatable :: h_chunk
-  !----------------------------------------------
-
-  !-----------------------------------------------
-  ! Dump h and blending coefficients:
-  !
-  filemode = IOR(MPI_MODE_WRONLY,MPI_MODE_CREATE)
-  disp     = 0
-
-  n_loc = nc/n_chunk
-  n_remain = nc-n_loc*n_chunk
-  allocate(h_chunk(n_loc+n_remain,nv_loc))
-
-  offset1 = size(h_chunk,kind=MPI_OFFSET_KIND)*i_proc
-
-  if (offset1<0) then
-     call cgyro_error('ERROR: (CGYRO) overflow detected in cgyro_write_restart_v1')
-     return
-  endif
-
-  do j=1,n_chunk
-
-     call MPI_INFO_CREATE(finfo,i_err)
-
-     call MPI_INFO_SET(finfo,"striping_factor",mpiio_stripe_str,i_err)
-
-     call MPI_FILE_OPEN(CGYRO_COMM_WORLD,&
-          trim(path)//runfile_restart//rtag(j),&
-          filemode,&
-          finfo,&
-          fhv,&
-          i_err)
-
-     call MPI_FILE_SET_VIEW(fhv,&
-          disp,&
-          MPI_COMPLEX16,&
-          MPI_COMPLEX16,&
-          'native',&
-          finfo,&
-          i_err)
-
-     i1 = 1+(j-1)*n_loc
-     i2 = j*n_loc
-     if (j == n_chunk) then
-        i2 = nc
-        n_loc = n_loc+n_remain
-     endif
-
-     h_chunk(1:n_loc,:) = h_x(i1:i2,:)
-
-     call MPI_FILE_WRITE_AT(fhv,&
-          offset1,&
-          h_chunk,&
-          size(h_chunk),&
-          MPI_COMPLEX16,&
-          fstatus,&
-          i_err)
-     if (i_err /= 0) then
-       call cgyro_error('ERROR: (CGYRO) MPI_FILE_WRITE_AT in cgyro_write_restart_v1 failed')
-        return
-     endif
-
-     call MPI_FILE_SYNC(fhv,i_err)
-     call MPI_FILE_CLOSE(fhv,i_err)
-     call MPI_INFO_FREE(finfo,i_err)
-
-  enddo
-
-  deallocate(h_chunk)
-
-  !---------------------------------------------------------
-  ! Dump restart parameters to ASCII file.
-  !
-  if (i_proc == 0) then
-     call cgyro_write_restart_tag
-
-     ! remove ambiguity
-     if (input_restart_format==2) then
-        call cgyro_del_restart_v2
-        input_restart_format=0 ! avoid doing it in the next iteration
-     endif
-  endif
-  !---------------------------------------------------------
-
-end subroutine cgyro_write_restart_v1
-
-subroutine cgyro_del_restart_v1
-  use mpi
-  use cgyro_globals
-  use cgyro_io
-
-  integer :: j
-  integer :: stat
-
-  do j=1,n_chunk
-    open(unit=io,file=trim(path)//runfile_restart//rtag(j),iostat=stat,status='old')
-    if (stat == 0) close(io, status='delete')
-  enddo
-end subroutine cgyro_del_restart_v1
 
