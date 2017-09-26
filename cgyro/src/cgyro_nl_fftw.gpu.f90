6fb6ce633b5844f567e38c533e29f0557fcfbcfb
Igor Sfiligoi <igor.sfiligoi@ga.com>
2017-09-26 16:52:28 -0400

    Create and use cgyro_nl_fftw_comm2 in cgyro_nl_fftw.gpu.f90

--- cgyro/src/cgyro_nl_fftw.gpu.f90
+++ cgyro/src/cgyro_nl_fftw.gpu.f90
@@ -39,6 +39,36 @@ subroutine cgyro_nl_fftw_comm1
   call parallel_slib_f_nc(fpack,f_nl)
 end subroutine cgyro_nl_fftw_comm1
 
+subroutine cgyro_nl_fftw_comm2
+  use timer_lib
+  use parallel_lib
+  use cgyro_globals
+
+  implicit none
+
+  integer :: ir,it,iv_loc_m,ic_loc_m
+  integer :: iexch
+
+!$omp parallel do private(it,ir,iexch,ic_loc_m,iv_loc_m)
+  do iv_loc_m=1,nv_loc
+     do it=1,n_theta
+        iexch = it + (iv_loc_m-1)*n_theta
+        do ir=1,n_radial
+           ic_loc_m = it + (ir-1)*n_theta
+           gpack(ir,iexch) = psi(ic_loc_m,iv_loc_m)
+        enddo
+     enddo
+  enddo
+
+  do iexch=nv_loc*n_theta+1,nsplit*n_toroidal
+     gpack(:,iexch) = (0.0,0.0)
+  enddo
+
+  call parallel_slib_f_nc(gpack,g_nl)
+
+end subroutine cgyro_nl_fftw_comm2
+
+
 subroutine cgyro_nl_fftw(ij)
 
   use precision_m
@@ -62,23 +92,7 @@ subroutine cgyro_nl_fftw(ij)
   include 'fftw3.f03'
 
   call timer_lib_in('nl_comm')
-
-!$omp parallel do private(it,ir,iexch,ic_loc)
-  do iv_loc=1,nv_loc
-     do it=1,n_theta
-        iexch = it + (iv_loc-1)*n_theta
-        do ir=1,n_radial
-           ic_loc = it + (ir-1)*n_theta
-           gpack(ir,iexch) = psi(ic_loc,iv_loc)
-        enddo
-     enddo
-  enddo
-
-  do iexch=nv_loc*n_theta+1,nsplit*n_toroidal
-     gpack(:,iexch) = (0.0,0.0)
-  enddo
-
-  call parallel_slib_f_nc(gpack,g_nl)
+  call cgyro_nl_fftw_comm2
   call timer_lib_out('nl_comm')
 
   call timer_lib_in('nl')
