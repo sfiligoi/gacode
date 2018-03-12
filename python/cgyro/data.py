1c0a3fc76dab67719365c5c36964bd925da91887
Jeff Candy <jcandy@san.rr.com>
2018-03-12 14:46:49 -0700

    Improved CGYRO test mode

--- python/cgyro/data.py
+++ python/cgyro/data.py
@@ -312,11 +312,10 @@ class cgyrodata:
       #-----------------------------------------------------------------
       # Read complex fields
       #
-        
       # 1a. kxky_phi
       nd = 2*self.n_radial*self.theta_plot*self.n_n*nt
       t,fmt,data = self.extract('.cgyro.kxky_phi')
-      if fmt != 'null':  
+      if fmt != 'null':
          print 'INFO: (data.py) Read data in '+fmt+'.cgyro.kxky_phi. '+t
          self.kxky_phi = np.reshape(data[0:nd],(2,self.n_radial,self.theta_plot,self.n_n,nt),'F')
          self.kxky_phi_abs = np.sqrt(self.kxky_phi[0,:,:,:,:]**2+self.kxky_phi[1,:,:,:,:]**2)
@@ -324,7 +323,7 @@ class cgyrodata:
       # 1b. kxky_apar
       nd = 2*self.n_radial*self.theta_plot*self.n_n*nt
       t,fmt,data = self.extract('.cgyro.kxky_apar')
-      if fmt != 'null':  
+      if fmt != 'null':
          print 'INFO: (data.py) Read data in '+fmt+'.cgyro.kxky_apar. '+t
          self.kxky_apar = np.reshape(data[0:nd],(2,self.n_radial,self.theta_plot,self.n_n,nt),'F')
          self.kxky_apar_abs = np.sqrt(self.kxky_apar[0,:,:,:,:]**2+self.kxky_apar[1,:,:,:,:]**2)
@@ -332,16 +331,16 @@ class cgyrodata:
       # 1c. kxky_bpar
       nd = 2*self.n_radial*self.theta_plot*self.n_n*nt
       t,fmt,data = self.extract('.cgyro.kxky_bpar')
-      if fmt != 'null':  
+      if fmt != 'null':
          print 'INFO: (data.py) Read data in '+fmt+'.cgyro.kxky_bpar. '+t
          self.kxky_bpar = np.reshape(data[0:nd],(2,self.n_radial,self.theta_plot,self.n_n,nt),'F')
          self.kxky_bpar_abs = np.sqrt(self.kxky_bpar[0,:,:,:,:]**2+self.kxky_bpar[1,:,:,:,:]**2)
-       
+
       # 2. kxky_n
       nd = 2*self.n_radial*self.theta_plot*self.n_species*self.n_n*nt
       t,fmt,data = self.extract('.cgyro.kxky_n')
-           
-      if fmt != 'null':  
+
+      if fmt != 'null':
          print 'INFO: (data.py) Read data in '+fmt+'.cgyro.kxky_n.   '+t
          self.kxky_n = np.reshape(data[0:nd],(2,self.n_radial,self.theta_plot,self.n_species,self.n_n,nt),'F')
 
@@ -349,24 +348,20 @@ class cgyrodata:
       nd = 2*self.n_radial*self.theta_plot*self.n_species*self.n_n*nt
       t,fmt,data = self.extract('.cgyro.kxky_e')
 
-      if fmt != 'null':  
+      if fmt != 'null':
          print 'INFO: (data.py) Read data in '+fmt+'.cgyro.kxky_e.   '+t
          self.kxky_e = np.reshape(data[0:nd],(2,self.n_radial,self.theta_plot,self.n_species,self.n_n,nt),'F')
       #-----------------------------------------------------------------
 
    def getgeo(self):
 
-      """Read geometry arrays"""
+      """Read theta-dependent geometry functions"""
 
       import numpy as np
-
-      # Convenience definition
-      nt = self.n_time
 
       self.geotag = []
 
       #-----------------------------------------------------------------
-      # Read powers
       #
       try:
          data = np.fromfile(self.dir+'out.cgyro.geo',dtype='float32',sep=' ')
