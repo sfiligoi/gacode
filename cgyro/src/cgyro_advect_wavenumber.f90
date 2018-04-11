1cbaacd3b2e7e70b2164e67beeda99381e9a77af
Igor Sfiligoi <igor.sfiligoi@ga.com>
2018-04-11 12:10:10 -0700

    Larger OMP kernel for cgyro_advect_wavenumber

--- cgyro/src/cgyro_advect_wavenumber.f90
+++ cgyro/src/cgyro_advect_wavenumber.f90
@@ -19,97 +19,91 @@ subroutine cgyro_advect_wavenumber(ij)
   complex :: dh
   real :: scale
 
-
-  call timer_lib_in('shear')
-  allocate(he(n_theta,1-2*n_wave:n_radial+2*n_wave))
-
-  ! Wavenumber advection ExB shear
-
-  if (shear_method == 2) then
-    !ic_c(ir,j) = j + (ir-1)*n_theta
-
-!$omp parallel do private(j,ir,in,icc,l,ll,he)
-    do in=1,nv_loc
-      he(:,1-2*n_wave:0) =0.0
-      do ir=1,n_radial
-        !icc =ic_c(ir,1)-1
-        icc = (ir-1)*n_theta
-        do j=1,n_theta
-           he(j,ir) = omega_eb*h_x(icc+j,in)
-        enddo
-      enddo
-      he(:,n_radial+1:n_radial+2*n_wave) =0.0
-
-      do ir=1,n_radial
-         !icc =ic_c(ir,1)-1
-         icc = (ir-1)*n_theta
-         do l=1,n_wave
-            ll = 2*l-1
-            do j=1,n_theta
-               rhs(icc+j,in,ij) = rhs(icc+j,in,ij)+c_wave(l)*(he(j,ir+ll)-he(j,ir-ll))
-            enddo
-         enddo
-      enddo
-    enddo
-  endif
-
-  ! Wavenumber advection profile shear
-
-  if (profile_shear_flag == 1) then
-    !ic_c(ir,j) = j + (ir-1)*n_theta
-
-!$omp parallel do private(j,ir,in,icc,l,ll,he)
-    do in=1,nv_loc
-      he(:,1-2*n_wave:0) =0.0
-      do ir=1,n_radial
-        !icc =ic_c(ir,1)-1
-        icc = (ir-1)*n_theta
-        do j=1,n_theta
-           he(j,ir) = sum(omega_ss(:,icc+j,in)*field(:,icc+j))
-        enddo
-      enddo
-      he(:,n_radial+1:n_radial+2*n_wave) =0.0
-
-      do ir=1,n_radial
-         !icc =ic_c(ir,1)-1
-         icc = (ir-1)*n_theta
-         do l=1,n_wave
-            ll = 2*l-1
-            do j=1,n_theta
-               rhs(icc+j,in,ij) = rhs(icc+j,in,ij)+c_wave(l)*(he(j,ir+ll)-he(j,ir-ll))
-            enddo
-         enddo
-      enddo
-    enddo
-  endif
-
-  ! Zonal damping (to reduce box-size correlation)
-
   if (profile_shear_flag == 1 .or. shear_method == 2) then
-     if (n == 0) then
-        scale = nu_global*abs(gamma_e)/2.0
+     call timer_lib_in('shear')
+     allocate(he(n_theta,1-2*n_wave:n_radial+2*n_wave))
 
-        irm = -1+1+n_radial/2
-        irp = irm+2
-        !irmc = ic_c(irm,j)-1
-        !irpc = ic_c(irp,j)-1
-        irmc = (irm-1)*n_theta
-        irpc = irmc + 2*n_theta
+!$omp parallel do private(j,ir,in,icc,l,ll,he)
+     do in=1,nv_loc
+       he(:,1-2*n_wave:0) =0.0
+       he(:,n_radial+1:n_radial+2*n_wave) =0.0
 
-!$omp parallel do private(in,j,dh)
-        do in=1,nv_loc
+       ! Wavenumber advection ExB shear
+
+       if (shear_method == 2) then
+         !ic_c(ir,j) = j + (ir-1)*n_theta
+
+         do ir=1,n_radial
+           !icc =ic_c(ir,1)-1
+           icc = (ir-1)*n_theta
            do j=1,n_theta
+             he(j,ir) = omega_eb*h_x(icc+j,in)
+           enddo
+         enddo
+
+         do ir=1,n_radial
+           !icc =ic_c(ir,1)-1
+           icc = (ir-1)*n_theta
+           do l=1,n_wave
+              ll = 2*l-1
+              do j=1,n_theta
+                rhs(icc+j,in,ij) = rhs(icc+j,in,ij)+c_wave(l)*(he(j,ir+ll)-he(j,ir-ll))
+              enddo
+           enddo
+         enddo
+       endif
+
+       ! Wavenumber advection profile shear
+
+       if (profile_shear_flag == 1) then
+         !ic_c(ir,j) = j + (ir-1)*n_theta
+ 
+         do ir=1,n_radial
+           !icc =ic_c(ir,1)-1
+           icc = (ir-1)*n_theta
+           do j=1,n_theta
+              he(j,ir) = sum(omega_ss(:,icc+j,in)*field(:,icc+j))
+           enddo
+         enddo
+
+         do ir=1,n_radial
+           !icc =ic_c(ir,1)-1
+           icc = (ir-1)*n_theta
+           do l=1,n_wave
+             ll = 2*l-1
+             do j=1,n_theta
+                rhs(icc+j,in,ij) = rhs(icc+j,in,ij)+c_wave(l)*(he(j,ir+ll)-he(j,ir-ll))
+              enddo
+           enddo
+         enddo
+       endif
+
+       ! Zonal damping (to reduce box-size correlation)
+
+       if (n == 0) then
+         scale = nu_global*abs(gamma_e)/2.0
+
+         irm = -1+1+n_radial/2
+         irp = irm+2
+         !irmc = ic_c(irm,j)-1
+         !irpc = ic_c(irp,j)-1
+         irmc = (irm-1)*n_theta
+         irpc = irmc + 2*n_theta
+
+         do j=1,n_theta
              dh = scale*(h_x(irpc+j,in)-h_x(irmc+j,in))
              ! p = +1
              rhs(irpc+j,in,ij) = rhs(irpc+j,in,ij)-dh
              ! p = -1
              rhs(irmc+j,in,ij) = rhs(irmc+j,in,ij)+dh
-            enddo
-        enddo
-     endif
-  endif
+         enddo
+       endif
 
-  deallocate(he)
-  call timer_lib_out('shear')
+    enddo ! in=1,nv_loc
+
+    deallocate(he)
+    call timer_lib_out('shear')
+
+  endif 
 
 end subroutine cgyro_advect_wavenumber
