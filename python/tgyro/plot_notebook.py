8d8a54f75c01b0e4c7c3802e2de4f64d941f77f9
Jeff Candy <jcandy@san.rr.com>
2018-04-24 17:21:34 -0700

    Fixed consistent profile mode in TGYRO

--- python/tgyro/plot_notebook.py
+++ python/tgyro/plot_notebook.py
@@ -26,6 +26,9 @@ rc('font',size=18)
 simdir = sys.argv[1]
 units  = int(sys.argv[2])
 ext    = sys.argv[3]
+nstr   = sys.argv[4]
+
+n = int(nstr)
 
 sim = tgyrodata(simdir)
 
@@ -34,7 +37,6 @@ print 'Number of radii : ',sim.n_r
 print 'Evolution eqns  : ',sim.n_evolve
 print 'Completed iter  : ',sim.n_iterations
 
-n   = sim.n_iterations
 x   = sim.data['r/a'][0]
 ggb = sim.data['Gamma_GB'][n]
 qgb = sim.data['Q_GB'][n]
@@ -44,7 +46,24 @@ n_ion = sim.n_ion
 
 wdir = os.path.realpath(simdir)
 
-def plot_input_profiles(ax,tag):
+if n == -1:
+   fin = r'$\mathtt{iter=final}$'
+else:
+   fin = r'$\mathtt{iter='+nstr+'}$'
+
+init=r'$\mathtt{iter=0}$'
+  
+def plot_select(ax,tag):
+   if 'flux' in tag:
+      plot_flux(ax,tag)
+   if 'z' in tag:
+      plot_z(ax,tag)
+   else:
+      plot_smooth(ax,tag)
+  
+def plot_input_profiles(ax,tag,scale=0):
+
+   # Helper routine to plot data (tag) from input.profiles
 
    f0 = 'input.profiles.'+str(0)
    fn = 'input.profiles.'+str(n)
@@ -57,121 +76,157 @@ def plot_input_profiles(ax,tag):
       prof = profiles_genData('input.profiles')
       
    xp = prof.data['rmin']
+   
+   snorm = max(xp)**scale
+    
    xp = xp/max(xp)
-   ax.plot(xp,prof.data[tag],color=color,alpha=alpha,linewidth=width,
+
+   ax.plot(xp,prof.data[tag]*snorm,color=color,alpha=alpha,linewidth=width,
            label=r'$\mathbf{'+label+'}$')
 
    if os.path.isfile(fn):
       prof = profiles_genData(fn)
-      ax.plot(xp,prof.data[tag],color=color,alpha=alpha,linewidth=width,
+      ax.plot(xp,prof.data[tag]*snorm,color=color,alpha=alpha,linewidth=width,
               label=r'$\mathbf{'+label+'}$')
 
-def plot_gen(ax,tag):
+def plot_z(ax,tag):
 
-    ax.grid(which="major",ls="-",alpha=0.4)
-    ax.grid(which="minor",ls=":",alpha=0.4)
-    ax.set_xlabel('$r/a$')
+   # Gradient scale lengths
+
+   ax.grid(which="major",ls="-",alpha=0.1,linewidth=2)
+   ax.grid(which="minor",ls=":",alpha=0.1,linewidth=2)
+   ax.set_xlabel('$r/a$')
+
+   if tag == 'zte':
+      ax.plot(x,sim.data['a/Lte'][0],color='k',label=init)
+      ax.plot(x,sim.data['a/Lte'][n],color='magenta',label=fin)
+      ax.set_ylabel('$z_\mathrm{Te} = a/L_\mathrm{Te}$',color='k')
+      plot_input_profiles(ax,'dlntedr',1)
+   elif tag == 'zti':
+      ax.plot(x,sim.data['a/Lti1'][0],color='k',label=init)
+      ax.plot(x,sim.data['a/Lti1'][n],color='magenta',label=fin)
+      ax.set_ylabel('$z_\mathrm{Ti} = a/L_\mathrm{Ti}$',color='k')
+      plot_input_profiles(ax,'dlntidr_1',1)
+   elif tag == 'zne':
+      ax.plot(x,sim.data['a/Lne'][0],color='k',label=init)
+      ax.plot(x,sim.data['a/Lne'][n],color='magenta',label=fin)
+      ax.set_ylabel('$z_\mathrm{ne} = a/L_\mathrm{ne}$',color='k')
+      plot_input_profiles(ax,'dlnnedr',1)
+
+   ax.set_ylim([0.0,10.0])
+   ax.legend(loc=2)
+   plt.tight_layout
+
+def plot_flux(ax,tag):
+
+   # Fluxes
+
+   ax.grid(which="major",ls="-",alpha=0.1,linewidth=2)
+   ax.grid(which="minor",ls=":",alpha=0.1,linewidth=2)
+   ax.set_xlabel('$r/a$')
  
-    tot=r'$\mathbf{total}$'
-    tar=r'$\mathbf{target}$'
+   tot=r'$\mathbf{total}$'
+   tar=r'$\mathbf{target}$'
 
-    if tag == 'eflux_e_target':
-        if units == 0:
-            ax.plot(x,sim.data['eflux_e_tot'][n],label=tot)
-            ax.plot(x,sim.data['eflux_e_target'][n],label=tar)
-            ax.set_ylabel('$Q_e/Q_{GB}$',color='k')
-        else:
-            ax.plot(x,sim.data['eflux_e_tot'][n]*qgb,label=tot)
-            ax.plot(x,sim.data['eflux_e_target'][n]*qgb,label=tar)
-            ax.set_ylabel('$Q_e [MW/m^2]$',color='k')
-    elif tag == 'eflux_i_target':
-        if units == 0:
-            ax.plot(x,sim.data['eflux_i_tot'][n],label=tot)
-            ax.plot(x,sim.data['eflux_i_target'][n],label=tar)
-            ax.set_ylabel('$Q_i/Q_{GB}$',color='k')
-        else:
-            ax.plot(x,sim.data['eflux_i_tot'][n]*qgb,label=tot)
-            ax.plot(x,sim.data['eflux_i_target'][n]*qgb,label=tar)
-            ax.set_ylabel('$Q_i~[MW/m^2]$',color='k')
-    elif tag == 'pflux_e_target':
-        if units == 0:
-            ax.plot(x,sim.data['pflux_e_tot'][n],label=tot)
-            ax.plot(x,sim.data['pflux_e_target'][n],label=tar)
-            ax.set_ylabel('$\Gamma_e/\Gamma_{GB}$',color='k')
-        else:
-            ax.plot(x,sim.data['pflux_e_tot'][n]*ggb,label=tot)
-            ax.plot(x,sim.data['pflux_e_target'][n]*ggb,label=tar)
-            ax.set_ylabel('$\Gamma_e~[10^{19}/m^2/s]$',color='k')
+   if tag == 'eflux_e_target':
+      if units == 0:
+         ax.plot(x,sim.data['eflux_e_tot'][n],label=tot)
+         ax.plot(x,sim.data['eflux_e_target'][n],label=tar)
+         ax.set_ylabel('$Q_e/Q_{GB}$',color='k')
+      else:
+         ax.plot(x,sim.data['eflux_e_tot'][n]*qgb,label=tot)
+         ax.plot(x,sim.data['eflux_e_target'][n]*qgb,label=tar)
+         ax.set_ylabel('$Q_e [MW/m^2]$',color='k')
+   elif tag == 'eflux_i_target':
+      if units == 0:
+         ax.plot(x,sim.data['eflux_i_tot'][n],label=tot)
+         ax.plot(x,sim.data['eflux_i_target'][n],label=tar)
+         ax.set_ylabel('$Q_i/Q_{GB}$',color='k')
+      else:
+         ax.plot(x,sim.data['eflux_i_tot'][n]*qgb,label=tot)
+         ax.plot(x,sim.data['eflux_i_target'][n]*qgb,label=tar)
+         ax.set_ylabel('$Q_i~[MW/m^2]$',color='k')
+   elif tag == 'pflux_e_target':
+      if units == 0:
+         ax.plot(x,sim.data['pflux_e_tot'][n],label=tot)
+         ax.plot(x,sim.data['pflux_e_target'][n],label=tar)
+         ax.set_ylabel('$\Gamma_e/\Gamma_{GB}$',color='k')
+      else:
+         ax.plot(x,sim.data['pflux_e_tot'][n]*ggb,label=tot)
+         ax.plot(x,sim.data['pflux_e_target'][n]*ggb,label=tar)
+         ax.set_ylabel('$\Gamma_e~[10^{19}/m^2/s]$',color='k')
             
-    for i in range(n_ion):
-        pstr = 'pflux_i'+str(i+1)
-        if tag == pstr+'_target':
-            if units == 0:
-                ax.plot(x,sim.data[pstr+'_tot'][n],label=tot)
-                ax.plot(x,sim.data[pstr+'_target'][n],label=tar)
-                ax.set_ylabel('$\Gamma_{i'+str(i+1)+'}/\Gamma_{GB}$',color='k')
-            else:
-                ax.plot(x,sim.data[pstr+'_tot'][n]*ggb,label=tot)
-                ax.plot(x,sim.data[pstr+'_target'][n]*ggb,label=tar)
-                ax.set_ylabel('$\Gamma_{i'+str(i+1)+'}~[10^{19}/m^2/s]$',color='k')
-            break
+   for i in range(n_ion):
+      pstr = 'pflux_i'+str(i+1)
+      if tag == pstr+'_target':
+         if units == 0:
+            ax.plot(x,sim.data[pstr+'_tot'][n],label=tot)
+            ax.plot(x,sim.data[pstr+'_target'][n],label=tar)
+            ax.set_ylabel('$\Gamma_{i'+str(i+1)+'}/\Gamma_{GB}$',color='k')
+         else:
+            ax.plot(x,sim.data[pstr+'_tot'][n]*ggb,label=tot)
+            ax.plot(x,sim.data[pstr+'_target'][n]*ggb,label=tar)
+            ax.set_ylabel('$\Gamma_{i'+str(i+1)+'}~[10^{19}/m^2/s]$',color='k')
+         break
 
-    # Smooth curves
+   ax.legend(loc=2)
+   plt.tight_layout
 
-    init=r'$\mathbf{initial}$'
-    fin=r'$\mathbf{final}$'
+def plot_smooth(ax,tag):
 
-    if tag == 'te':
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lte'][0],sim.data['te'][0],64)
-        ax.plot(xf,pf,color='black',label=init)
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lte'][n],sim.data['te'][n],64)
-        ax.plot(xf,pf,color='magenta',label=fin)
-        ax.set_ylabel(r'$\mathrm{T_e~[keV]}$')
-        # Dots
-        ax.plot(sim.data['r/a'][0],sim.data['te'][0],'o',color='k')
-        ax.plot(sim.data['r/a'][0],sim.data['te'][n],'o',color='k')
-        plot_input_profiles(ax,'Te')
+   # Smooth curves
 
-    elif tag == 'ti':
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lti1'][0],sim.data['ti1'][0],64)
-        ax.plot(xf,pf,color='black',label=init)
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lti1'][n],sim.data['ti1'][n],64)
-        ax.plot(xf,pf,color='magenta',label=fin)
-        ax.set_ylabel(r'$\mathrm{T_i~[keV]}$')
-        # Dots
-        ax.plot(sim.data['r/a'][0],sim.data['ti1'][0],'o',color='k')
-        ax.plot(sim.data['r/a'][0],sim.data['ti1'][n],'o',color='k')
-        plot_input_profiles(ax,'Ti_1')
+   ax.grid(which="major",ls="-",alpha=0.1,linewidth=2)
+   ax.grid(which="minor",ls=":",alpha=0.1,linewidth=2)
+   ax.set_xlabel('$r/a$')
 
-    elif tag == 'ne':
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lne'][0],sim.data['ne'][0],64)
-        ax.plot(xf,pf/1e13,color='black',label=init)
-        xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/Lne'][n],sim.data['ne'][n],64)
-        ax.plot(xf,pf/1e13,color='magenta',label=fin)
-        ax.set_ylabel(r'$\mathrm{n_e~[10^{19}/m^3]}$')
-        # Dots
-        ax.plot(sim.data['r/a'][0],sim.data['ne'][0]/1e13,'o',color='k')
-        ax.plot(sim.data['r/a'][0],sim.data['ne'][n]/1e13,'o',color='k')
-        plot_input_profiles(ax,'ne')
+   if tag == 'te':
+      xf,pf = smooth_pro(x,sim.data['a/Lte'][0],sim.data['te'][0],64)
+      ax.plot(xf,pf,color='black',label=init)
+      xf,pf = smooth_pro(x,sim.data['a/Lte'][n],sim.data['te'][n],64)
+      ax.plot(xf,pf,color='magenta',label=fin)
+      ax.set_ylabel(r'$\mathrm{T_e~[keV]}$')
+      # Dots
+      ax.plot(x,sim.data['te'][0],'o',color='k')
+      ax.plot(x,sim.data['te'][n],'o',color='k')
+      plot_input_profiles(ax,'Te')
+   elif tag == 'ti':
+      xf,pf = smooth_pro(x,sim.data['a/Lti1'][0],sim.data['ti1'][0],64)
+      ax.plot(xf,pf,color='black',label=init)
+      xf,pf = smooth_pro(x,sim.data['a/Lti1'][n],sim.data['ti1'][n],64)
+      ax.plot(xf,pf,color='magenta',label=fin)
+      ax.set_ylabel(r'$\mathrm{T_i~[keV]}$')
+      # Dots
+      ax.plot(x,sim.data['ti1'][0],'o',color='k')
+      ax.plot(x,sim.data['ti1'][n],'o',color='k')
+      plot_input_profiles(ax,'Ti_1')
+   elif tag == 'ne':
+      xf,pf = smooth_pro(x,sim.data['a/Lne'][0],sim.data['ne'][0],64)
+      ax.plot(xf,pf/1e13,color='black',label=init)
+      xf,pf = smooth_pro(x,sim.data['a/Lne'][n],sim.data['ne'][n],64)
+      ax.plot(xf,pf/1e13,color='magenta',label=fin)
+      ax.set_ylabel(r'$\mathrm{n_e~[10^{19}/m^3]}$')
+      # Dots
+      ax.plot(x,sim.data['ne'][0]/1e13,'o',color='k')
+      ax.plot(x,sim.data['ne'][n]/1e13,'o',color='k')
+      plot_input_profiles(ax,'ne')
 
-    for i in range(n_ion):
-        if tag == 'ni'+str(i+1):
-            xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/L'+tag][0],sim.data[tag][0],64)
-            ax.plot(xf,pf/1e13,color='black',label=init)
-            xf,pf = smooth_pro(sim.data['r/a'][0],sim.data['a/L'+tag][n],sim.data[tag][n],64)
-            ax.plot(xf,pf/1e13,color='magenta',label=fin)
-            ax.set_ylabel(r'$\mathrm{n_i~[10^{19}/m^3]}$')
-            # Dots
-            ax.plot(sim.data['r/a'][0],sim.data[tag][0]/1e13,'o',color='k')
-            ax.plot(sim.data['r/a'][0],sim.data[tag][n]/1e13,'o',color='k')
-            plot_input_profiles(ax,'ni_'+str(i+1))
-            break
+   for i in range(n_ion):
+      if tag == 'ni'+str(i+1):
+         xf,pf = smooth_pro(x,sim.data['a/L'+tag][0],sim.data[tag][0],64)
+         ax.plot(xf,pf/1e13,color='black',label=init)
+         xf,pf = smooth_pro(x,sim.data['a/L'+tag][n],sim.data[tag][n],64)
+         ax.plot(xf,pf/1e13,color='magenta',label=fin)
+         ax.set_ylabel(r'$\mathrm{n_i~[10^{19}/m^3]}$')
+         # Dots
+         ax.plot(x,sim.data[tag][0]/1e13,'o',color='k')
+         ax.plot(x,sim.data[tag][n]/1e13,'o',color='k')
+         plot_input_profiles(ax,'ni_'+str(i+1))
+         break
     
-    if 'flux' in tag:
-        ax.legend(loc=2)
-    else:
-        ax.legend(loc=1)
-
+   ax.legend(loc=1)
+   plt.tight_layout
+        
 #-------------------------------------------------------------------------------------
 
 class TabPanel(wx.Panel):
@@ -186,10 +241,10 @@ class TabPanel(wx.Panel):
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         self.SetSizer(self.sizer)
         self.Fit()
-
-    def draw(self,tag):
-        plot_gen(self.ax,tag)
         
+    def draw(self,tag):
+        plot_select(self.ax,tag)
+  
 #-------------------------------------------------------------------------------------
 
 class DemoFrame(wx.Frame):
@@ -236,6 +291,18 @@ class DemoFrame(wx.Frame):
             tab.draw('ni'+str(i+1))
             notebook.AddPage(tab,'ni'+str(i+1))
 
+        tab = TabPanel(notebook)
+        tab.draw('zte')
+        notebook.AddPage(tab,'zTe')
+
+        tab = TabPanel(notebook)
+        tab.draw('zti')
+        notebook.AddPage(tab,'zTi')
+
+        tab = TabPanel(notebook)
+        tab.draw('zne')
+        notebook.AddPage(tab,'zne')
+
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(notebook, 1, wx.ALL|wx.EXPAND, 5)
         panel.SetSizer(sizer)
@@ -265,12 +332,13 @@ if __name__ == "__main__":
     list=list+['te','ti','ne']
     for i in range(n_ion):
         list.append('ni'+str(i+1)+'_target')
+    list=list+['zte','zti','zne']
 
     for x in list:
         figure = plt.figure(figsize=(9,6))
         figure.subplots_adjust(left=0.12,right=0.95,bottom=0.16)
-        ax = figure.add_subplot(111)
-        plot_gen(ax,x)
+        ax = figure.add_subplot(111) 
+        plot_select(ax,x)
         pfile = 'out.'+x+'.'+ext
         plt.savefig(pfile)
         print 'INFO: (notebook.py) Wrote '+pfile
