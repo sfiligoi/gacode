742069f6a259cc26b9736f604e2379bfaafa50c6
Jeff Candy <jcandy@san.rr.com>
2018-05-23 16:18:45 -0700

    Fixed le3 make

--- gapy/src/gapy_surfpar.f90
+++ gapy/src/gapy_surfpar.f90
@@ -1,6 +1,12 @@
 module surfpar
-  
-  double precision :: x
+
+  integer :: iselect=16
+  integer :: nf=2
+  double precision, dimension(:), allocatable :: r,z
+  double precision, dimension(:), allocatable :: vr,vz
+  double precision, dimension(:), allocatable :: pr,pz
+  double precision, dimension(:), allocatable :: x
+  double precision, dimension(:), allocatable :: rp,zp
 
 contains
 
@@ -14,11 +20,10 @@ contains
     integer, intent(in) :: n_psi,n_arc
     double precision, intent(in), dimension(n_psi,n_arc) :: r_in,z_in
 
-    integer, parameter :: nf=2
     
     integer :: i,ix,p
     integer :: n1,m1,n2,m2,s
-    double precision, dimension(:), allocatable :: r,z,dl,ur,uz,l,vr,vz,x
+    double precision, dimension(:), allocatable :: dl,ur,uz,l
     double precision, dimension(:), allocatable :: cr,cz,sr,sz
     double precision :: rmin,rmaj,zmin,zmaj,eps,pi
     !-------------------------------------
@@ -32,23 +37,22 @@ contains
     allocate(uz(n_arc))
     allocate(vr(n_arc))
     allocate(vz(n_arc))
+    allocate(pr(n_arc))
+    allocate(pz(n_arc))
+    allocate(rp(n_arc))
+    allocate(zp(n_arc))
     allocate(l(n_arc))
     allocate(x(n_arc))
     allocate(sr(nf))
     allocate(sz(nf))
     allocate(cr(0:nf))
     allocate(cz(0:nf))
-    
-    ix = 2
+   
+    r = r_in(iselect,:)
+    z = z_in(iselect,:)
 
-    r = r_in(ix,:)
-    z = z_in(ix,:)
-
-    ! Compute arc lengths
-    do i=1,n_arc-1
-       dl(i) = sqrt((r(i+1)-r(i))**2+(z(i+1)-z(i))**2)
-    enddo
-    dl(n_arc) = dl(1)
+    r = r(n_arc:1:-1)
+    z = z(n_arc:1:-1)
 
     ! Pointwise Extrema ------------------------------
     n1 = maxloc(z,1) ; m1 = minloc(z,1)
@@ -59,18 +63,18 @@ contains
     rmaj = 0.5*(r(n2)+r(m2))
     rmin = 0.5*(r(n2)-r(m2))
 
-    ! Compute generalized angles ---------------------
-    eps = 1.0-1e-11
-    uz = asin(eps*(z-zmaj)/zmin)
-    ur = acos(eps*(r-rmaj)/rmin)
-
     s = n2
 
-    ur(1:n_arc-1) = cshift(ur(1:n_arc-1),-s)
-    uz(1:n_arc-1) = cshift(uz(1:n_arc-1),-s)
-    dl(1:n_arc-1) = cshift(dl(1:n_arc-1),-s)
-    ur(n_arc) = ur(1)
-    uz(n_arc) = uz(1)
+    ! Shift elements so that first index at max(R).
+    r(1:n_arc-1) = cshift(r(1:n_arc-1),s)
+    z(1:n_arc-1) = cshift(z(1:n_arc-1),s)
+    r(n_arc) = r(1)
+    z(n_arc) = z(1)
+
+    ! Compute arc lengths
+    do i=1,n_arc-1
+       dl(i) = sqrt((r(i+1)-r(i))**2+(z(i+1)-z(i))**2)
+    enddo
     dl(n_arc) = dl(1)
 
     !---------------------------------------------------
@@ -79,28 +83,28 @@ contains
     do i=1,n_arc-1
        l(i+1) = l(i)+dl(i)
     enddo
+    x = 2*pi*l/l(n_arc)
+
+    ! Compute generalized angles ---------------------
+    eps = 1.0-1e-9
+    uz = asin(eps*(z-zmaj)/zmin)
+    ur = acos(eps*(r-rmaj)/rmin)
 
     ! Sort out proper branches
     vr = ur ; vz = uz
-    do i=1,n_arc-1
-       if (ur(i+1) <= ur(i)) vr(i+1) = 2*pi-ur(i+1)
+    do i=1,n_arc-2
+       if (ur(i+1) < ur(i)) vr(i+1) = 2*pi-ur(i+1)
        if (uz(i+1) <= uz(i)) vz(i+1) = pi-uz(i+1)
-       if (uz(i+1) > uz(i) .and. l(i)/l(n_arc) > 0.5) then
+       if (uz(i+1) >= uz(i) .and. x(i) > pi) then
           vz(i+1) = 2*pi+uz(i+1)
        endif
     enddo
 
-    x = 2*pi*l/l(n_arc)
-
-    do i=1,n_arc
-       print *,ur(i)
-    enddo
-    
     vr = vr-x
     vz = vz-x
-
-    !print *,vr
-    
+    vr(n_arc) = vr(1)
+    vz(n_arc) = vz(1)
+     
     cr = 0.0 ; cz = 0.0
     cr(0) = moment(n_arc,vr,x*0.0+1,dl)
     cz(0) = moment(n_arc,vz,x*0.0+1,dl)
@@ -112,10 +116,15 @@ contains
        sz(p) = moment(n_arc,vz,sin(p*x),dl)
     enddo
 
-    !print *,cr
-    !print *,cz
-    !print *,sr
-    !print *,sz
+    ! Regenerage based on shape param
+    pr = cr(0) ; pz = cz(0)
+    do p=1,nf
+       pr = pr+cr(p)*cos(p*x)+sr(p)*sin(p*x)
+       pz = pz+cz(p)*cos(p*x)+sz(p)*sin(p*x)
+    enddo
+
+    rp = rmaj+rmin*cos(x+pr)
+    zp = zmaj+zmin*sin(x+pz)
     
   end subroutine surfpar_do
 
