d5bea171ba4fe549779ae1a7a4d6a1eb7463b8fe
Jeff Candy <jcandy@san.rr.com>
2018-10-03 11:09:24 -0700

    Updates for CORI

--- cgyro/bin/cgyro_parse.py
+++ cgyro/bin/cgyro_parse.py
@@ -76,7 +76,7 @@ x.add('PSYM_FLAG','0')
 x.add('PROFILE_SHEAR_FLAG','0')
 x.add('THETA_PLOT','1')
 x.add('MPIIO_SMALL_STRIPE_FACTOR','4')
-x.add('MPIIO_STRIPE_FACTOR','32')
+x.add('MPIIO_STRIPE_FACTOR','24')
 x.add('GPU_BIGMEM_FLAG','0')
 
 x.add('RMIN','0.5')
