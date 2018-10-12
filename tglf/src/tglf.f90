424aa7087047e84891c3f81e0bd8d097d4a110dd
MicheleFasciana <fascianamichele@gmail.com>
2018-10-12 09:57:16 -0700

    Added the intensity pectrum per mode and field spectrum per mode as subroutine

--- tglf/src/tglf.f90
+++ tglf/src/tglf.f90
@@ -99,6 +99,12 @@ program tglf
      ! write QL weights spectrum to file out.tglf.QL_weight_spectrum
      CALL write_tglf_QL_weight_spectrum
 
+     ! write intensity fluctuation amplitude spectrum per mode to file out.tglf.intensity_spectrum_per_mode
+     CALL write_tglf_intensity_spectrum_per_mode
+
+     ! write field fluctuation amplitude spectrum per mode to file out.tglf.field_spectrum_per_mode
+     CALL write_tglf_field_spectrum_per_mode
+
   else
 
      print 10,'     ky:',tglf_ky_in
