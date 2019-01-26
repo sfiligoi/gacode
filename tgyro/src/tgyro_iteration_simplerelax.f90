fa0e239851765e262e21ab559ea50e2c0a9abfc5
Jeff Candy <jcandy@san.rr.com>
2019-01-25 18:32:56 -0800

    Simpler simplerelax

--- tgyro/src/tgyro_iteration_simplerelax.f90
+++ tgyro/src/tgyro_iteration_simplerelax.f90
@@ -13,7 +13,7 @@ subroutine tgyro_iteration_simplerelax
   use tgyro_iteration_variables
 
   real :: simpledz
-
+  
   if (i_proc_global == 0) then
      open(unit=1,file=trim(runfile),position='append')
      write(1,'(t2,a)') 'INFO: (TGYRO) Beginning standard iterations'
@@ -50,7 +50,7 @@ subroutine tgyro_iteration_simplerelax
      call tgyro_write_data(1)
   endif
   !----------------------------------------------------
-
+  
   do i_tran_loop=1,tgyro_relax_iterations
 
      i_tran = i_tran+1
@@ -66,81 +66,33 @@ subroutine tgyro_iteration_simplerelax
      ! Relax based on (flux-target)/max(target,1)
      ! dz/z = -loc_relax*(Q_tot - Qtarget)/max(Qtarget,Qtot)
 
-     if(i_tran_loop==1)then 
-        relax(:)=loc_relax
-        do p=1,p_max
-           relax(p)=loc_relax
-        enddo
+     if (i_tran_loop == 1) then
+        relax = loc_relax
      endif
-     p=0
-     do i=2,n_r
-        
-        if (loc_ti_feedback_flag == 1) then
-           p = p+1
-           simpledz = relax(p)*(f_vec(p)-g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)            
-           if (abs(simpledz) > loc_dx_max) then
-              simpledz = loc_dx_max*(simpledz/abs(simpledz))
-           endif
-           x_vec(p) = x_vec(p)*(1.0-simpledz)
 
+     ! Construct adjustment vector
+     do p=1,p_max
+        simpledz = relax(p)*(f_vec(p)-g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)                   
+        if (abs(simpledz) > loc_dx_max) then
+           simpledz = loc_dx_max*simpledz/abs(simpledz)
         endif
-
-        if (loc_te_feedback_flag == 1) then
-           p = p+1
-           simpledz = relax(p)*(f_vec(p) - g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)
-           if (abs(simpledz) > loc_dx_max) then
-              simpledz = loc_dx_max*(simpledz/abs(simpledz))
-           endif
-           x_vec(p) = x_vec(p)*(1.0-simpledz)
-      
-        endif
-
-        if (loc_er_feedback_flag == 1) then
-           p = p+1
-           simpledz = -relax(p)*(f_vec(p) - g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)           
-           if (abs(simpledz) > loc_dx_max) then
-              simpledz = loc_dx_max*(simpledz/abs(simpledz))
-           endif
-           x_vec(p) = x_vec(p)*(1.0-simpledz)
-         
-        endif
-
-        if (evo_e(0) == 1) then
-           p = p+1
-           simpledz = relax(p)*(f_vec(p) - g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)             
-           if (abs(simpledz) > loc_dx_max) then
-              simpledz = loc_dx_max*(simpledz/abs(simpledz))
-           endif
-           x_vec(p) = x_vec(p)*(1.0-simpledz)
-                 
-        endif
-
-        do i_ion=1,loc_n_ion
-           if (evo_e(i_ion) >= 1) then
-              p = p+1 
-              simpledz = relax(p)*(f_vec(p) - g_vec(p))/sqrt(f_vec(p)**2+g_vec(p)**2)
-              if (abs(simpledz) > loc_dx_max) then
-                  simpledz = loc_dx_max*(simpledz/abs(simpledz))
-              endif
-              x_vec(p) = x_vec(p)*(1.0-simpledz)
-           endif
-        enddo
+        x_vec(p) = x_vec(p)*(1.0-simpledz)
      enddo
 
      call tgyro_profile_set(x_vec,0.0,0)
      call tgyro_profile_functions 
 
-     !----------------------------------------------------- 
      ! Update fluxes
      call tgyro_target_vector(x_vec,g_vec)
      call tgyro_flux_vector(x_vec,f_vec,0.0,0)
-     !
+     
      ! Compute initial residual
      call tgyro_residual(f_vec,g_vec,res,p_max,loc_residual_method)
      call tgyro_write_intermediate(0,res)
+
      ! Output results
      call tgyro_write_data(1)
 
   enddo
-
+  
 end subroutine tgyro_iteration_simplerelax
