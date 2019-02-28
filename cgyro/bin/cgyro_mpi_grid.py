a099c7324345ab11c1d9a95a6d50d1243a8841fe
Jeff Candy <jcandy@san.rr.com>
2019-02-28 14:50:12 -0800

    Starting python version of MPI grid tool

---
+++ cgyro/bin/cgyro_mpi_grid.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import numpy as np
+
+if len(sys.argv) == 1:
+    print('USAGE: python cgyro_mpi_grid.py')
+    print('       (assumes input.cgyro.gen in CWD')
+    sys.exit()
+
+
