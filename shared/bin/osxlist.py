6344eecf9819b48de77513596e9a3ef608d72215
Jeff Candy <jcandy@san.rr.com>
2019-02-28 13:08:48 -0500

    Summit fixes

--- shared/bin/osxlist.py
+++ shared/bin/osxlist.py
@@ -4,4 +4,4 @@ flags = fcntl.fcntl(sys.stdout, fcntl.F_GETFL)
 fcntl.fcntl(sys.stdout, fcntl.F_SETFL, flags&~os.O_NONBLOCK)
 
 with open(sys.argv[1], 'r') as f:
-    print f.read()
+    print(f.read())
