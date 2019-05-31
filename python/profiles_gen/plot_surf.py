c5bad0b3e331a7bfeafaa5bb4b5b72e03832f3be
Jeff Candy <jcandy@san.rr.com>
2019-05-30 17:53:26 -0700

    flux surface plots working

--- python/profiles_gen/plot_surf.py
+++ python/profiles_gen/plot_surf.py
@@ -1,3 +1,4 @@
+import gapy
 import sys
 import string
 import numpy as np
@@ -14,83 +15,88 @@ surf  = sys.argv[1]
 n     = int(sys.argv[2])
 ftype = sys.argv[3]
 
+# Read profiles
+gapy.expro.expro_read('input.gacode')
+nexp = int(gapy.expro.expro_n_exp) 
+nfourier = int(gapy.expro.expro_nfourier)
+
+print('nexp     = {:d}'.format(nexp))
+print('nfourier = {:d}'.format(nfourier))
+
 fig = plt.figure(figsize=(8,12))
 ax = fig.add_subplot(111,aspect='equal')
 ax.set_xlabel(r'$R$')
 ax.set_ylabel(r'$Z$')
 
-t = 2*np.pi*np.arange(0,narc)/float(narc-1)
+t = 2*np.pi*np.linspace(0,1,narc)
 
-if i0 > 0:
-    i1=0
-    i2=prof.n_exp
+if n > 0:
+   rlist = np.arange(0,nexp,n)
 else:
-    i1=i0
-    i2=i1+1
+   rlist = np.arange(abs(n),abs(n)+1)
 
-if surf == 'miller' or surf == 'both':
-    # Miller geometry flux-surfaces
-    for i in np.arange(i1,i2):
-        bigr = prof.data['rmaj'][i]  
-        bigz = prof.data['zmag'][i]  
-        r    = prof.data['rmin'][i]
-        d    = prof.data['delta'][i]
-        k    = prof.data['kappa'][i]
-        z    = prof.data['zeta'][i]
+geo_ar = gapy.expro.expro_geo[0,:,:]
+geo_br = gapy.expro.expro_geo[1,:,:]
+geo_az = gapy.expro.expro_geo[2,:,:]
+geo_bz = gapy.expro.expro_geo[3,:,:]
 
-        x = bigr+r*np.cos(t+np.arcsin(d)*np.sin(t))
-        y = bigz+k*r*np.sin(t+z*np.sin(2*t))
+if surf == 'msurf' or surf == 'surf':
+   # Miller geometry flux-surfaces
+   for i in rlist:
+      bigr = gapy.expro.expro_rmaj[i]  
+      bigz = gapy.expro.expro_zmag[i]   
+      r    = gapy.expro.expro_rmin[i]
+      d    = gapy.expro.expro_delta[i]
+      k    = gapy.expro.expro_kappa[i]
+      z    = gapy.expro.expro_zeta[i]
+      
+      x = bigr+r*np.cos(t+np.arcsin(d)*np.sin(t))
+      y = bigz+k*r*np.sin(t+z*np.sin(2*t))
 
-        if i == i1:
-            ax.plot(x,y,'-k',linewidth=1,label=r'$\mathrm{Miller}$')
-        else:
-            ax.plot(x,y,'-k',linewidth=1)
+      if i == rlist[0]:
+         ax.plot(x,y,'-k',linewidth=1,label=r'$\mathrm{Miller}$')
+      else:
+         ax.plot(x,y,'-k',linewidth=1)
 
 
-if surf == 'fourier' or surf == 'both':
-    # Fourier geometry flux-surfaces 
-    for i in np.arange(i1,i2):
-        a0r = prof.geo['ar'][0,i]
-        a0z = prof.geo['az'][0,i]
+if surf == 'fsurf' or surf == 'surf':
+   # Fourier geometry flux-surfaces 
+   for i in rlist:
+      x = geo_ar[0,i]/2
+      y = geo_az[0,i]/2
+      for j in range(nfourier):
+         p = j+1
+         ar = geo_ar[p,i]
+         br = geo_br[p,i]
+         az = geo_az[p,i]
+         bz = geo_bz[p,i]
+         x = x+ar*np.cos(p*t)+br*np.sin(p*t)
+         y = y+az*np.cos(p*t)+bz*np.sin(p*t)
 
-        x = a0r/2
-        y = a0z/2
-
-        for j in range(1,1+prof.nfourier,1):
-            ar = prof.geo['ar'][j,i]
-            br = prof.geo['br'][j,i]
-            az = prof.geo['az'][j,i]
-            bz = prof.geo['bz'][j,i]
-            x = x+ar*np.cos(j*t)+br*np.sin(j*t)
-            y = y+az*np.cos(j*t)+bz*np.sin(j*t)
-
-        if i == i1:
-            ax.plot(x,y,'-b',linewidth=1,label=r'$\mathrm{Fourier}~'+str(prof.nfourier)+'$')
-        else:
-            ax.plot(x,y,'-b',linewidth=1)
-
+      if i == rlist[0]:
+         ax.plot(x,y,'-b',linewidth=1,label=r'$\mathrm{Fourier}~'+str(nfourier)+'$')
+      else:
+         ax.plot(x,y,'-b',linewidth=1)
 
 # LCFS
-i = prof.n_exp-1
-a0r = prof.geo['ar'][0,i]
-a0z = prof.geo['az'][0,i]
+i = nexp-1
+x = geo_ar[0,i]/2
+y = geo_az[0,i]/2
 
-x = a0r/2
-y = a0z/2
-
-for j in range(1,1+prof.nfourier,1):
-    ar = prof.geo['ar'][j,i]
-    br = prof.geo['br'][j,i]
-    az = prof.geo['az'][j,i]
-    bz = prof.geo['bz'][j,i]
-    x = x+ar*np.cos(j*t)+br*np.sin(j*t)
-    y = y+az*np.cos(j*t)+bz*np.sin(j*t)
+for j in range(nfourier):
+   p = j+1
+   ar = geo_ar[p,i]
+   br = geo_br[p,i]
+   az = geo_az[p,i]
+   bz = geo_bz[p,i]
+   x = x+ar*np.cos(p*t)+br*np.sin(p*t)
+   y = y+az*np.cos(p*t)+bz*np.sin(p*t)
 
 ax.plot(x,y,'-m',linewidth=2)
 
 ax.legend()
 if ftype == 'screen':
-    plt.show()
+   plt.show()
 else:
-    outfile = ftype
-    plt.savefig(outfile)
+   outfile = ftype
+   plt.savefig(outfile)
