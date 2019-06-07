58f2dbc825dad54d1a2aca8e040d0265f2585527
Jeff Candy <jcandy@san.rr.com>
2019-06-07 08:49:12 -0700

    New build process via copying files over

--- f2py/README.txt
+++ f2py/README.txt
@@ -1,11 +1,12 @@
-pygacode: Python interface to gacode profile and geometry tools in Fortran
+gacode
+------
+
+Python interface to gacode profile and geometry tools in Fortran
 
 Usage: 
-- import pygacode 
+- from pygacode import cgyro/neo/tgyro/gyro  [python data/plotting classes]
+- from gacode import expro/geo               [f2py interfaces to expro and geo]
 
-Modules:
-- pygacode.expro
-- pygacode.geo
 
 
 
--- f2py/setup.py
+++ f2py/setup.py
@@ -1,19 +1,31 @@
 from numpy.distutils.core import setup, Extension
 
-wrapper = Extension('pygacode',
+wrapper = Extension('gacode',
                     sources=['expro/expro.f90',
                              'expro/expro_util.f90',
                              'expro/expro_pycomm.f90',
                              'geo/geo.f90'])
 
-setup(name='pygacode',
+setup(name='gacode',
       version='0.1',
       description='Python interface to gacode profile and geometry tools.',
       url='https://gafusion.github.io/doc',
       author='General Atomics Theory Group',
       author_email='candy@fusion.gat.com',
       license='MIT',
-      packages=['expro','geo'],
-      ext_modules=[wrapper],
-      zip_safe=True
+      packages=['expro',
+                'geo',
+                'pygacode.cgyro',
+                'pygacode.gyro',
+                'pygacode.tgyro',
+                'pygacode.neo'
+      ],
+      package_dir={'expro':'expro',
+                   'geo':'geo',
+                   'pygacode.cgyro':'pygacode/cgyro',
+                   'pygacode.gyro':'pygacode/gyro',
+                   'pygacode.tgyro':'pygacode/tgyro',
+                   'pygacode.neo':'pygacode/neo'
+      },
+      ext_modules=[wrapper]
 )
