d217492e385e619442cbdf5820ea851156995507
Jeff Candy <jcandy@san.rr.com>
2019-09-10 15:38:24 -0700

    Fixed behaviour near origin

--- python/profiles_gen/prgen_shapeprofile.py
+++ python/profiles_gen/prgen_shapeprofile.py
@@ -8,16 +8,22 @@ from prgen_geqdsk import *
 from prgen_contour import *
 from prgen_shape import *
 
-def ring(x,u,xm):
-   i = np.argmin(np.abs(x-xm))
-   x0 = x[i]
-   y0 = u[i]
-   d = 0.0
-   for j in range(8):
-      d = y0*(1.0+d-x0)
+def extrap(x,u):
+   m = (u[5]-u[4])/(x[5]-x[4])
+   b = u[5]-m*x[5]
+   u[0] = b
+   u[1] = m*x[1]+b
+   u[2] = m*x[2]+b
+   u[3] = m*x[3]+b
+   return u
 
-   z = d/(1+d-x)
-   return z
+def zero(x,u):
+   r = u[4]/x[4]
+   u[0] = 0.0
+   u[1] = x[1]*r
+   u[2] = x[2]*r
+   u[3] = x[3]*r
+   return u
 
 def iring(x,u,xm):
    i = np.argmin(np.abs(x-xm))
@@ -29,18 +35,6 @@ def iring(x,u,xm):
 
    z = d/(1+d-x)
    return z,i
-
-def sing(x,u,xm):
-   i = np.argmin(np.abs(x-xm))
-   x0 = x[i]
-   y0 = u[i]
-   r  = 1/y0
-   d = 0.0
-   for j in range(8):
-      d = (1.0+d-x0)**r
-      
-   z = np.log(1+d-x)/np.log(d)
-   return z
 
 def ising(x,u,xm):
    i = np.argmin(np.abs(x-xm))
@@ -68,14 +62,13 @@ efit = prgen_geqdsk(gfile)
 n_arc = 512
 
 ri,zi,psi,q,p,fpol = prgen_contour(efit,nrz=nrz,levels=npsi,psinorm=0.999,narc=n_arc,quiet=False)
-#print(fpol)
 
 pnorm = ((psi[:]-psi[0])/(psi[-1]-psi[0]))
 rnorm = np.sqrt(pnorm)
 
 # ci -> cosine terms
 # si -> sine terms
-# xi -> rmin,rmaj,kappa,delta
+# xi -> rmin,rmaj,kappa,zmaj
 
 if ix < 1:
    ci = np.zeros([nf+1,npsi])
@@ -87,14 +80,28 @@ if ix < 1:
       ci[:,i+1] = cr[:]
       si[:,i+1] = sr[:]
       xi[:,i+1] = xr[:]
-   ci[0,0] = 2*ci[0,1]-ci[0,2] # tilt
-   xi[:,0] = 2*xi[:,1]-xi[:,2] # tilt
 else:
    r=ri[:,ix] ; z=zi[:,ix]
    cr,sr,xr = prgen_shape(r,z,n_arc,nf,True)
    sys.exit()
 
-# Repair functions
+# Repair functions near origin
+
+xi[0,:] = zero(rnorm,xi[0,:]) # rmin
+xi[1,:] = extrap(rnorm,xi[1,:]) # rmaj
+xi[2,:] = extrap(rnorm,xi[2,:]) # kappa
+xi[3,:] = extrap(rnorm,xi[3,:]) # zmaj
+
+si[1,:] = zero(rnorm,si[1,:]) # delta
+si[2,:] = zero(rnorm,si[2,:]) # zeta
+si[3,:] = zero(rnorm,si[3,:]) # s3
+
+ci[0,:] = extrap(rnorm,ci[0,:]) # tilt
+ci[1,:] = zero(rnorm,ci[1,:]) # c1
+ci[2,:] = zero(rnorm,ci[2,:]) # c2
+ci[3,:] = zero(rnorm,ci[3,:]) # c3
+
+# Repair near separatrix
 si0 = np.zeros([nf+1,npsi])
 ci0 = np.zeros([nf+1,npsi])
 si0[:,:] = si[:,:]
@@ -115,10 +122,6 @@ ci[1,i:] = u[-1]*z[i:]
 u = ci0[2,:]
 z,i = iring(pnorm,u/u[-1],0.81)
 ci[2,i:] = u[-1]*z[i:]
-
-u = ci0[3,:]
-z,i = iring(pnorm,u/u[-1],0.81)
-ci[3,i:] = u[-1]*z[i:]
 
 if ix == 0:
    f=open('out.dim','w')
@@ -145,11 +148,20 @@ rc('text',usetex=True)
 rc('font',size=18)
 
    
-fig = plt.figure(figsize=(14,8))
+fig = plt.figure(figsize=(14,12))
 
-label=['c_0','c_1','c_2','c_3','c_4']
-for i in range(nf+1):
-   ax = fig.add_subplot(2,nf+1,i+1)
+label=['r','R','\kappa','Z']
+for i in range(4):
+   ax = fig.add_subplot(3,4,i+1)
+   ax.set_xlabel(r'$\psi^{1/2}$')
+   ax.set_title(r'$'+label[i]+'$')
+   ax.grid(which="both",ls=":")
+   ax.set_xlim([0.0,1])
+   ax.plot(rnorm,xi[i,:],'-k',linewidth=1,alpha=1)
+
+label=['c_0','c_1','c_2','c_3']
+for i in range(4):
+   ax = fig.add_subplot(3,4,i+5)
    ax.set_xlabel(r'$\psi$')
    ax.set_title(r'$'+label[i]+'$')
    ax.grid(which="both",ls=":")
@@ -158,23 +170,17 @@ for i in range(nf+1):
    ax.plot(pnorm,u,'-r',linewidth=1,alpha=1)
    ax.plot(pnorm,u0,'-k',linewidth=1,alpha=1)
 
-label=['\kappa','\delta','\zeta','s_3','s_4']
-for i in range(nf+1):
-   ax = fig.add_subplot(2,nf+1,i+1+nf+1)
+label=['-','\delta','-\zeta','s_3']
+for i in range(4):
+   ax = fig.add_subplot(3,4,i+9)
    ax.set_xlabel(r'$\psi$')
    ax.set_title(r'$'+label[i]+'$')
    ax.grid(which="both",ls=":")
-   ax.set_xlim([0.0,1])
+   ax.set_xlim([0.0,1.0])
    if i > 0:
       u = si[i,:] ; u0 = si0[i,:]
-      if i == 1:         
-         ax.plot(rnorm,u,'-r',linewidth=1,alpha=1)
-         ax.plot(rnorm,u0,'-k',linewidth=1,alpha=1)
-      else:
-         ax.plot(pnorm,u,'-r',linewidth=1,alpha=1)
-         ax.plot(pnorm,u0,'-k',linewidth=1,alpha=1)
-   else:
-      ax.plot(pnorm,xi[1,:],'-k',linewidth=1)
+      ax.plot(pnorm,u,'-r',linewidth=1,alpha=1)
+      ax.plot(pnorm,u0,'-k',linewidth=1,alpha=1)
       
       
 plt.tight_layout()
