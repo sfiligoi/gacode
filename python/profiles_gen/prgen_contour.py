q calculation fixes

python/profiles_gen/prgen_contour.py
@@ -297,11 +297,10 @@ def prgen_contour(geqdsk,nrz,levels,psinorm,narc,quiet):
     loopint = np.zeros([levels])
     for k in range(levels):
        for i in range(narc-1):
-          loopint[k] = loopint[k]+(RI[i+1,k]-RI[i,k])*(ZI[i+1,k]+ZI[i,k])/(RI[i+1,k]+RI[i,k])-(ZI[i+1,k]-ZI[i,k])
+          loopint[k] = loopint[k]+(RI[i+1,k]-RI[i,k])*(ZI[i+1,k]+ZI[i,k])/(RI[i+1,k]+RI[i,k])
                     
-    loopint = loopint/(4*np.pi)
-    new_q = 2*out_f*np.gradient(loopint,out_psi)
+    loopint = loopint/(2*np.pi)
+    new_q = out_f*np.gradient(loopint,out_psi)
 
-    print(new_q)
-    return RI,ZI,out_psi,out_q,out_p,out_f
+    return RI,ZI,out_psi,new_q,out_p,out_f
 