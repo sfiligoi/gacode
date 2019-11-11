35fa1ac1b26ac1df0ada00e19a906bbe5c3e249a
Orso Meneghini <orso82@gmail.com>
2019-11-10 19:55:56 -0800

    Cleanup debug statements from omfit gapy

--- f2py/gacode/omfit.py
+++ f2py/gacode/omfit.py
@@ -3,36 +3,35 @@ from gacode import expro
 
 # Function to decode the insane string returned by gacode/f2py
 def gapystr(s):
-    import sys
     if sys.version_info[0] == 2:
         u = []
         for i in range(len(s)):
             u.append(str(s[i]).strip())
         return u
     else:
-        return str(s,'utf-8').split()
+        return str(s, 'utf-8').split()
+
 
 class gapy(dict):
-
     def __init__(self, filename, input_profiles_compatibility_mode=True):
         expro.expro_read(filename)
-        list=gapystr(expro.expro_list)
+        list = gapystr(expro.expro_list)
 
         # Define Gapy class members corresponding to list[]
         for item in list:
-           print('* '+item)
-           self[item] = getattr(expro,'expro_'+item)
+            self[item] = getattr(expro, 'expro_' + item)
 
         # Species name and type
         self['name'] = gapystr(expro.expro_name)
         self['type'] = gapystr(expro.expro_type)
 
+        # refactor output to be compatible with input.profiles format
         if input_profiles_compatibility_mode:
             self['Te'] = self['te']
             del self['te']
             self['Ti'] = self['ti']
             del self['ti']
-            for quantity in ['ni','Ti','vpol','vtor']:
+            for quantity in ['ni', 'Ti', 'vpol', 'vtor']:
                 if quantity in self:
                     for k in range(self['n_ion']):
                         self[quantity + '_%d' % (k + 1)] = self[quantity][k]
@@ -42,5 +41,3 @@ class gapy(dict):
                 self['IONS'].append([self['name'][k], self['z'][k], self['mass'][k], self['type'][k]])
             del self['z']
             del self['mass']
-            print(self['IONS'])
-
