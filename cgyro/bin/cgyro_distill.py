1f5a977858b34d507e0f48361a61c9126f9e3963
Jeff Candy <candy@jaamweb.com>
2019-12-16 13:54:29 -0800

    Improved distill diagnostics

--- cgyro/bin/cgyro_distill.py
+++ cgyro/bin/cgyro_distill.py
@@ -34,14 +34,12 @@ t = sim.t
 imin,imax=iwindow(t,w,0.0)
 
 header = 'Averaging over '+str(t[imin])+' < (c_s/a) t < '+str(t[imax])
-header = header+' nkx = '+str(nr)+' nky = '+str(nn)+'\n'
-
-print(header)
+header = header+' | nkx = '+str(nr)+' | nky = '+str(nn)+'\n'
 
 # kxky_phi -> [2,self.n_radial,self.theta_plot,self.n_n,nt]
 
-# 1. Average phi^2
-phi2 = sim.kxky_phi[0,:,0,:,:]**2+sim.kxky_phi[1,:,0,:,:]**2
+# 1. Average phi^2/rho_{*D}
+phi2 = (sim.kxky_phi[0,:,0,:,:]**2+sim.kxky_phi[1,:,0,:,:]**2)/sim.rho**2
 
 ave_phi2 = np.zeros([nr,nn])
 for j in range(nn):
@@ -49,14 +47,17 @@ for j in range(nn):
 
 np.savetxt('out.cgyro.phi2kxky',ave_phi2,fmt='%1.6e',header=header)
 
-print('Wrote out.cgyro.phi2kxky')
-
 # 2. Average Re(phi_0)
-phi0 = sim.kxky_phi[:,:,0,0,:]
+phi0 = sim.kxky_phi[:,:,0,0,:]/sim.rho
 ave_phi0 = np.zeros([nr,2])
 ave_phi0[:,0] = average_n(phi0[0,:,:],t,w,0.0,nr)
 ave_phi0[:,1] = average_n(phi0[1,:,:],t,w,0.0,nr)
 
 np.savetxt('out.cgyro.phi0kx',ave_phi0,fmt='%1.6e',header=header)
 
-print('Wrote out.cgyro.phi0kx')
+# Diagnostic print
+
+print('INFO: (cgyro_distill).py) Wrote out.cgyro.phi2kxky = <|phi_k|^2>/rho^2_*D')
+print('INFO: (cgyro_distill).py) Wrote out.cgyro.phi0kx   = <Re(phi_0)>/rho_*D, Im(phi_0)>/rho_{D*}')
+print('INFO: (cgyro_distill).py) All field normalized to rho_{*D}')
+print('INFO: (cgyro_distill).py) '+header)
