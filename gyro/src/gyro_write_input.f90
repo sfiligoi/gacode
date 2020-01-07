83382806781866039bb3377c4d481acc53eaf501
Bhavin Patel <bhavin.patel@york.ac.uk>
2020-01-07 11:12:27 +0000

    Added normalisations to interface. Added surface area to text output

--- gyro/src/gyro_write_input.f90
+++ gyro/src/gyro_write_input.f90
@@ -219,6 +219,7 @@ subroutine gyro_write_input
      write(1,20) 'alpha_MHD',q_norm**2*rmaj_s(ir_norm)*beta_star_s(ir_norm)
      write(1,20) 'lambda_*',lambda_debye/rhos_norm
      write(1,20) 'volume (1/a^3)',GEO_volume
+     write(1,20) 'surface area (1/a^2)',GEO_surf
      write(1,20) 'omega_00 (c_s/a)',w0_s(ir_norm)
      write(1,*) '* Note that f = f_sim exp(i n omega0[r0] t)'
      if (betae_unit_norm > 0.0) then
