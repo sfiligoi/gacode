8416e990f03ecbe1825410dc654245c60ce59a9f
Jeff Candy <candy@jaamweb.com>
2020-07-08 11:24:29 -0700

    Removed some profile shear variables, removed spurious write_prec

--- gyro/src/gyro_profile_init.f90
+++ gyro/src/gyro_profile_init.f90
@@ -33,7 +33,7 @@ subroutine gyro_profile_init
   implicit none
   !
   integer :: ic
-  real :: loglam,x0,tri
+  real :: loglam
   real :: cc
   !---------------------------------------------------
 
@@ -636,19 +636,6 @@ subroutine gyro_profile_init
      if (angp(i) > 0.5) angp(i) = angp(i)-1.0
   enddo
   !-------------------------------------------------------------
-
-  ! Profile shearing
-  if (i_proc == 0) open(unit=10,file='out.gyro.star',status='replace')
-  do i=1,n_x
-
-     x0 = (r(i)-r_norm)/x_length*2*pi
-     tri = (4.0/pi)*(sin(x0)-sin(3*x0)/9.0)
-     dlntdr_s(:,i) = dlntdr_s(:,ir_norm)+tri*sdlntdr(:)*x_length/rhosda_s(ir_norm)/(2*pi)
-     dlnndr_s(:,i) = dlnndr_s(:,ir_norm)+tri*sdlnndr(:)*x_length/rhosda_s(ir_norm)/(2*pi)
-
-     if (i_proc == 0) write(10,'(2(1pe12.5,1x))') x0,dlnndr_s(1,i) 
-  enddo
-  if (i_proc == 0) close(10)
      
   if (debug_flag == 1 .and. i_proc == 0) then
      print *,"[gyro_profile_init done]"
