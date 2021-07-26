32bba1928267b46c1ac7ef51e402475230210499
Emily Belli <bellie@fusion.gat.com>
2021-07-26 15:43:17 -0700

    Added extra cgyro_run vecs

--- tgyro/src/tgyro_multi_driver.f90
+++ tgyro/src/tgyro_multi_driver.f90
@@ -30,13 +30,17 @@ subroutine tgyro_multi_driver
 
   else 
 
+     allocate(cgyro_status_vec(n_inst))
+     allocate(cgyro_n_species_vec(n_inst))
+     allocate(cgyro_tave_min_vec(n_inst))
+     allocate(cgyro_tave_max_vec(n_inst))
+     allocate(cgyro_flux_tave_vec(11,3,n_inst))
+     allocate(cgyro_flux_tave_out(11,3))
+     
      ! Initialize CGYRO
-     print *, 'calling cgyro_init'
      call cgyro_init(lpath,gyro_comm)
-     print *, 'calling cgyro_init_kernel'
      call cgyro_init_kernel
 
-     print *, 'calling cgyro_run'
      ! Run CGYRO
      cgyro_var_in = -1.0
      call cgyro_run(gyrotest_flag,cgyro_var_in,cgyro_n_species_out, &
@@ -44,6 +48,13 @@ subroutine tgyro_multi_driver
           cgyro_status_out)
      call cgyro_final_kernel
 
+     if(allocated(cgyro_status_vec))    deallocate(cgyro_status_vec)
+     if(allocated(cgyro_n_species_vec)) deallocate(cgyro_n_species_vec)
+     if(allocated(cgyro_tave_min_vec))  deallocate(cgyro_tave_min_vec)
+     if(allocated(cgyro_tave_max_vec))  deallocate(cgyro_tave_max_vec)
+     if(allocated(cgyro_flux_tave_vec)) deallocate(cgyro_flux_tave_vec)
+     if(allocated(cgyro_flux_tave_out)) deallocate(cgyro_flux_tave_out)
+     
   endif
 
 
