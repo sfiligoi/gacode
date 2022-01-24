d0d2c65cde0e9dff073fe4fb5764b9ca65044fd4
Jeff Candy <candy@jaamweb.com>
2022-01-24 14:48:28 -0800

    Refined version test for prgen_contour

--- f2py/pygacode/profiles_gen/prgen_contour.py
+++ f2py/pygacode/profiles_gen/prgen_contour.py
@@ -19,10 +19,10 @@ def contourPaths(x, y, Z, levels):
     allsegs = []
     for level in levels:
         segs_=contour_generator.create_contour(level)
-        if sys.version_info[1] < 8:
-            iseg = segs_
-        else:
+        if sys.version_info[1] > 8 and sys.version_info[2] > 3:
             iseg = segs_[0]
+        else:
+            iseg = segs_
         segs=list(map(mp.Path,iseg))
         allsegs.append(segs)
 
