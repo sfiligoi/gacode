907143b945f7a77c8d8a2d2a09426fbfa9c91f86
Jeff Candy <candy@jaamweb.com>
2022-03-14 10:54:32 -0700

    Added small amount of new mapper documentation

--- f2py/pygacode/profiles_gen/prgen_contour.py
+++ f2py/pygacode/profiles_gen/prgen_contour.py
@@ -22,6 +22,8 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
    psi_efit = g['PSIRZ']
    #-------------------------------------------------------------------------
 
+   #-------------------------------------------------------------------------
+   # Pixel representation
    xv = np.linspace(0,1,nx)
    yv = np.linspace(0,1,ny)
 
@@ -41,8 +43,10 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
       mx = nx
       my = ny
 
-   xaxis = int((g['RMAXIS']-g['RLEFT'])/g['RDIM']*(mx-1))
-   yaxis = int((g['ZMAXIS']-g['ZMID']+g['ZDIM']/2)/g['ZDIM']*(my-1))
+   # Pixel values of magnetic axis
+   kx = int((g['RMAXIS']-g['RLEFT'])/g['RDIM']*(mx-1))
+   ky = int((g['ZMAXIS']-g['ZMID']+g['ZDIM']/2)/g['ZDIM']*(my-1))
+   #-------------------------------------------------------------------------
 
    #-------------------------------------------------------------------------
    # Find separatrix using bisection
@@ -58,7 +62,7 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
       for contour in contours:
          if contour[0,0] == contour[-1,0] and contour[0,1] == contour[-1,1]:
             xc0 = contour[:,1] ; yc0 = contour[:,0]
-            if min(xc0) < xaxis < max(xc0) and min(yc0) < yaxis < max(yc0):
+            if min(xc0) < kx < max(xc0) and min(yc0) < ky < max(yc0):
                found = 1
                xc = xc0 ; yc = yc0
       if found == 0:
@@ -66,16 +70,22 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
          dz = dz/2
 
    psi_sep = z0
+   #-------------------------------------------------------------------------
 
    psi_efit_min = np.min(psi_efit[yaxis-8:yaxis+8,xaxis-8:xaxis+8])
    print('INFO: (prgen_contour) psi_min (% efit) = {:.3f}'.format(100*psi_efit_min/psi0))
    print('INFO: (prgen_contour) psi_sep (% efit) = {:.3f}'.format(100*psi_sep/psi1))
-   
+
+   #-------------------------------------------------------------------------
+   # Compute interior contours
+   #
    rv = np.zeros([narc,nc])
    zv = np.zeros([narc,nc])
    psic = np.zeros(nc)
 
+   # Move inside of separatrix by amount psinorm
    dpsi = psinorm*(psi1-psi0)/nc
+
    for i in range(nc):
       z0 = psi0+(i+1.0)*dpsi
       psic[i] = z0
@@ -87,7 +97,7 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
                # Arc length
                dl = np.sqrt(np.diff(xc)**2+np.diff(yc)**2)
                larc = np.zeros([len(xc)]) ; larc[1:] = np.cumsum(dl)
-        
+
                # Cubic interpolation from fine contour mesh to coarse t-mesh
                t = np.linspace(0,1,narc)*larc[-1]
                cs = interpolate.splrep(larc,xc,per=True) ; r=interpolate.splev(t,cs) 
@@ -98,11 +108,14 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
                r[0:-1] = np.roll(r[0:-1],-s) ; r[-1] = r[0] 
                z[0:-1] = np.roll(z[0:-1],-s) ; z[-1] = z[0]
 
+               # Map back from pixels to physical dimensions
                rv[:,i] = g['RLEFT']+g['RDIM']/(mx-1)*r
                zv[:,i] = g['ZMID']-g['ZDIM']/2+g['ZDIM']/(my-1)*z
-            
    #-------------------------------------------------------------------------
 
+   #-------------------------------------------------------------------------
+   # Radial profiles of p,f,q
+   #
    efitpsi = np.linspace(psi0,psi1,npsi)
    cs = interpolate.interp1d(efitpsi,efitp,kind='quadratic') ; out_p = cs(psic)
    cs = interpolate.interp1d(efitpsi,efitf,kind='quadratic') ; out_f = cs(psic)
@@ -115,7 +128,6 @@ def prgen_contour(g,mult,nc,psinorm,narc,quiet):
 
    cs = interpolate.splrep(psic,loopint) ; out_q = interpolate.splev(psic,cs,der=1)
    out_q = out_f*out_q/(2*np.pi)
+   #-------------------------------------------------------------------------
 
    return rv,zv,psic,out_p,out_f,out_qe,out_q
-
-
