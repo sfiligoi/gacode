ec22fbc830c9c70b7881eb898d607d97ca10ef76
Igor Sfiligoi <igor.sfiligoi@ga.com>
2022-08-18 16:22:52 -0400

    Move calc loop into separate subroutine, use the case code for CPU and GPU-no-bigmem

--- cgyro/src/cgyro_step_collision.F90
+++ cgyro/src/cgyro_step_collision.F90
@@ -8,43 +8,25 @@
 !                       H = h + ze/T G phi
 !------------------------------------------------------------------------------
 
-#ifndef _OPENACC
-subroutine cgyro_step_collision_cpu
+subroutine cgyro_calc_collision_cpu(nj_loc)
 
   use parallel_lib
-  use timer_lib
-
   use cgyro_globals
 
   implicit none
-
-  integer :: is,ivp,it,j,k,nj_loc
+  !
+  integer, intent(in) :: nj_loc
+  !
+  integer :: ivp,j,k
   complex, dimension(nv) :: bvec,cvec
   real :: cvec_re,cvec_im
   real :: cval
 
-  !----------------------------------------------------------------
-  ! Perform data tranpose from _c to _v data layouts:
-  call timer_lib_in('coll_mem')
-  call parallel_lib_rtrans_pack(cap_h_c)
-  call timer_lib_out('coll_mem')
-  call timer_lib_in('coll_comm')
-  call parallel_lib_r_do(cap_h_v)
-  call timer_lib_out('coll_comm')
-  !----------------------------------------------------------------
-
-  call timer_lib_in('coll')
-
-  call parallel_lib_nj_loc(nj_loc)
-
-!$omp parallel do private(ic,ic_loc,it,iv,ivp,cvec,bvec,cvec_re,cvec_im,cval) firstprivate(collision_model)
+!$omp parallel do private(ic,ic_loc,iv,ivp,cvec,bvec,cvec_re,cvec_im,cval,j,k) &
+!$omp&            shared(cap_h_v,fsendf,cmat) firstprivate(nv,nproc,nj_loc)
   do ic=nc1,nc2
 
      ic_loc = ic-nc1+1
-     ! Set-up the RHS: H = f + ze/T G phi
-
-     it = it_c(ic)
-
      ! Set-up the RHS: H = f + ze/T G phi
 
      do iv=1,nv
@@ -79,6 +61,35 @@ subroutine cgyro_step_collision_cpu
     endif
  enddo
 
+end subroutine cgyro_calc_collision_cpu
+
+#ifndef _OPENACC
+subroutine cgyro_step_collision_cpu
+
+  use parallel_lib
+  use timer_lib
+
+  use cgyro_globals
+
+  implicit none
+
+  integer :: is,nj_loc
+
+  !----------------------------------------------------------------
+  ! Perform data tranpose from _c to _v data layouts:
+  call timer_lib_in('coll_mem')
+  call parallel_lib_rtrans_pack(cap_h_c)
+  call timer_lib_out('coll_mem')
+  call timer_lib_in('coll_comm')
+  call parallel_lib_r_do(cap_h_v)
+  call timer_lib_out('coll_comm')
+  !----------------------------------------------------------------
+
+  call timer_lib_in('coll')
+
+  call parallel_lib_nj_loc(nj_loc)
+  call cgyro_calc_collision_cpu(nj_loc)
+
   call timer_lib_out('coll')
 
   ! Compute the new phi
@@ -96,7 +107,7 @@ subroutine cgyro_step_collision_cpu
 
   ! Compute H given h and [phi(h), apar(h)]
 
-!$omp parallel do private(iv_loc,is,ic,iv)
+!$omp parallel do private(iv_loc,is,ic,iv) firstprivate(nc)
   do iv=nv1,nv2
      iv_loc = iv-nv1+1
      do ic=1,nc
@@ -119,39 +130,22 @@ end subroutine cgyro_step_collision_cpu
   ! else _OPENACC
 #else
 
-subroutine cgyro_step_collision_gpu
+subroutine cgyro_calc_collision_gpu(nj_loc)
 
   use parallel_lib
-  use timer_lib
-
   use cgyro_globals
 
   implicit none
+  !
+  integer, intent(in) :: nj_loc
+  !
 
   integer :: j,k
-  integer :: is,ivp,nj_loc
+  integer :: ivp
 
   complex, dimension(nv) :: bvec,cvec
   real :: cvec_re,cvec_im
   real :: cval
-
-  !----------------------------------------------------------------
-  ! Perform data tranpose from _c to _v data layouts:
-  call timer_lib_in('coll_mem')
-  call parallel_lib_rtrans_pack_gpu(cap_h_c)
-  call timer_lib_out('coll_mem')
-  call timer_lib_in('coll_comm')
-  call parallel_lib_r_do_gpu(cap_h_v)
-  call timer_lib_out('coll_comm')
-  !----------------------------------------------------------------
-
-
-  call parallel_lib_nj_loc(nj_loc)
-
-  ! Note that if GPU is absent, gpu_bibmem_flag will be reset to 0
-
-  if (gpu_bigmem_flag == 1) then
-     call timer_lib_in('coll')
 
 !$acc parallel loop gang private(bvec,cvec) &
 !$acc& present(cmat,cap_h_v,fsendf) 
@@ -196,7 +190,37 @@ subroutine cgyro_step_collision_gpu
            endif
         endif
      enddo
+end subroutine cgyro_calc_collision_gpu
 
+subroutine cgyro_step_collision_gpu
+
+  use parallel_lib
+  use timer_lib
+
+  use cgyro_globals
+
+  implicit none
+
+  integer :: is,nj_loc
+
+  !----------------------------------------------------------------
+  ! Perform data tranpose from _c to _v data layouts:
+  call timer_lib_in('coll_mem')
+  call parallel_lib_rtrans_pack_gpu(cap_h_c)
+  call timer_lib_out('coll_mem')
+  call timer_lib_in('coll_comm')
+  call parallel_lib_r_do_gpu(cap_h_v)
+  call timer_lib_out('coll_comm')
+  !----------------------------------------------------------------
+
+
+  call parallel_lib_nj_loc(nj_loc)
+
+  ! Note that if GPU is absent, gpu_bibmem_flag will be reset to 0
+
+  if (gpu_bigmem_flag == 1) then
+     call timer_lib_in('coll')
+     call cgyro_calc_collision_gpu(nj_loc)
      call timer_lib_out('coll')
 
   else
@@ -206,48 +230,8 @@ subroutine cgyro_step_collision_gpu
      call timer_lib_out('coll_mem')
 
      call timer_lib_in('coll')
-
-!$omp parallel do private(ic_loc,iv,ivp,cvec,bvec,cvec_re,cvec_im,cval)
-     do ic=nc1,nc2
-
-        ic_loc = ic-nc1+1
-
-        ! Set-up the RHS: H = f + ze/T G phi
-
-        do iv=1,nv
-           cvec(iv) = cap_h_v(ic_loc,iv)
-        enddo
-
-        bvec(:) = (0.0,0.0)
-
-        ! This is a key loop for performance
-        do ivp=1,nv
-           cvec_re = real(cvec(ivp))
-           cvec_im = aimag(cvec(ivp))
-           do iv=1,nv
-              cval = cmat(iv,ivp,ic_loc)
-              bvec(iv) = bvec(iv)+ cmplx(cval*cvec_re,cval*cvec_im)
-           enddo
-        enddo
-
-        ! Pack communication array while bvec still in cache
-        do k=1,nproc
-           do j=1,nj_loc
-              fsendf(j,ic_loc,k) = bvec(j+(k-1)*nj_loc)
-           enddo
-        enddo
-
-        if (collision_field_model == 1) then
-           if (.not.(n == 0 .and. ae_flag == 1)) then
-              ! cap_h_v not re-used else
-              do iv=1,nv
-                 cap_h_v(ic_loc,iv) = bvec(iv)
-              enddo
-           endif
-        endif
-     enddo
-
-    call timer_lib_out('coll')
+     call cgyro_calc_collision_cpu(nj_loc)
+     call timer_lib_out('coll')
 
     call timer_lib_in('coll_mem')
 !$acc update device(fsendf)
