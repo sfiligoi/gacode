c9655a2473a3c718d70c6a3cf44fd60afa9cf80b
Igor Sfiligoi <isfiligoi@sdsc.edu>
2022-12-15 12:03:55 -0800

    Refactor shear_hammett to make it more parallelizable

--- cgyro/src/cgyro_shear_hammett.F90
+++ cgyro/src/cgyro_shear_hammett.F90
@@ -20,61 +20,68 @@ subroutine cgyro_shear_hammett
   implicit none
 
   integer :: ir
+  integer :: n_changes
+  integer, dimension(nt1:nt2) :: change_sign
+  integer :: my_sign
 
   
+  n_changes = 0
+
+  call timer_lib_in('shear')
   gtime(my_toroidal) = gtime(my_toroidal)+omega_eb_base*my_toroidal*delta_t
 
   ! Forward shearing
   if (gtime(my_toroidal) > 0.5) then
 
-     call timer_lib_in('shear')
      gtime(my_toroidal) = gtime(my_toroidal)-1.0
+     n_changes = n_changes + 1
+     change_sign(my_toroidal) = 1
 
+  ! Backward shearing
+  else if (gtime(my_toroidal) < -0.5) then
+
+     gtime(my_toroidal) = gtime(my_toroidal)+1.0
+     n_changes = n_changes + 1
+     change_sign(my_toroidal) = -1
+
+  else
+     change_sign(my_toroidal) = 0
+  endif
+
+  if (n_changes > 0) then
 #ifdef _OPENACC
-!$acc parallel loop independent gang private(ir) present(h_x,ic_c)
+!$acc parallel loop independent gang vector_length(n_theta) &
+!$acc&              private(ir,my_sign) present(h_x,ic_c) copyin(change_sign)
 #else
-!$omp parallel do private(ir)
+!$omp parallel do private(ir,my_sign)
 #endif
      do iv_loc=1,nv_loc
 
+      my_sign = change_sign(my_toroidal)
+      if (my_sign == 1) then
+       ! Forward shearing
 !acc loop seq
        do ir=2,n_radial
          h_x(ic_c(ir-1,:),iv_loc,my_toroidal) = h_x(ic_c(ir,:),iv_loc,my_toroidal)
        enddo
 
        h_x(ic_c(n_radial,:),iv_loc,my_toroidal) = 0.0
-     enddo
-
-     call timer_lib_out('shear')
-
-     call cgyro_field_c
-
-  endif
-
-  ! Backward shearing
-  if (gtime(my_toroidal) < -0.5) then
-
-     call timer_lib_in('shear')
-     gtime(my_toroidal) = gtime(my_toroidal)+1.0
-
-#ifdef _OPENACC
-!$acc parallel loop independent gang private(ir) present(h_x,ic_c)
-#else
-!$omp parallel do private(ir)
-#endif
-     do iv_loc=1,nv_loc
-
+      else if (my_sign == -1) then
+       ! Backward shearing
 !acc loop seq
        do ir=n_radial-1,1,-1
          h_x(ic_c(ir+1,:),iv_loc,my_toroidal) = h_x(ic_c(ir,:),iv_loc,my_toroidal)
        enddo
 
        h_x(ic_c(1,:),iv_loc,my_toroidal) = 0.0
+      endif
      enddo
-     call timer_lib_out('shear')
+  endif
 
+  call timer_lib_out('shear')
+
+  if (n_changes > 0) then
      call cgyro_field_c
-
   endif
 
 end subroutine cgyro_shear_hammett
