Fixed time-averaging bug

f2py/pygacode/cgyro/data_plot.py
@@ -516,8 +516,8 @@ class cgyrodata_plot(data.cgyrodata):
 
       for ispec in range(ns):
          y_norm = y[ispec,:]*norm_vec[ispec]
-         ave = time_average(y_norm,t,imin,imax)
-         y_ave   = ave*np.ones(len(t))
+         ave   = time_average(y_norm,t,imin,imax)
+         y_ave = ave*np.ones(len(t))
          u = specmap(self.mass[ispec],self.z[ispec])
          label = r'$'+mtag+mnorm+'_'+u+'/'+mtag+self.gbnorm+': '+str(round(ave,3))+'$'
          if not ftype == 'nox':

f2py/pygacode/gacodefuncs.py
@@ -32,19 +32,19 @@ def time_index(t,w):
 def time_average(f,t,imin,imax):
     
     T = t[imax]-t[imin]
-    dt = np.diff(t)/T
+    dt = np.diff(t)[imin:imax]/T
 
     if f.ndim == 1:
         # 1D array
-        sf = 0.5*(f[1:]+f[:-1])
+        sf = 0.5*(f[imin:imax]+f[imin+1:imax+1])
         ave = np.sum(sf*dt)
     elif f.ndim ==2:
         # 2D array (average over last dimension)
-        sf = 0.5*(f[:,1:]+f[:,:-1])
+        sf = 0.5*(f[:,imin:imax]+f[:,imin+1:imax+1])
         ave = np.sum(sf*dt,axis=-1)
     else:
         # 3D array (average over last dimension)
-        sf = 0.5*(f[:,:,1:]+f[:,:,:-1])
+        sf = 0.5*(f[:,:,imin:imax]+f[:,:,imin+1:imax+1])
         ave = np.sum(sf*dt,axis=-1)
         
     return ave