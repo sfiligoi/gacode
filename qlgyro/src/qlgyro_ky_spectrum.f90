0b5240dd8cb7d0ca0afad1a1ccc25d0bfd8cccad
Joseph McClenaghan <jmcclena@gmail.com>
2024-02-15 09:29:22 -0800

    fix qlgyro few remaining paths from GACODE_ADD_ROOT to GACODE_ROOT remove weird kygrid_model=0 logic

--- qlgyro/src/qlgyro_ky_spectrum.f90
+++ qlgyro/src/qlgyro_ky_spectrum.f90
@@ -18,18 +18,6 @@
          allocate(tglf_ky_spectrum_out(nky), tglf_dky_spectrum_out(nky))
       end if
 
-      if (tglf_kygrid_model_in .eq. 0) then         
-         
-         n_tor = (/2.0, 3.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0, 70.0, 100.0, 120.0, 140./)
-         ky_spectrum = 0.0
-         ky_spectrum(1) = kymin * n_tor(1)
-         dky_spectrum(1) = kymin * n_tor(1)
-         do i_ky=2,nky
-            ky_spectrum(i_ky) = kymin * n_tor(i_ky)
-            dky_spectrum(i_ky) = ky_spectrum(i_ky) - ky_spectrum(i_ky -1)
-         end do
-      end if
-
       tglf_ky_spectrum_out = ky_spectrum(:nky)
       tglf_dky_spectrum_out = dky_spectrum(:nky)
 
